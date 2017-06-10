5031f7eb3ea9ec2258352499edfdcc33cf1391d6
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2017-06-09 20:49:35 -0700

    Update python package name

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='corenlp',
+    name='stanford-corenlp',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='3.7.0',
 
-    description='Python interface for Stanford CoreNLP',
+    description='Official python interface for Stanford CoreNLP',
     long_description=long_description,
 
     # The project's main homepage.
