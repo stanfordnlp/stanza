c334e2c20e7b0aa57cb18e96a4f75627a008bba5
qipeng <qipeng@users.noreply.github.com>
2017-10-16 15:31:47 -0700

    Initial commit with working segmenter label generator for English

---
+++ utils/prepare_segmenter_data.py
@@ -0,0 +1,61 @@
+import argparse
+import re
+from smart_open import smart_open
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('plaintext_file', type=str, help="Plaintext file containing the raw input")
+parser.add_argument('conllu_file', type=str, help="CoNLL-U file containing tokens and sentence breaks")
+parser.add_argument('-o', '--output', default=None, type=str, help="Output file name; output to the console if not specified (the default)")
+
+args = parser.parse_args()
+
+with smart_open(args.plaintext_file, 'r') as f:
+    text = ''.join(f.readlines())
+
+output = sys.stdout if args.output is None else smart_open(args.output, 'w')
+
+index = 0 # character offset in rawtext
+
+def find_next_word(index, text, word, output):
+    while index < len(text) and text[index] != word[0]:
+        if text[index] == '\n' and index+1 < len(text) and text[index+1] == '\n':
+            # paragraph break
+            output.write('\n')
+            index += 1
+        elif re.match(r'^\s$', text[index]):
+            output.write('0') # not word boundary
+        else:
+            assert False, "character mismatch: raw text contains |%s| but the next word is |%s|." % (text[index:(index+20)], word)
+        index += 1
+    return index
+
+with smart_open(args.conllu_file, 'r') as f:
+    buf = ''
+    for line in f:
+        line = line.strip()
+        if len(line):
+            if line[0] == "#":
+                # comment, don't do anything
+                continue
+
+            line = line.split('\t')
+            if '.' in line[0]:
+                # the tokenizer doesn't deal with ellipsis
+                continue
+
+            word = line[1]
+            if len(buf):
+                output.write(buf)
+            index = find_next_word(index, text, word, output)
+            assert text[index:(index+len(word))] == word, "word mismatch: raw text contains |%s| but the next word is |%s|." % (text[index:(index+len(word))], word)
+            buf = '0' * (len(word)-1) + '1'
+            index += len(word)
+        else:
+            # sentence break found
+            if len(buf):
+                output.write(buf[:-1] + '2')
+                buf = ''
+
+output.close()
