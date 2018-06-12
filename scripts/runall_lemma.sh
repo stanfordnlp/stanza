27f51f68c9b8b8ae045043635ff7929cfec6c37b
Yuhao Zhang <zyh@stanford.edu>
2018-06-11 23:28:38 -0700

    Add all treebanks

--- scripts/runall_lemma.sh
+++ scripts/runall_lemma.sh
@@ -1,9 +1,10 @@
 args=$@
 
 n_gpus=`gpustat | tail -n+2 | wc -l`
+treebank_file=./lemma_test_treebanks
 
 i=0
-for tb in `cat ./lemma_test_treebanks`; do
+for tb in `cat $treebank_file`; do
     echo "Treebank: $tb"
     short=`bash scripts/treebank_to_shorthand.sh ud $tb`
     ./scripts/run_lemma.sh $tb $((i % n_gpus)) $args 2>&1 | tee logs/${short}.log &
@@ -18,7 +19,7 @@ done
 
 echo ""
 echo "Training done. Printing results..."
-for tb in `cat ./lemma_test_treebanks`; do
+for tb in `cat $treebank_file`; do
     short=`bash scripts/treebank_to_shorthand.sh ud $tb`
     tail -n1 logs/${short}.log
 done
