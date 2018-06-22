2c96be202c97f847ad228b91cf28dfd03231e7c5
qipeng <qipeng@users.noreply.github.com>
2018-06-22 07:27:38 -0700

    Don't evaluate identity lemmatizer when there's no gold file

--- models/identity_lemmatizer.py
+++ models/identity_lemmatizer.py
@@ -17,10 +17,10 @@ def parse_args():
     parser.add_argument('--eval_file', type=str, default=None, help='Input file for data loader.')
     parser.add_argument('--output_file', type=str, default=None, help='Output CoNLL-U file.')
     parser.add_argument('--gold_file', type=str, default=None, help='Output CoNLL-U file.')
-    
+
     parser.add_argument('--mode', default='train', choices=['train', 'predict'])
     parser.add_argument('--lang', type=str, help='Language')
-    
+
     parser.add_argument('--batch_size', type=int, default=50)
     parser.add_argument('--seed', type=int, default=1234)
     args = parser.parse_args()
@@ -29,11 +29,11 @@ def parse_args():
 def main():
     args = parse_args()
     random.seed(args.seed)
-    
+
     args = vars(args)
 
     print("[Launching identity lemmatizer...]")
-    
+
     if args['mode'] == 'train':
         print("[No training is required; will only generate evaluation output...]")
 
@@ -43,13 +43,14 @@ def main():
 
     # use identity mapping for prediction
     preds = batch.conll.get(['word'])
-    
+
     # write to file and score
     batch.conll.write_conll_with_lemmas(preds, system_pred_file)
-    _, _, score = scorer.score(system_pred_file, gold_file)
-    
-    print("Lemma score:")
-    print("{} {:.2f}".format(args['lang'], score*100))
+    if gold_file is not None:
+        _, _, score = scorer.score(system_pred_file, gold_file)
+
+        print("Lemma score:")
+        print("{} {:.2f}".format(args['lang'], score*100))
 
 if __name__ == '__main__':
     main()
