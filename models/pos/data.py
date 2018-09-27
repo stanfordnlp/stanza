901d538afaa546d4d0870c4c611d5b8108b9d943
Peng Qi <qipeng@users.noreply.github.com>
2018-09-27 14:01:45 -0700

    Fix slurm scripts; Fix Norwegian pretrained lookup

--- models/pos/data.py
+++ models/pos/data.py
@@ -66,7 +66,7 @@ class DataLoader:
         if not os.path.exists(vocab_file) or not os.path.exists(vec_file):
             lcode, tcode = shorthand.split('_')
 
-            lang = lcode2lang[lcode]
+            lang = lcode2lang[lcode] if lcode != 'no' else lcode2lang[shothand]
             if lcode == 'zh':
                 lang = 'ChineseT'
             wordvec_file = os.path.join(wordvec_dir, lang, '{}.vectors.xz'.format(lcode))
--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=jag-lo --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=5120
+#SBATCH --mem=10g
 #SBATCH --gres=gpu:1
 #SBATCH --output=/sailhome/pengqi/logs/slurm-%j.out
 #SBATCH --mail-user=pengqi@cs.stanford.edu
@@ -16,7 +16,7 @@ echo "SLURMTMPDIR="$SLURMTMPDIR
 echo "working directory = "$SLURM_SUBMIT_DIR
 
 outputprefix=$1
-if [[ "$outputprefix" == "tokenize" || "$outputprefix" == "mwt" || "$outputprefix" == "lemma" ]]; then
+if [[ "$outputprefix" == "tokenize" || "$outputprefix" == "mwt" || "$outputprefix" == "lemma" || "$outputprefix" == "pos" || "$outputprefix" == "depparse" ]]; then
     outputprefix=""
 else
     shift
