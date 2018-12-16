030f4ee79ed3e4a09f6ff3cb1abfe7eabc44d9b2
J38 <jebolton@stanford.edu>
2018-12-16 00:11:07 -0800

    training scripts

---
+++ scripts/prep_pos_data.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+source scripts/config.sh
+treebank=$1
+shift
+UDPIPEBASE=$UDBASE/UDPipe_out
+DATADIR=data/pos
+short=`bash scripts/treebank_to_shorthand.sh ud $treebank`
+lang=`echo $short | sed -e 's#_.*##g'`
+
+if [ -d "$UDBASE/${treebank}_XV" ]; then
+    src_treebank="${treebank}_XV"
+    src_short="${short}_xv"
+else
+    src_treebank=$treebank
+    src_short=$short
+fi
+
+train_conllu=$UDBASE/$src_treebank/${src_short}-ud-train.conllu
+dev_conllu=$UDBASE/$src_treebank/${src_short}-ud-dev.conllu # gold dev
+dev_gold_conllu=$UDBASE/$src_treebank/${src_short}-ud-dev.conllu
+
+#dev_conllu=$UDPIPEBASE/${short}-dev-pred-udpipe.conllu
+train_in_file=$DATADIR/${short}.train.in.conllu
+dev_in_file=$DATADIR/${short}.dev.in.conllu
+dev_gold_file=$DATADIR/${short}.dev.gold.conllu
+# copy conllu file if exists; otherwise create empty files
+if [ -e $train_conllu ]; then
+    cp $train_conllu $train_in_file
+else
+    touch $train_in_file
+fi
+
+if [ -e $dev_conllu ]; then
+    cp $dev_conllu $dev_in_file
+else
+    touch $dev_in_file
+fi
+
+if [ -e $dev_gold_conllu ]; then
+    cp $dev_gold_conllu $dev_gold_file
+else
+    touch $dev_gold_file
+fi
---
+++ scripts/run_mwt.sh
@@ -0,0 +1,36 @@
+source scripts/config.sh
+
+outputprefix=$1
+if [[ "$outputprefix" == "UD_"* ]]; then
+    outputprefix=""
+else
+    shift
+fi
+treebank=$1
+shift
+gpu=$1
+shift
+short=`bash scripts/treebank_to_shorthand.sh ud $treebank`
+lang=`echo $short | sed -e 's#_.*##g'`
+args=$@
+DATADIR=data/mwt
+
+train_file=${DATADIR}/${short}.train.in.conllu
+eval_file=${DATADIR}/${short}.dev.in.conllu
+output_file=${DATADIR}/${short}.dev.${outputprefix}pred.conllu
+gold_file=${DATADIR}/${short}.dev.gold.conllu
+
+if [ ! -e $train_file ]; then
+    bash scripts/prep_mwt_data.sh $treebank
+fi
+
+dec_len=$(python -c "from math import ceil; print(ceil($(python stanfordnlp/utils/max_mwt_length.py data/tokenize/${short}-ud-train-mwt.json data/tokenize/${short}-ud-dev-mwt.json) * 1.1 + 1))")
+
+echo "Running $args..."
+CUDA_VISIBLE_DEVICES=$gpu python -m stanfordnlp.models.mwt_expander --train_file $train_file --eval_file $eval_file \
+    --output_file $output_file --gold_file $gold_file --lang $lang --shorthand $short --mode train --max_dec_len $dec_len --save_dir ${outputprefix}saved_models/mwt $args
+CUDA_VISIBLE_DEVICES=$gpu python -m stanfordnlp.models.mwt_expander --eval_file $eval_file \
+    --output_file $output_file --gold_file $gold_file --lang $lang --shorthand $short --mode predict --save_dir ${outputprefix}saved_models/mwt $args
+results=`python stanfordnlp/utils/conll18_ud_eval.py -v $gold_file $output_file | head -5 | tail -n+5 | awk '{print $7}'`
+echo $results $args >> ${DATADIR}/${short}.${outputprefix}results
+echo $short $results $args
