08cc405395bda2a3542ab4ca47180d2b3842afb1
J38 <jebolton@stanford.edu>
2018-12-16 20:35:52 -0800

    update to latest code

--- stanfordnlp/models/depparse/data.py
+++ stanfordnlp/models/depparse/data.py
@@ -136,7 +136,7 @@ class DataLoader:
 
         if not self.eval:
             # sort sentences (roughly) by length for better memory utilization
-            data = sorted(data, key = lambda x: len(x[0]) + random.random() * 5)
+            data = sorted(data, key = lambda x: len(x[0]), reverse=random.random() > .5)
 
         current = []
         currentlen = 0
