7409c056c5a7f1363b0c1d47c2bc207442cf5815
J38 <jebolton@stanford.edu>
2019-01-14 00:48:34 -0800

    fix merge

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,11 @@
-<<<<<<< HEAD
-export UDBASE=/u/nlp/data/dependency_treebanks/CoNLL18
-=======
+# set UDBASE to the location of CoNLL18 folder
 # export UDBASE=/path/to/CoNLL18
->>>>>>> UD-from-scratch/master
+
+# link to external resources
+#if [ ! -e extern_data/CoNLL18 ]; then
+    #ln -s /path/to/CoNLL18 extern_data/CoNLL18
+#fi
+
+#if [ ! -e extern_data/word2vec ]; then
+    #ln -s /path/to/word2vec extern_data/word2vec
+#fi
