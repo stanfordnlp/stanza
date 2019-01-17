ad876fbd4ef6a8f95eef0bba7239bdfda5f2cef6
J38 <jebolton@stanford.edu>
2019-01-17 01:24:40 -0800

    reduce version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='0.1.1',
 
     description='Official Stanford NLP Python Library',
     long_description=long_description,
