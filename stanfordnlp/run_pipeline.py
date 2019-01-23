28f7be306332f7a3a3d96842356f35b25830337f
J38 <jebolton@stanford.edu>
2019-01-23 00:46:49 -0800

    code cleanup for run_pipeline

--- stanfordnlp/run_pipeline.py
+++ stanfordnlp/run_pipeline.py
@@ -5,20 +5,21 @@ script for running full pipeline on command line
 import argparse
 import os
 
-from stanfordnlp import download, Document, Pipeline
+from stanfordnlp import download, Pipeline
 from stanfordnlp.utils.resources import default_treebanks, DEFAULT_MODEL_DIR
 
 
 if __name__ == '__main__':
     # get arguments
-    # determine home directory
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--models_dir', help='location of models files | default: ~/stanfordnlp_data', 
+    parser.add_argument('-d', '--models_dir', help='location of models files | default: ~/stanfordnlp_resources', 
                         default=DEFAULT_MODEL_DIR)
-    parser.add_argument('-l', '--language', help='language of text | default: en_ewt', default='en_ewt')
+    parser.add_argument('-l', '--language', help='language of text | default: en', default='en')
     parser.add_argument('text_file')
     args = parser.parse_args()
+    # set output file path
     output_file_path = args.text_file+'.out'
+    # map language code to treebank shorthand
     treebank_shorthand = default_treebanks[args.language]
     # check for models
     print('checking for models...')
@@ -28,12 +29,9 @@ if __name__ == '__main__':
         download(args.language, resource_dir=args.models_dir)
     # set up pipeline
     pipeline = Pipeline(lang=args.language, models_dir=args.models_dir)
-    # run process
-    # load input text
-    input_text = open(args.text_file).read()
     # build document
     print('running pipeline...')
-    doc = pipeline(input_text)
+    doc = pipeline(open(args.text_file).read())
     # write conll to file
     doc.write_conll_to_file(output_file_path)
     print('done.')
