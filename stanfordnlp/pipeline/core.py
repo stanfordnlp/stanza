513bd4071fe76eb01419dde68d4ccc903cc2840e
J38 <jebolton@stanford.edu>
2019-01-23 04:17:06 -0800

    remove magic strings from pipeline/core.py, improve code quality of processor building

--- stanfordnlp/pipeline/core.py
+++ stanfordnlp/pipeline/core.py
@@ -10,9 +10,15 @@ from stanfordnlp.pipeline.lemma_processor import LemmaProcessor
 from stanfordnlp.pipeline.depparse_processor import DepparseProcessor
 from stanfordnlp.utils.resources import DEFAULT_MODEL_DIR, default_treebanks, mwt_languages, build_default_config
 
+DEFAULT_PROCESSORS_LIST = 'tokenize,mwt,pos,lemma,depparse'
+
+NAME_TO_PROCESSOR_CLASS = {'tokenize': TokenizeProcessor, 'mwt': MWTProcessor, 'pos': POSProcessor,
+                           'lemma': LemmaProcessor, 'depparse': DepparseProcessor}
+
+
 class Pipeline:
 
-    def __init__(self, processors='tokenize,mwt,lemma,pos,depparse', lang='en', models_dir=DEFAULT_MODEL_DIR, shorthand=None, **kwargs):
+    def __init__(self, processors=DEFAULT_PROCESSORS_LIST, lang='en', models_dir=DEFAULT_MODEL_DIR, shorthand=None, **kwargs):
         shorthand = default_treebanks[lang] if shorthand is None else shorthand
         config = build_default_config(shorthand, models_dir)
         config.update(kwargs)
@@ -24,36 +30,15 @@ class Pipeline:
         self.processor_names = self.config['processors'].split(',')
         self.processors = {'tokenize': None, 'mwt': None, 'lemma': None, 'pos': None, 'depparse': None}
         # set up processors
-        if 'tokenize' in self.processor_names:
-            print('loading tokenizer...')
-            tokenize_config = self.filter_config('tokenize', self.config)
-            print('with settings')
-            print(tokenize_config)
-            self.processors['tokenize'] = TokenizeProcessor(config=tokenize_config)
-        if 'mwt' in self.processor_names and self.config['shorthand'] in mwt_languages:
-            mwt_config = self.filter_config('mwt', self.config)
-            print('loading mwt expander...')
-            print('with settings')
-            print(mwt_config)
-            self.processors['mwt'] = MWTProcessor(config=mwt_config)
-        if 'pos' in self.processor_names:
-            pos_config = self.filter_config('pos', self.config)
-            print('loading part of speech tagger...')
-            print('with settings')
-            print(pos_config)
-            self.processors['pos'] = POSProcessor(config=pos_config)
-        if 'lemma' in self.processor_names:
-            lemma_config = self.filter_config('lemma', self.config)
-            print('loading lemmatizer...')
-            print('with settings')
-            print(lemma_config)
-            self.processors['lemma'] = LemmaProcessor(config=lemma_config)
-        if 'depparse' in self.processor_names:
-            depparse_config = self.filter_config('depparse', self.config)
-            print('loading dependency parser...')
-            print('with settings')
-            print(depparse_config)
-            self.processors['depparse'] = DepparseProcessor(config=depparse_config)
+        for processor_name in self.processor_names:
+            if processor_name == 'mwt' and self.config['shorthand'] not in mwt_languages:
+                continue
+            print('---')
+            print('loading: '+processor_name)
+            curr_processor_config = self.filter_config(processor_name, self.config)
+            print('with settings: ')
+            print(curr_processor_config)
+            self.processors[processor_name] = NAME_TO_PROCESSOR_CLASS[processor_name](config=curr_processor_config)
         print("done loading processors!")
 
     def filter_config(self, prefix, config_dict):
@@ -65,7 +50,7 @@ class Pipeline:
 
     def process(self, doc):
         # run the pipeline
-        for processor_name in ['tokenize', 'mwt', 'pos', 'lemma', 'depparse']:
+        for processor_name in self.processor_names:
             if self.processors[processor_name] is not None:
                 self.processors[processor_name].process(doc)
         doc.load_annotations()
