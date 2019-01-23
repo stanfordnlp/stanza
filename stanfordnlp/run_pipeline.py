04382986f977de4f7bc84bf70ef62915ed0ef2cf
J38 <jebolton@stanford.edu>
2019-01-23 00:36:14 -0800

    update run_pipeline to work with recent changes

--- stanfordnlp/run_pipeline.py
+++ stanfordnlp/run_pipeline.py
@@ -5,48 +5,35 @@ script for running full pipeline on command line
 import argparse
 import os
 
-from pathlib import Path
 from stanfordnlp import download, Document, Pipeline
-from stanfordnlp.utils.resources import build_default_config, load_config
+from stanfordnlp.utils.resources import default_treebanks, DEFAULT_MODEL_DIR
 
 
 if __name__ == '__main__':
     # get arguments
     # determine home directory
-    home_dir = str(Path.home())
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', help='pipeline config file | default: None', default=None)
     parser.add_argument('-d', '--models_dir', help='location of models files | default: ~/stanfordnlp_data', 
-                        default=home_dir+'/stanfordnlp_data')
+                        default=DEFAULT_MODEL_DIR)
     parser.add_argument('-l', '--language', help='language of text | default: en_ewt', default='en_ewt')
     parser.add_argument('text_file')
     args = parser.parse_args()
-    # set up output file
     output_file_path = args.text_file+'.out'
+    treebank_shorthand = default_treebanks[args.language]
     # check for models
     print('checking for models...')
-    lang_models_dir = '%s/%s_models' % (args.models_dir,args.language)
+    lang_models_dir = '%s/%s_models' % (args.models_dir, treebank_shorthand)
     if not os.path.exists(lang_models_dir):
         print('could not find: '+lang_models_dir)
         download(args.language, resource_dir=args.models_dir)
     # set up pipeline
-    if args.config is not None:
-        print('loading pipeline configs from: '+args.config)
-        pipeline_config = load_config(args.config)
-    else:
-        print('using default pipeline configs for: '+args.language)
-        pipeline_config = build_default_config(args.language, args.models_dir)
-    print('using following config for pipeline: ')
-    print(pipeline_config)
-    pipeline = Pipeline(config=pipeline_config)
+    pipeline = Pipeline(lang=args.language, models_dir=args.models_dir)
     # run process
     # load input text
     input_text = open(args.text_file).read()
     # build document
-    doc = Document(input_text)
-    # run pipeline 
     print('running pipeline...')
-    pipeline.process(doc)
+    doc = pipeline(input_text)
     # write conll to file
     doc.write_conll_to_file(output_file_path)
     print('done.')
