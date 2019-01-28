b382689d27f07710b7983e86fe06a4d4161724f5
J38 <jebolton@stanford.edu>
2019-01-27 22:45:59 -0800

    remove annotator stuff for now

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
