f12e6a7e2f2eda47dfe95b205ff0fa56d76ef7d9
J38 <jebolton@stanford.edu>
2019-02-19 23:09:46 -0800

    fix version number

--- stanfordnlp/__init__.py
+++ stanfordnlp/__init__.py
@@ -2,4 +2,4 @@ from stanfordnlp.pipeline.core import Pipeline
 from stanfordnlp.pipeline.doc import Document
 from stanfordnlp.utils.resources import download
 
-__version__='0.1.0'
+__version__='0.1.1'
