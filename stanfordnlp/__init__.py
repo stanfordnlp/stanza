eeb61569fe5f3d0feda6fcdf7b1ffdfa96ef654c
J38 <jebolton@stanford.edu>
2019-03-02 21:25:59 -0800

    prepare for version 0.1.3

--- stanfordnlp/__init__.py
+++ stanfordnlp/__init__.py
@@ -2,4 +2,4 @@ from stanfordnlp.pipeline.core import Pipeline
 from stanfordnlp.pipeline.doc import Document
 from stanfordnlp.utils.resources import download
 
-__version__='0.1.2'
+__version__='0.1.3'
