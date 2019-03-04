f6123a8a6c642a019577b0310e72d31f3b065ae5
J38 <jebolton@stanford.edu>
2019-03-03 23:25:36 -0800

    add safe_rm

--- tests/__init__.py
+++ tests/__init__.py
@@ -34,3 +34,56 @@ def test_mode(tm):
         pytest.skip(f'Test module: test_run_pipeline only runs in FULL mode (set {TEST_MODE_VAR} to {test_mode})',
                     allow_module_level=True)
 
+
+# langauge resources
+LANGUAGE_RESOURCES = {}
+
+# French resources
+FR_KEY = 'fr'
+# regression file paths
+FR_TEST_IN = f'{TEST_WORKING_DIR}/in/fr_gsd.test.txt'
+FR_TEST_OUT = f'{TEST_WORKING_DIR}/out/fr_gsd.test.txt.out'
+FR_TEST_GOLD_OUT = f'{TEST_WORKING_DIR}/out/fr_gsd.test.txt.out.gold'
+# models
+FR_MODELS_DIR = f'{TEST_WORKING_DIR}/fr_gsd_models'
+FR_TOKENIZE_MODEL = "fr_gsd_tokenizer.pt"
+FR_MWT_MODEL = "fr_gsd_mwt_expander.pt"
+FR_POS_MODEL = "fr_gsd_tagger.pt"
+FR_POS_PRETRAIN = "fr_gsd.pretrain.pt"
+FR_LEMMA_MODEL = "fr_gsd_lemmatizer.pt"
+FR_DEPPARSE_MODEL = "fr_gsd_parser.pt"
+FR_DEPPARSE_PRETRAIN = "fr_gsd.pretrain.pt"
+ALL_FR_MODELS = [f'{FR_MODELS_DIR}/{fr_m}' for fr_m in [FR_TOKENIZE_MODEL, FR_MWT_MODEL, FR_POS_MODEL, FR_POS_PRETRAIN,
+                                                        FR_LEMMA_MODEL, FR_DEPPARSE_MODEL, FR_DEPPARSE_PRETRAIN]]
+
+
+# utils for clean up
+# only allow removal of dirs/files in this approved list
+REMOVABLE_PATHS = ['fr_gsd_models', 'fr_gsd_tokenizer.pt', 'fr_gsd_mwt_expander.pt', 'fr_gsd_tagger.pt',
+                   'fr_gsd.pretrain.pt', 'fr_gsd_lemmatizer.pt', 'fr_gsd_parser.pt', 'fr_gsd.test.txt',
+                   'fr_gsd.test.txt.out', 'fr_gsd.test.txt.out.gold']
+
+
+def safe_rm(path_to_rm):
+    """
+    Safely remove a directory of files or a file
+    1.) check path exists
+    2.) only remove things on approved list REMOVABLE_PATHS
+    3.) assert no longer exists
+    """
+    # handle directory
+    if os.path.isdir(path_to_rm):
+        files_to_rm = [f'{path_to_rm}/{fname}' for fname in os.listdir(path_to_rm)]
+        dir_to_rm = path_to_rm
+    else:
+        files_to_rm = [path_to_rm]
+        dir_to_rm = None
+    # clear out files
+    for file_to_rm in files_to_rm:
+        if os.path.isfile(file_to_rm) and os.path.basename(file_to_rm) in REMOVABLE_PATHS:
+            os.remove(file_to_rm)
+            assert not os.path.exists(file_to_rm), f'Error removing: {file_to_rm}'
+    # clear out directory
+    if os.path.isdir(dir_to_rm):
+        os.rmdir(dir_to_rm)
+        assert not os.path.exists(dir_to_rm), f'Error removing: {dir_to_rm}'
--- tests/test_run_pipeline.py
+++ tests/test_run_pipeline.py
@@ -8,26 +8,8 @@ import subprocess
 from datetime import datetime
 from tests import *
 
-# check testing environment, only run these tests in FULL mode
+# only run this test if TEST_MODE = FULL
 test_mode(TEST_MODE_FULL)
-
-# French pipeline paths
-# files
-FR_TEST_IN = f'{TEST_WORKING_DIR}/in/fr_gsd.test.txt'
-FR_TEST_OUT = f'{TEST_WORKING_DIR}/out/fr_gsd.test.txt.out'
-FR_TEST_GOLD_OUT = f'{TEST_WORKING_DIR}/out/fr_gsd.test.txt.out.gold'
-# models
-FR_MODELS_DIR = f'{TEST_WORKING_DIR}/fr_gsd_models'
-FR_TOKENIZE_MODEL = "fr_gsd_tokenizer.pt"
-FR_MWT_MODEL = "fr_gsd_mwt_expander.pt"
-FR_POS_MODEL = "fr_gsd_tagger.pt"
-FR_POS_PRETRAIN = "fr_gsd.pretrain.pt"
-FR_LEMMA_MODEL = "fr_gsd_lemmatizer.pt"
-FR_DEPPARSE_MODEL = "fr_gsd_parser.pt"
-FR_DEPPARSE_PRETRAIN = "fr_gsd.pretrain.pt"
-ALL_FRENCH_MODELS = [f'{FR_MODELS_DIR}/{fr_m}' for fr_m in [FR_TOKENIZE_MODEL, FR_MWT_MODEL, FR_POS_MODEL,
-                                                            FR_POS_PRETRAIN, FR_LEMMA_MODEL, FR_DEPPARSE_MODEL,
-                                                            FR_DEPPARSE_PRETRAIN]]
 
 
 def test_fr_pipeline():
@@ -35,27 +17,17 @@ def test_fr_pipeline():
     assert os.path.exists(FR_TEST_IN), f'Missing test input file: {FR_TEST_IN}'
     assert os.path.exists(FR_TEST_GOLD_OUT), f'Missing test gold output file: {FR_TEST_GOLD_OUT}'
     # verify models not downloaded and output file doesn't exist
-    if os.path.exists(FR_TEST_OUT):
-        os.remove(FR_TEST_OUT)
-    assert not os.path.exists(FR_TEST_OUT), f'Error removing: {FR_TEST_OUT}'
-    for fr_model in ALL_FRENCH_MODELS:
-        if os.path.exists(fr_model):
-            os.remove(fr_model)
-        assert not os.path.exists(fr_model), f'Error removing: {fr_model}'
-    if os.path.exists(FR_MODELS_DIR):
-        os.rmdir(FR_MODELS_DIR)
-    assert not os.path.exists(FR_MODELS_DIR), f'Error removing: {FR_MODELS_DIR}'
+    safe_rm(FR_TEST_OUT)
+    safe_rm(FR_MODELS_DIR)
     # run french pipeline command and check results
     fr_pipeline_cmd = \
         f"python -m stanfordnlp.run_pipeline -l fr -d {TEST_WORKING_DIR} --force-download -o {FR_TEST_OUT} {FR_TEST_IN}"
     subprocess.call(fr_pipeline_cmd, shell=True)
     assert open(FR_TEST_GOLD_OUT).read() == open(FR_TEST_OUT).read(), f'Test failure: output does not match gold'
     # cleanup
+    # log this test run's final output
     if os.path.exists(FR_TEST_OUT):
         curr_timestamp = re.sub(' ', '-', str(datetime.now()))
         os.rename(FR_TEST_OUT, f'{FR_TEST_OUT}-{curr_timestamp}')
-    for fr_model in ALL_FRENCH_MODELS:
-        if os.path.exists(fr_model):
-            os.remove(fr_model)
-    if os.path.exists(FR_MODELS_DIR):
-        os.rmdir(FR_MODELS_DIR)
+    safe_rm(FR_MODELS_DIR)
+
