007ca0435c7c30a77f34ee764cedead7a7b65912
J38 <jebolton@stanford.edu>
2019-04-30 21:09:16 -0700

    fix fixture issue

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,26 +15,26 @@ MAX_REQUEST_ATTEMPTS = 5
 
 def setup_module(module):
     """ Make an initital request to the server """
-    client.annotate(TEXT)
+    corenlp_client().annotate(TEXT)
 
 
 def teardown_module(module):
     """ Stop the client at the end """
-    client.stop()
+    corenlp_client().stop()
 
 
 @pytest.fixture(scope="module")
-def client():
+def corenlp_client():
     """ Client to run tests on """
-    corenlp_client = corenlp.CoreNLPClient(annotators='tokenize,ssplit,pos,lemma,ner,depparse',
+    client = corenlp.CoreNLPClient(annotators='tokenize,ssplit,pos,lemma,ner,depparse',
                                    server_id='stanfordnlp_main_test_server')
-    return corenlp_client
+    return client
 
 
-def test_connect():
-    client.ensure_alive()
-    assert client.is_active
-    assert client.is_alive()
+def test_connect(corenlp_client):
+    corenlp_client.ensure_alive()
+    assert corenlp_client.is_active
+    assert corenlp_client.is_alive()
 
 
 def test_context_manager():
@@ -43,20 +43,20 @@ def test_context_manager():
         assert corenlp.to_text(ann.sentence[0]) == TEXT[:-1]
 
 
-def test_annotate():
-    ann = client.annotate(TEXT)
+def test_annotate(corenlp_client):
+    ann = corenlp_client.annotate(TEXT)
     assert corenlp.to_text(ann.sentence[0]) == TEXT[:-1]
 
 
-def test_update():
-    ann = client.annotate(TEXT)
-    ann = client.update(ann)
+def test_update(corenlp_client):
+    ann = corenlp_client.annotate(TEXT)
+    ann = corenlp_client.update(ann)
     assert corenlp.to_text(ann.sentence[0]) == TEXT[:-1]
 
 
-def test_tokensregex():
+def test_tokensregex(corenlp_client):
     pattern = '([ner: PERSON]+) /wrote/ /an?/ []{0,3} /sentence|article/'
-    matches = client.tokensregex(TEXT, pattern)
+    matches = corenlp_client.tokensregex(TEXT, pattern)
     assert len(matches["sentences"]) == 1
     assert matches["sentences"][0]["length"] == 1
     assert matches == {
@@ -76,7 +76,7 @@ def test_tokensregex():
 
 def test_semgrex():
     pattern = '{word:wrote} >nsubj {}=subject >dobj {}=object'
-    matches = client.semgrex(TEXT, pattern, to_words=True)
+    matches = corenlp_client.semgrex(TEXT, pattern, to_words=True)
     assert matches == [
         {
             "text": "wrote",
