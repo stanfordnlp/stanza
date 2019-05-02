49056a5aa7d3e45f1591f5225ea738108cede30b
J38 <jebolton@stanford.edu>
2019-05-02 05:27:30 -0700

    more tests

--- tests/test_server_start.py
+++ tests/test_server_start.py
@@ -47,6 +47,60 @@ Tokens:
 [Text=. CharacterOffsetBegin=29 CharacterOffsetEnd=30 PartOfSpeech=.]
 """.strip()
 
+GERMAN_DOC = "Angela Merkel ist seit 2005 Bundeskanzlerin der Bundesrepublik Deutschland."
+
+GERMAN_FULL_PROPS_GOLD = """
+Sentence #1 (10 tokens):
+Angela Merkel ist seit 2005 Bundeskanzlerin der Bundesrepublik Deutschland.
+
+Tokens:
+[Text=Angela CharacterOffsetBegin=0 CharacterOffsetEnd=6 PartOfSpeech=NE Lemma=angela NamedEntityTag=PERSON]
+[Text=Merkel CharacterOffsetBegin=7 CharacterOffsetEnd=13 PartOfSpeech=NE Lemma=merkel NamedEntityTag=PERSON]
+[Text=ist CharacterOffsetBegin=14 CharacterOffsetEnd=17 PartOfSpeech=VAFIN Lemma=ist NamedEntityTag=O]
+[Text=seit CharacterOffsetBegin=18 CharacterOffsetEnd=22 PartOfSpeech=APPR Lemma=seit NamedEntityTag=O]
+[Text=2005 CharacterOffsetBegin=23 CharacterOffsetEnd=27 PartOfSpeech=CARD Lemma=2005 NamedEntityTag=O]
+[Text=Bundeskanzlerin CharacterOffsetBegin=28 CharacterOffsetEnd=43 PartOfSpeech=NN Lemma=bundeskanzlerin NamedEntityTag=O]
+[Text=der CharacterOffsetBegin=44 CharacterOffsetEnd=47 PartOfSpeech=ART Lemma=der NamedEntityTag=O]
+[Text=Bundesrepublik CharacterOffsetBegin=48 CharacterOffsetEnd=62 PartOfSpeech=NN Lemma=bundesrepublik NamedEntityTag=LOCATION]
+[Text=Deutschland CharacterOffsetBegin=63 CharacterOffsetEnd=74 PartOfSpeech=NE Lemma=deutschland NamedEntityTag=LOCATION]
+[Text=. CharacterOffsetBegin=74 CharacterOffsetEnd=75 PartOfSpeech=$. Lemma=. NamedEntityTag=O]
+
+Constituency parse: 
+(ROOT
+  (S
+    (MPN (NE Angela) (NE Merkel))
+    (VAFIN ist)
+    (PP (APPR seit) (CARD 2005) (NN Bundeskanzlerin)
+      (NP (ART der) (NN Bundesrepublik) (NE Deutschland)))
+    ($. .)))
+
+
+Extracted the following NER entity mentions:
+Angela Merkel	PERSON
+Bundesrepublik Deutschland	LOCATION
+""".strip()
+
+
+GERMAN_SMALL_PROPS = {'annotators': 'tokenize,ssplit,pos', 'tokenize.language': 'de',
+                      'pos.model': 'edu/stanford/nlp/models/pos-tagger/german/german-hgc.tagger'}
+
+GERMAN_SMALL_PROPS_GOLD = """
+Sentence #1 (10 tokens):
+Angela Merkel ist seit 2005 Bundeskanzlerin der Bundesrepublik Deutschland.
+
+Tokens:
+[Text=Angela CharacterOffsetBegin=0 CharacterOffsetEnd=6 PartOfSpeech=NE]
+[Text=Merkel CharacterOffsetBegin=7 CharacterOffsetEnd=13 PartOfSpeech=NE]
+[Text=ist CharacterOffsetBegin=14 CharacterOffsetEnd=17 PartOfSpeech=VAFIN]
+[Text=seit CharacterOffsetBegin=18 CharacterOffsetEnd=22 PartOfSpeech=APPR]
+[Text=2005 CharacterOffsetBegin=23 CharacterOffsetEnd=27 PartOfSpeech=CARD]
+[Text=Bundeskanzlerin CharacterOffsetBegin=28 CharacterOffsetEnd=43 PartOfSpeech=NN]
+[Text=der CharacterOffsetBegin=44 CharacterOffsetEnd=47 PartOfSpeech=ART]
+[Text=Bundesrepublik CharacterOffsetBegin=48 CharacterOffsetEnd=62 PartOfSpeech=NN]
+[Text=Deutschland CharacterOffsetBegin=63 CharacterOffsetEnd=74 PartOfSpeech=NE]
+[Text=. CharacterOffsetBegin=74 CharacterOffsetEnd=75 PartOfSpeech=$.]
+""".strip()
+
 
 def annotate_and_time(client, text, properties={}):
     """ Submit an annotation request and return how long it took """
@@ -71,4 +125,19 @@ def test_props_file():
     with corenlp.CoreNLPClient(properties=SERVER_TEST_PROPS, server_id='test_server_start_props_file') as client:
         ann = client.annotate(EN_DOC, output_format="text")
         assert ann.strip() == EN_PROPS_FILE_GOLD
+
+
+def test_lang_start():
+    """ Test starting the server with a Stanford CoreNLP language name """
+    with corenlp.CoreNLPClient(properties='german', server_id='test_server_start_lang_name') as client:
+        ann = client.annotate(GERMAN_DOC, output_format='text')
+        assert ann.strip() == GERMAN_FULL_PROPS_GOLD
+
+
+def test_python_dict():
+    """ Test starting the server with a Python dictionary as default properties """
+    with corenlp.CoreNLPClient(properties=GERMAN_SMALL_PROPS, server_id='test_server_start_python_dict') as client:
+        ann = client.annotate(GERMAN_DOC, output_format='text')
+        assert ann.strip() == GERMAN_SMALL_PROPS_GOLD
+
 
