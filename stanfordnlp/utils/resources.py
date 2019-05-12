51560d5b2c2c2a97c771c91f2b7db45df5e0704a
J38 <jebolton@stanford.edu>
2019-05-11 21:24:13 -0700

    small changes to download url

--- stanfordnlp/utils/resources.py
+++ stanfordnlp/utils/resources.py
@@ -4,8 +4,6 @@ utilities for getting resources
 
 import os
 import requests
-import sys
-import urllib.request
 import zipfile
 
 from tqdm import tqdm
@@ -13,7 +11,8 @@ from pathlib import Path
 
 # set home dir for default
 HOME_DIR = str(Path.home())
-DEFAULT_MODEL_DIR = os.path.join(HOME_DIR,'stanfordnlp_resources')
+DEFAULT_MODEL_DIR = os.path.join(HOME_DIR, 'stanfordnlp_resources')
+DEFAULT_MODELS_URL = 'http://nlp.stanford.edu/software/stanfordnlp_models/latest'
 
 # list of language shorthands
 conll_shorthands = ['af_afribooms', 'ar_padt', 'bg_btb', 'bxr_bdt', 'ca_ancora', 'cs_cac', 'cs_fictree', 'cs_pdt', 'cu_proiel', 'da_ddt', 'de_gsd', 'el_gdt', 'en_ewt', 'en_gum', 'en_lines', 'es_ancora', 'et_edt', 'eu_bdt', 'fa_seraji', 'fi_ftb', 'fi_tdt', 'fr_gsd', 'fro_srcmf', 'fr_sequoia', 'fr_spoken', 'ga_idt', 'gl_ctg', 'gl_treegal', 'got_proiel', 'grc_perseus', 'grc_proiel', 'he_htb', 'hi_hdtb', 'hr_set', 'hsb_ufal', 'hu_szeged', 'hy_armtdp', 'id_gsd', 'it_isdt', 'it_postwita', 'ja_gsd', 'kk_ktb', 'kmr_mg', 'ko_gsd', 'ko_kaist', 'la_ittb', 'la_perseus', 'la_proiel', 'lv_lvtb', 'nl_alpino', 'nl_lassysmall', 'no_bokmaal', 'no_nynorsklia', 'no_nynorsk', 'pl_lfg', 'pl_sz', 'pt_bosque', 'ro_rrt', 'ru_syntagrus', 'ru_taiga', 'sk_snk', 'sl_ssj', 'sl_sst', 'sme_giella', 'sr_set', 'sv_lines', 'sv_talbanken', 'tr_imst', 'ug_udt', 'uk_iu', 'ur_udtb', 'vi_vtb', 'zh_gsd']
@@ -91,8 +90,8 @@ def download_ud_model(lang_name, resource_dir=None, should_unzip=True, confirm_i
             os.makedirs(download_dir)
         print('')
         print('Downloading models for: '+lang_name)
-        model_zip_file_name = lang_name+'_models.zip'
-        download_url = 'http://nlp.stanford.edu/software/conll_2018/'+model_zip_file_name
+        model_zip_file_name = f'{lang_name}_models.zip'
+        download_url = f'{DEFAULT_MODELS_URL}/{model_zip_file_name}'
         download_file_path = os.path.join(download_dir, model_zip_file_name)
         print('Download location: '+download_file_path)
 
