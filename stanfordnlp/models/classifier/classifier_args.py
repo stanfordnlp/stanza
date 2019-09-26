dff0b24b9f265702f0ba6c0962eb52339555a406
John Bauer <horatio@gmail.com>
2019-09-26 10:17:33 -0700

    Slightly different use of second particle

--- stanfordnlp/models/classifier/classifier_args.py
+++ stanfordnlp/models/classifier/classifier_args.py
@@ -14,9 +14,8 @@ def add_pretrain_args(parser):
     parser.add_argument('--pretrain_max_vocab', type=int, default=-1)
     parser.add_argument('--wordvec_dir', type=str, default='extern_data/word2vec', help='Directory of word vectors')
     parser.add_argument('--wordvec_type', type=lambda x: WVType[x.upper()], default='word2vec', help='Different vector types have different options, such as google 300d replacing numbers with #')
-    # TODO: is this the right usage of the second particle?
-    # answer: no, the second particle should in some way describe the dataset itself
-    parser.add_argument('--shorthand', type=str, default='en_w2v100', help="Treebank shorthand, eg 'en' for English")
+    # TODO: the second particle should reflect the actual treebank, eg SST for sentiment treebank
+    parser.add_argument('--shorthand', type=str, default='en_ewt', help="Treebank shorthand, eg 'en' for English")
     
 def add_device_args(parser):
     parser.add_argument('--cuda', action='store_true', help='Use CUDA for training/testing', default=torch.cuda.is_available())
