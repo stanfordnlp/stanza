ee98120ed51ff30be9dd8091ce1aae4c496419c3
Maciej Rapacz <maciek.rapacz@gmail.com>
2019-10-04 19:33:39 +0200

    Add tests for introduced depparse preanalyzed text changes

--- stanfordnlp/pipeline/core.py
+++ stanfordnlp/pipeline/core.py
@@ -45,8 +45,9 @@ PROCESSOR_SETTINGS = {
     'depparse': ['batch_size', 'beta2', 'char', 'char_emb_dim', 'char_hidden_dim', 'char_num_layers',
                  'char_rec_dropout', 'composite_deep_biaff_hidden_dim', 'deep_biaff_hidden_dim', 'distance', 'dropout',
                  'eval_interval', 'hidden_dim', 'linearization', 'log_step', 'lr', 'max_grad_norm', 'max_steps',
-                 'max_steps_before_stop', 'num_layers', 'optim', 'pretrain', 'rec_dropout', 'sample_train', 'seed',
-                 'shorthand', 'tag_emb_dim', 'transformed_dim', 'word_dropout', 'word_emb_dim', 'wordvec_dir']
+                 'max_steps_before_stop', 'num_layers', 'optim', 'preanalyzed', 'pretrain', 'rec_dropout',
+                 'sample_train', 'seed', 'shorthand', 'tag_emb_dim', 'transformed_dim', 'word_dropout', 'word_emb_dim',
+                 'wordvec_dir']
 }
 
 PROCESSOR_SETTINGS_LIST = \
@@ -55,7 +56,8 @@ PROCESSOR_SETTINGS_LIST = \
 BOOLEAN_PROCESSOR_SETTINGS = {
     TOKENIZE: ['pretokenized'],
     MWT: ['dict_only'],
-    LEMMA: ['dict_only', 'edit', 'ensemble_dict', 'pos', 'use_identity']
+    LEMMA: ['dict_only', 'edit', 'ensemble_dict', 'pos', 'use_identity'],
+    DEPPARSE: ['preanalyzed']
 }
 
 BOOLEAN_PROCESSOR_SETTINGS_LIST = \
