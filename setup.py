78623c661ae2828c94b1b1bbbb5ebef75c987873
Peng Qi <qipeng@users.noreply.github.com>
2019-12-12 15:22:44 -0800

    torch.uint8 -> torch.bool to get rid of deprecation warning

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'protobuf', 'requests', 'torch>=1.0.0', 'tqdm'],
+    install_requires=['numpy', 'protobuf', 'requests', 'torch>=1.2.0', 'tqdm'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
