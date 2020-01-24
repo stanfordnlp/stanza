e3c2bd45ed5bcd11e49ff92c85291490007c981b
Yuhui Zhang <zyh.thu@gmail.com>
2020-01-23 23:58:13 -0800

    initial version for loading works

--- stanfordnlp/utils/resources.py
+++ stanfordnlp/utils/resources.py
@@ -24,40 +24,63 @@ DEFAULT_DOWNLOAD_VERSION = 'latest'
 DEFAULT_PROCESSORS = "default_processors"
 DEFAULT_DEPENDENCIES = "default_dependencies"
 MD5 = 'md5'
-PIPELINE_NAME = ['tokenizer', 'mwt', 'lemmatizer', 'tagger', 'parser', 'nertagger', 'charlm']
+# PIPELINE_NAME = ['tokenizer', 'mwt', 'lemmatizer', 'tagger', 'parser', 'nertagger', 'charlm']
 
-# list of language shorthands
-conll_shorthands = ['af_afribooms', 'ar_padt', 'bg_btb', 'bxr_bdt', 'ca_ancora', 'cs_cac', 'cs_fictree', 'cs_pdt', 'cu_proiel', 'da_ddt', 'de_gsd', 'el_gdt', 'en_ewt', 'en_gum', 'en_lines', 'es_ancora', 'et_edt', 'eu_bdt', 'fa_seraji', 'fi_ftb', 'fi_tdt', 'fr_gsd', 'fro_srcmf', 'fr_sequoia', 'fr_spoken', 'ga_idt', 'gl_ctg', 'gl_treegal', 'got_proiel', 'grc_perseus', 'grc_proiel', 'he_htb', 'hi_hdtb', 'hr_set', 'hsb_ufal', 'hu_szeged', 'hy_armtdp', 'id_gsd', 'it_isdt', 'it_postwita', 'ja_gsd', 'kk_ktb', 'kmr_mg', 'ko_gsd', 'ko_kaist', 'la_ittb', 'la_perseus', 'la_proiel', 'lv_lvtb', 'nl_alpino', 'nl_lassysmall', 'no_bokmaal', 'no_nynorsklia', 'no_nynorsk', 'pl_lfg', 'pl_sz', 'pt_bosque', 'ro_rrt', 'ru_syntagrus', 'ru_taiga', 'sk_snk', 'sl_ssj', 'sl_sst', 'sme_giella', 'sr_set', 'sv_lines', 'sv_talbanken', 'tr_imst', 'ug_udt', 'uk_iu', 'ur_udtb', 'vi_vtb', 'zh_gsd']
+# # list of language shorthands
+# conll_shorthands = ['af_afribooms', 'ar_padt', 'bg_btb', 'bxr_bdt', 'ca_ancora', 'cs_cac', 'cs_fictree', 'cs_pdt', 'cu_proiel', 'da_ddt', 'de_gsd', 'el_gdt', 'en_ewt', 'en_gum', 'en_lines', 'es_ancora', 'et_edt', 'eu_bdt', 'fa_seraji', 'fi_ftb', 'fi_tdt', 'fr_gsd', 'fro_srcmf', 'fr_sequoia', 'fr_spoken', 'ga_idt', 'gl_ctg', 'gl_treegal', 'got_proiel', 'grc_perseus', 'grc_proiel', 'he_htb', 'hi_hdtb', 'hr_set', 'hsb_ufal', 'hu_szeged', 'hy_armtdp', 'id_gsd', 'it_isdt', 'it_postwita', 'ja_gsd', 'kk_ktb', 'kmr_mg', 'ko_gsd', 'ko_kaist', 'la_ittb', 'la_perseus', 'la_proiel', 'lv_lvtb', 'nl_alpino', 'nl_lassysmall', 'no_bokmaal', 'no_nynorsklia', 'no_nynorsk', 'pl_lfg', 'pl_sz', 'pt_bosque', 'ro_rrt', 'ru_syntagrus', 'ru_taiga', 'sk_snk', 'sl_ssj', 'sl_sst', 'sme_giella', 'sr_set', 'sv_lines', 'sv_talbanken', 'tr_imst', 'ug_udt', 'uk_iu', 'ur_udtb', 'vi_vtb', 'zh_gsd']
 
-# all languages with mwt
-mwt_languages = ['ar_padt', 'ca_ancora', 'cs_cac', 'cs_fictree', 'cs_pdt', 'de_gsd', 'el_gdt', 'es_ancora', 'fa_seraji', 'fi_ftb', 'fr_gsd', 'fr_sequoia', 'gl_ctg', 'gl_treegal', 'he_htb', 'hy_armtdp', 'it_isdt', 'it_postwita', 'kk_ktb', 'pl_sz', 'pt_bosque', 'tr_imst']
+# # all languages with mwt
+# mwt_languages = ['ar_padt', 'ca_ancora', 'cs_cac', 'cs_fictree', 'cs_pdt', 'de_gsd', 'el_gdt', 'es_ancora', 'fa_seraji', 'fi_ftb', 'fr_gsd', 'fr_sequoia', 'gl_ctg', 'gl_treegal', 'he_htb', 'hy_armtdp', 'it_isdt', 'it_postwita', 'kk_ktb', 'pl_sz', 'pt_bosque', 'tr_imst']
 
-# default treebank for languages
-default_treebanks = {'af': 'af_afribooms', 'grc': 'grc_proiel', 'ar': 'ar_padt', 'hy': 'hy_armtdp', 'eu': 'eu_bdt', 'bg': 'bg_btb', 'bxr': 'bxr_bdt', 'ca': 'ca_ancora', 'zh': 'zh_gsd', 'hr': 'hr_set', 'cs': 'cs_pdt', 'da': 'da_ddt', 'nl': 'nl_alpino', 'en': 'en_ewt', 'et': 'et_edt', 'fi': 'fi_tdt', 'fr': 'fr_gsd', 'gl': 'gl_ctg', 'de': 'de_gsd', 'got': 'got_proiel', 'el': 'el_gdt', 'he': 'he_htb', 'hi': 'hi_hdtb', 'hu': 'hu_szeged', 'id': 'id_gsd', 'ga': 'ga_idt', 'it': 'it_isdt', 'ja': 'ja_gsd', 'kk': 'kk_ktb', 'ko': 'ko_kaist', 'kmr': 'kmr_mg', 'la': 'la_ittb', 'lv': 'lv_lvtb', 'sme': 'sme_giella', 'no_bokmaal': 'no_bokmaal', 'no_nynorsk': 'no_nynorsk', 'cu': 'cu_proiel', 'fro': 'fro_srcmf', 'fa': 'fa_seraji', 'pl': 'pl_lfg', 'pt': 'pt_bosque', 'ro': 'ro_rrt', 'ru': 'ru_syntagrus', 'sr': 'sr_set', 'sk': 'sk_snk', 'sl': 'sl_ssj', 'es': 'es_ancora', 'sv': 'sv_talbanken', 'tr': 'tr_imst', 'uk': 'uk_iu', 'hsb': 'hsb_ufal', 'ur': 'ur_udtb', 'ug': 'ug_udt', 'vi': 'vi_vtb'}
+# # default treebank for languages
+# default_treebanks = {'af': 'af_afribooms', 'grc': 'grc_proiel', 'ar': 'ar_padt', 'hy': 'hy_armtdp', 'eu': 'eu_bdt', 'bg': 'bg_btb', 'bxr': 'bxr_bdt', 'ca': 'ca_ancora', 'zh': 'zh_gsd', 'hr': 'hr_set', 'cs': 'cs_pdt', 'da': 'da_ddt', 'nl': 'nl_alpino', 'en': 'en_ewt', 'et': 'et_edt', 'fi': 'fi_tdt', 'fr': 'fr_gsd', 'gl': 'gl_ctg', 'de': 'de_gsd', 'got': 'got_proiel', 'el': 'el_gdt', 'he': 'he_htb', 'hi': 'hi_hdtb', 'hu': 'hu_szeged', 'id': 'id_gsd', 'ga': 'ga_idt', 'it': 'it_isdt', 'ja': 'ja_gsd', 'kk': 'kk_ktb', 'ko': 'ko_kaist', 'kmr': 'kmr_mg', 'la': 'la_ittb', 'lv': 'lv_lvtb', 'sme': 'sme_giella', 'no_bokmaal': 'no_bokmaal', 'no_nynorsk': 'no_nynorsk', 'cu': 'cu_proiel', 'fro': 'fro_srcmf', 'fa': 'fa_seraji', 'pl': 'pl_lfg', 'pt': 'pt_bosque', 'ro': 'ro_rrt', 'ru': 'ru_syntagrus', 'sr': 'sr_set', 'sk': 'sk_snk', 'sl': 'sl_ssj', 'es': 'es_ancora', 'sv': 'sv_talbanken', 'tr': 'tr_imst', 'uk': 'uk_iu', 'hsb': 'hsb_ufal', 'ur': 'ur_udtb', 'ug': 'ug_udt', 'vi': 'vi_vtb'}
 
 # map processor name to file ending
 processor_to_ending = {'tokenize': 'tokenizer', 'mwt': 'mwt_expander', 'pos': 'tagger', 'lemma': 'lemmatizer', 'depparse': 'parser', 'ner': 'nertagger'}
 
+# # given a language and models path, build a default configuration
+# def build_default_config(treebank, models_path):
+#     default_config = {}
+#     if treebank in mwt_languages:
+#         default_config['processors'] = 'tokenize,mwt,pos,lemma,depparse,ner'
+#     else:
+#         default_config['processors'] = 'tokenize,pos,lemma,depparse,ner'
+#     if treebank == 'vi_vtb':
+#         default_config['lemma_use_identity'] = True
+#         default_config['lemma_batch_size'] = 5000
+#     lang, model = treebank.split('_')
+#     print(f'lang: {lang}, model: {model}')
+#     for processor in default_config['processors'].split(','):
+#         model_file_ending = f"{processor_to_ending[processor]}.pt"
+#         default_config[f"{processor}_model_path"] = os.path.join(models_path, lang, processor, model + '.pt')
+#         if processor in ['pos', 'depparse']:
+#             default_config[f"{processor}_pretrain_path"] = os.path.join(models_path, lang, 'pretrain', model + '.pt')
+#         if processor in ['ner']:
+#             default_config[f"{processor}_charlm_forward_file"] = os.path.join(models_path, lang, 'charlm', model + '_forward.pt')
+#             default_config[f"{processor}_charlm_backward_file"] = os.path.join(models_path, lang, 'charlm', model + '_backward.pt')
+#     print(default_config)
+#     return default_config
+
 # given a language and models path, build a default configuration
-def build_default_config(treebank, models_path):
+def build_default_config(resources, lang, dir, load_list):
     default_config = {}
-    if treebank in mwt_languages:
-        default_config['processors'] = 'tokenize,mwt,pos,lemma,depparse,ner'
-    else:
-        default_config['processors'] = 'tokenize,pos,lemma,depparse,ner'
-    if treebank == 'vi_vtb':
+    if lang == 'vi': # <TODO> identical ? treebank == 'vi_vtb':
         default_config['lemma_use_identity'] = True
         default_config['lemma_batch_size'] = 5000
-    lang, model = treebank.split('_')
-    print(f'lang: {lang}, model: {model}')
-    for processor in default_config['processors'].split(','):
-        model_file_ending = f"{processor_to_ending[processor]}.pt"
-        default_config[f"{processor}_model_path"] = os.path.join(models_path, lang, processor, model + '.pt')
-        if processor in ['pos', 'depparse']:
-            default_config[f"{processor}_pretrain_path"] = os.path.join(models_path, lang, 'pretrain', model + '.pt')
-        if processor in ['ner']:
-            default_config[f"{processor}_charlm_forward_file"] = os.path.join(models_path, lang, 'charlm', model + '_forward.pt')
-            default_config[f"{processor}_charlm_backward_file"] = os.path.join(models_path, lang, 'charlm', model + '_backward.pt')
+
+    for item in load_list:
+        processor, model, dependencies = item
+        default_config[f"{processor}_model_path"] = os.path.join(dir, lang, processor, model + '.pt')
+        if not dependencies: continue
+        for dependency in dependencies:
+            dep_processor, dep_model = dependency
+            if dep_processor == 'charlm': # <TODO>: special handle
+                direction = dep_model.split('_')[1]
+                default_config[f"{processor}_{dep_processor}_{direction}_file"] = os.path.join(dir, lang, dep_processor, dep_model + '.pt')
+            else:
+                default_config[f"{processor}_{dep_processor}_path"] = os.path.join(dir, lang, dep_processor, dep_model + '.pt')
+
+    # print(default_config)
     return default_config
 
 def ensure_path(path):
@@ -130,7 +153,7 @@ def maintain_download_list(resources, lang, package, processors):
                     else:
                         logger.debug(f'{key}: {package} is overwritten by {key}: {processors[key]}.')
             if not flag: logger.warning((f'Can not find package: {package}.'))
-    download_list = [(key, value) for key, value in download_list.items()] 
+    download_list = [[key, value] for key, value in download_list.items()] 
     return download_list
 
 
@@ -141,7 +164,7 @@ def add_dependencies(resources, lang, download_list):
         dependencies = default_dependencies.get(key, None)
         dependencies = resources[lang][key][value].get('dependencies', dependencies)
         if dependencies: dependencies_list += [tuple(dependency) for dependency in dependencies]
-    dependencies_list = list(set(dependencies_list))
+    dependencies_list = [list(i) for i in set(dependencies_list)]
     for key, value in dependencies_list:
         logger.debug(f'Find dependency {key}: {value}.')
     download_list += dependencies_list
@@ -163,14 +186,15 @@ def make_table(header, content, column_width=20):
     table_str += '=' * (len_column * column_width + 1) + '\n'
     
     table_str += '|'
-    for item in header: table_str += item.ljust(column_width - 1) + '|'
+    for item in header: table_str += str(item).ljust(column_width - 1) + '|'
     table_str += '\n'
     
     table_str += '-' * (len_column * column_width + 1) + '\n'
     
     for line in content:
         table_str += '|'
-        for item in line: table_str += item.ljust(column_width - 1) + '|'
+        for item in line:
+            table_str += str(item).ljust(column_width - 1) + '|'
         table_str += '\n'
     
     table_str += '=' * (len_column * column_width + 1) + '\n'
