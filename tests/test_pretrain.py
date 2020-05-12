db769bdcb9ef8094a42218f20c0d3735559f2abc
John Bauer <horatio@gmail.com>
2020-05-11 22:01:41 -0700

    Add another test for saving & loading a pretrain

--- tests/test_pretrain.py
+++ tests/test_pretrain.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 import pytest
 import numpy as np
 
@@ -30,3 +33,21 @@ def test_xz_pretrain():
     pt = pretrain.Pretrain(vec_filename=f'{TEST_WORKING_DIR}/in/tiny_emb.xz', save_to_file=False)
     check_pretrain(pt)
 
+def test_resave_pretrain():
+    """
+    Test saving a pretrain and then loading from the existing file
+    """
+    test_pt_file = tempfile.NamedTemporaryFile(dir=f'{TEST_WORKING_DIR}/out', suffix=".pt", delete=False)
+    try:
+        test_pt_file.close()
+        # note that this tests the ability to save a pretrain and the
+        # ability to fall back when the existing pretrain isn't working
+        pt = pretrain.Pretrain(filename=test_pt_file.name,
+                               vec_filename=f'{TEST_WORKING_DIR}/in/tiny_emb.xz')
+        check_pretrain(pt)
+
+        pt2 = pretrain.Pretrain(filename=test_pt_file.name,
+                               vec_filename=f'unban_mox_opal')
+        check_pretrain(pt2)
+    finally:
+        os.unlink(test_pt_file.name)
