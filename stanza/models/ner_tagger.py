8f71931fe37085a7d3fa15bed45eb100168cf854
Yuhui Zhang <zyh.thu@gmail.com>
2020-06-20 15:15:05 -0700

    restore ner_tagger.py

--- stanza/models/ner_tagger.py
+++ stanza/models/ner_tagger.py
@@ -113,7 +113,7 @@ def train(args):
     model_file = args['save_dir'] + '/' + args['save_name'] if args['save_name'] is not None \
             else '{}/{}_nertagger.pt'.format(args['save_dir'], args['shorthand'])
 
-    pretrain_vocab = None
+    pretrain = None
     vocab = None
     trainer = None
     if args['finetune'] and os.path.exists(model_file):
@@ -130,7 +130,6 @@ def train(args):
             vec_file = args['wordvec_file']
         # do not save pretrained embeddings individually
         pretrain = Pretrain(None, vec_file, args['pretrain_max_vocab'], save_to_file=False)
-        pretrain_vocab = pretrain.vocab
 
         if args['charlm']:
             if args['charlm_shorthand'] is None:
@@ -143,10 +142,10 @@ def train(args):
     # load data
     logger.info("Loading data with batch size {}...".format(args['batch_size']))
     train_doc = Document(json.load(open(args['train_file'])))
-    train_batch = DataLoader(train_doc, args['batch_size'], args, pretrain_vocab, vocab=vocab, evaluation=False)
+    train_batch = DataLoader(train_doc, args['batch_size'], args, pretrain, vocab=vocab, evaluation=False)
     vocab = train_batch.vocab
     dev_doc = Document(json.load(open(args['eval_file'])))
-    dev_batch = DataLoader(dev_doc, args['batch_size'], args, pretrain_vocab, vocab=vocab, evaluation=True)
+    dev_batch = DataLoader(dev_doc, args['batch_size'], args, pretrain, vocab=vocab, evaluation=True)
     dev_gold_tags = dev_batch.tags
 
     # skip training if the language does not have training or dev data
