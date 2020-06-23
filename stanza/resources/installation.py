c4a3fb8eacbc279942bcf6665491b7b9ea004072
Yuhui Zhang <zyh.thu@gmail.com>
2020-06-22 19:52:10 -0700

    support downloading models

--- stanza/resources/installation.py
+++ stanza/resources/installation.py
@@ -12,16 +12,18 @@ from stanza.resources.common import HOME_DIR, request_file, unzip, \
 
 logger = logging.getLogger('stanza')
 
-CORENLP_LATEST_URL = os.getenv(
+DEFAULT_CORENLP_URL = os.getenv(
     'CORENLP_URL',
-    "http://nlp.stanford.edu/software/stanford-corenlp-latest.zip"
+    "http://nlp.stanford.edu/software/"
 )
 DEFAULT_CORENLP_DIR = os.getenv(
     'CORENLP_HOME',
     os.path.join(HOME_DIR, 'stanza_corenlp')
 )
 
-def install_corenlp(dir=DEFAULT_CORENLP_DIR, url=CORENLP_LATEST_URL, logging_level='INFO'):
+AVAILABLE_MODELS = set(['arabic', 'chinese', 'english', 'english-kbp', 'french', 'german', 'spanish'])
+
+def install_corenlp(dir=DEFAULT_CORENLP_DIR, url=DEFAULT_CORENLP_URL, model=None, model_version=None, logging_level='INFO'):
     """
     A fully automatic way to install and setting up the CoreNLP library 
     to use the client functionality.
@@ -41,10 +43,9 @@ def install_corenlp(dir=DEFAULT_CORENLP_DIR, url=CORENLP_LATEST_URL, logging_lev
 
     logger.info(f"Installing CoreNLP package into {dir}...")
     # First download the URL package
-    dest_file = os.path.join(dir, 'corenlp.zip')
-    logger.debug(f"Download to destination file: {dest_file}")
+    logger.debug(f"Download to destination file: {os.path.join(dir, 'corenlp.zip')}")
     try:
-        request_file(url, dest_file)
+        request_file(url + 'stanford-corenlp-latest.zip', os.path.join(dir, 'corenlp.zip'))
     except:
         raise Exception(
             "Downloading CoreNLP zip file failed. "
@@ -59,16 +60,30 @@ def install_corenlp(dir=DEFAULT_CORENLP_DIR, url=CORENLP_LATEST_URL, logging_lev
     # e.g., stanford-corenlp-4.0.0. We need some hack around that and move
     # files back into our designated folder
     logger.debug(f"Moving files into the designated folder at: {dir}")
-    corenlp_dirname = get_root_from_zipfile(dest_file)
+    corenlp_dirname = get_root_from_zipfile(os.path.join(dir, 'corenlp.zip'))
     corenlp_dirname = os.path.join(dir, corenlp_dirname)
     for f in os.listdir(corenlp_dirname):
         shutil.move(os.path.join(corenlp_dirname, f), dir)
 
     # Remove original zip and folder
     logger.debug("Removing downloaded zip file...")
-    os.remove(dest_file)
+    os.remove(os.path.join(dir, 'corenlp.zip'))
     shutil.rmtree(corenlp_dirname)
+
+    # Download model
+    if model is not None and model_version is not None:
+        model = model.strip().lower()
+        if model not in AVAILABLE_MODELS:
+            raise KeyError(f'{model} is currently not supported. All the supported models: {list(AVAILABLE_MODELS)}.')
+        try:
+            request_file(url + f'stanford-corenlp-{model_version}-models-{model}.jar', os.path.join(dir, f'stanford-corenlp-{model_version}-models-{model}.jar'))
+        except:
+            raise Exception(
+                "Downloading CoreNLP model file failed. "
+                "Please try manual downloading: https://stanfordnlp.github.io/CoreNLP/."
+            )
 
     # Warn user to set up env
     if dir != DEFAULT_CORENLP_DIR:
         logger.warn(f"For customized downloading path, please set the `CORENLP_HOME` environment variable to the location of the folder: `export CORENLP_HOME={dir}`.")
+
