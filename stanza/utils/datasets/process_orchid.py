af514acfd0123c9d5a7a897360fcb802e3fd2617
John Bauer <horatio@gmail.com>
2020-07-03 16:38:50 -0700

    Refactor a common method

--- stanza/utils/datasets/process_orchid.py
+++ stanza/utils/datasets/process_orchid.py
@@ -27,10 +27,11 @@ test set scores:
 Apparently the random split produced a test set easier than the dev set.
 """
 
-import os
 import random
 import sys
 import xml.etree.ElementTree as ET
+
+from stanza.utils.datasets.process_thai_tokenization import write_dataset
 
 # line "122819" has some error in the tokenization of the musical notation
 # line "209380" is also messed up
@@ -140,59 +141,12 @@ def read_data(input_filename):
     return documents
 
 
-def write_section(output_dir, section, documents):
-    # TODO: no MWT in this dataset?
-    with open(os.path.join(output_dir, 'th_orchid-ud-%s-mwt.json' % section), 'w') as fout:
-        fout.write("[]\n")
-
-    text_out = open(os.path.join(output_dir, 'th_orchid.%s.txt' % section), 'w')
-    label_out = open(os.path.join(output_dir, 'th_orchid-ud-%s.toklabels' % section), 'w')
-    for document in documents:
-        for paragraph in document:
-            for sentence_idx, sentence in enumerate(paragraph):
-                for word_idx, word in enumerate(sentence):
-                    # TODO: split with newlines to make it more readable?
-                    text_out.write(word[0])
-                    for i in range(len(word[0]) - 1):
-                        label_out.write("0")
-                    if word_idx == len(sentence) - 1:
-                        label_out.write("2")
-                    else:
-                        label_out.write("1")
-                    if word[1] and sentence_idx != len(paragraph) - 1:
-                        text_out.write(' ')
-                        label_out.write('0')
-
-            text_out.write("\n\n")
-            label_out.write("\n\n")
-
-    text_out.close()
-    label_out.close()
-
-    with open(os.path.join(output_dir, 'th_orchid.%s.gold.conllu' % section), 'w') as fout:
-        for document in documents:
-            for paragraph in document:
-                for sentence in paragraph:
-                    for word_idx, word in enumerate(sentence):
-                        # SpaceAfter is left blank if there is space after the word
-                        space = '_' if word[1] else 'SpaceAfter=No'
-                        # Note the faked dependency structure: the conll reading code
-                        # needs it even if it isn't being used in any way
-                        fake_dep = 'root' if word_idx == 0 else 'dep'
-                        fout.write('{}\t{}\t_\t_\t_\t_\t{}\t{}\t{}:{}\t{}\n'.format(word_idx+1, word[0], word_idx, fake_dep, word_idx, fake_dep, space))
-                    fout.write('\n')
-
 def main():
+    random.seed(1007)
     input_filename = sys.argv[1]
     output_dir = sys.argv[2]
     documents = read_data(input_filename)
-    random.seed(1007)
-    random.shuffle(documents)
-    num_train = int(len(documents) * 0.8)
-    num_dev = int(len(documents) * 0.1)
-    write_section(output_dir, 'train', documents[:num_train])
-    write_section(output_dir, 'dev', documents[num_train:num_train+num_dev])
-    write_section(output_dir, 'test', documents[num_train+num_dev:])
+    write_dataset(documents, output_dir, "orchid")
 
 
 if __name__ == '__main__':
