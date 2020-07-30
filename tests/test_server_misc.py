73a0390383e54de208fa0c5e3b14083a45ad1234
J38 <jebolton@stanford.edu>
2020-07-30 00:10:28 -0700

    fix test

--- tests/test_server_misc.py
+++ tests/test_server_misc.py
@@ -16,25 +16,26 @@ Sentence #1 (6 tokens):
 Joe Smith lives in California.
 
 Tokens:
-[Text=Joe CharacterOffsetBegin=0 CharacterOffsetEnd=3 PartOfSpeech=NNP Lemma=Joe NamedEntityTag=PERSON]
-[Text=Smith CharacterOffsetBegin=4 CharacterOffsetEnd=9 PartOfSpeech=NNP Lemma=Smith NamedEntityTag=PERSON]
-[Text=lives CharacterOffsetBegin=10 CharacterOffsetEnd=15 PartOfSpeech=VBZ Lemma=live NamedEntityTag=O]
-[Text=in CharacterOffsetBegin=16 CharacterOffsetEnd=18 PartOfSpeech=IN Lemma=in NamedEntityTag=O]
-[Text=California CharacterOffsetBegin=19 CharacterOffsetEnd=29 PartOfSpeech=NNP Lemma=California NamedEntityTag=STATE_OR_PROVINCE]
-[Text=. CharacterOffsetBegin=29 CharacterOffsetEnd=30 PartOfSpeech=. Lemma=. NamedEntityTag=O]
+[Text=Joe CharacterOffsetBegin=0 CharacterOffsetEnd=3 PartOfSpeech=PROPN Lemma=joe NamedEntityTag=PERSON]
+[Text=Smith CharacterOffsetBegin=4 CharacterOffsetEnd=9 PartOfSpeech=PROPN Lemma=smith NamedEntityTag=PERSON]
+[Text=lives CharacterOffsetBegin=10 CharacterOffsetEnd=15 PartOfSpeech=ADJ Lemma=lives NamedEntityTag=O]
+[Text=in CharacterOffsetBegin=16 CharacterOffsetEnd=18 PartOfSpeech=ADP Lemma=in NamedEntityTag=O]
+[Text=California CharacterOffsetBegin=19 CharacterOffsetEnd=29 PartOfSpeech=PROPN Lemma=california NamedEntityTag=STATE_OR_PROVINCE]
+[Text=. CharacterOffsetBegin=29 CharacterOffsetEnd=30 PartOfSpeech=PUNCT Lemma=. NamedEntityTag=O]
 
 Dependency Parse (enhanced plus plus dependencies):
-root(ROOT-0, lives-3)
-compound(Smith-2, Joe-1)
-nsubj(lives-3, Smith-2)
+root(ROOT-0, Joe-1)
+flat(Joe-1, Smith-2)
+amod(Joe-1, lives-3)
 case(California-5, in-4)
-obl(lives-3, California-5)
-punct(lives-3, .-6)
+nmod(lives-3, California-5)
+punct(Joe-1, .-6)
 
 Extracted the following NER entity mentions:
-Joe Smith       PERSON              PERSON:0.9972202681743931
-California      STATE_OR_PROVINCE   LOCATION:0.9990868267559281
+Joe Smith       PERSON  PERSON:0.9989563558707318
+California      STATE_OR_PROVINCE       LOCATION:0.9911262738614187
 """
+
 
 EN_DOC_POS_ONLY_GOLD = """
 Sentence #1 (6 tokens):
@@ -52,16 +53,9 @@ Tokens:
 def test_english_request():
     """ Test case of starting server with Spanish defaults, and then requesting default English properties """
     with corenlp.CoreNLPClient(properties='spanish', server_id='test_english_request') as client:
-        ann = client.annotate(EN_DOC, properties_key='english', output_format='text')
+        ann = client.annotate(EN_DOC, properties='english', output_format='text')
         compare_ignoring_whitespace(ann, EN_DOC_GOLD)
 
-
-
-def test_unknown_properties():
-    """ Test case of starting server with Spanish defaults, and then requesting UNBAN_MOX_OPAL properties """
-    with corenlp.CoreNLPClient(properties='spanish', server_id='test_unknown_properties') as client:
-        with pytest.raises(ValueError):
-            ann = client.annotate(EN_DOC, properties_key='UNBAN_MOX_OPAL', output_format='text')
 
 def test_default_annotators():
     """
