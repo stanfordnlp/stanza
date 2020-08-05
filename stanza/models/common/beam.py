f13288ec095dab272d5f074c8277ee15142bf4d3
John Bauer <horatio@gmail.com>
2020-08-04 17:05:25 -0700

    This is cleaner syntax

--- stanza/models/common/beam.py
+++ stanza/models/common/beam.py
@@ -19,14 +19,6 @@ import stanza.models.common.seq2seq_constant as constant
  Takes care of beams, back pointers, and scores.
 """
 
-# as of torch 1.5, torch.floor_divide is the proper way to divide two
-# integer tensors
-# versions which don't have floor_divide just use x / y instead
-if hasattr(torch, 'floor_divide'):
-    DIVIDE = torch.floor_divide
-else:
-    DIVIDE = lambda x, y: x / y
-
 
 class Beam(object):
     def __init__(self, size, cuda=False):
@@ -91,7 +83,7 @@ class Beam(object):
         # word and beam each score came from
         # bestScoreId is the integer ids, and numWords is the integer length.
         # Need to do integer division
-        prevK = DIVIDE(bestScoresId, numWords)
+        prevK = bestScoresId // numWords
         self.prevKs.append(prevK)
         self.nextYs.append(bestScoresId - prevK * numWords)
         if copy_indices is not None:
