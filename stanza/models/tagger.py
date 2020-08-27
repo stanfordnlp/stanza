559a777731e51cf4150d1b1045e3ba6414b709ba
John Bauer <horatio@gmail.com>
2020-08-27 13:42:09 -0700

    Save instead of going haywire when the dev set is never evaluated

--- stanza/models/tagger.py
+++ stanza/models/tagger.py
@@ -207,8 +207,13 @@ def train(args):
 
     print("Training ended with {} steps.".format(global_step))
 
-    best_f, best_eval = max(dev_score_history)*100, np.argmax(dev_score_history)+1
-    print("Best dev F1 = {:.2f}, at iteration = {}".format(best_f, best_eval * args['eval_interval']))
+    if len(dev_score_history) > 0:
+        best_f, best_eval = max(dev_score_history)*100, np.argmax(dev_score_history)+1
+        print("Best dev F1 = {:.2f}, at iteration = {}".format(best_f, best_eval * args['eval_interval']))
+    else:
+        print("Dev set never evaluated.  Saving final model.")
+        trainer.save(model_file)
+
 
 def evaluate(args):
     # file paths
