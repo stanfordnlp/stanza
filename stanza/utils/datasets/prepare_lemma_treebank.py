d7270a56748447b007e12eff457ae8c3f4766de7
John Bauer <horatio@gmail.com>
2020-12-06 13:28:48 -0800

    Refactor the lemma preparation - the POS preparation will be almost the same

--- stanza/utils/datasets/prepare_lemma_treebank.py
+++ stanza/utils/datasets/prepare_lemma_treebank.py
@@ -18,15 +18,14 @@ import stanza.utils.datasets.prepare_tokenizer_treebank as prepare_tokenizer_tre
 
 from stanza.models.common.constant import treebank_to_short_name
 
-def copy_lemmas(tokenizer_dir, tokenizer_file, lemma_dir, lemma_file, short_name):
+def copy_conllu_file(tokenizer_dir, tokenizer_file, dest_dir, dest_file, short_name):
     original = f"{tokenizer_dir}/{short_name}.{tokenizer_file}.conllu"
-    copied = f"{lemma_dir}/{short_name}.{lemma_file}.conllu"
+    copied = f"{dest_dir}/{short_name}.{dest_file}.conllu"
 
     shutil.copyfile(original, copied)
 
-def process_treebank(treebank, paths):
-    lemma_dir = paths["LEMMA_DATA_DIR"]
-    os.makedirs(lemma_dir, exist_ok=True)
+def copy_conllu_treebank(treebank, paths, dest_dir):
+    os.makedirs(dest_dir, exist_ok=True)
 
     short_name = treebank_to_short_name(treebank)
     short_language = short_name.split("_")[0]
@@ -40,13 +39,15 @@ def process_treebank(treebank, paths):
         prepare_tokenizer_treebank.process_treebank(treebank, paths, augment=False, prepare_labels=False)
 
         # now we copy the processed conllu data files
-        os.makedirs(lemma_dir, exist_ok=True)
-        copy_lemmas(tokenizer_dir, "train.gold", lemma_dir, "train.in", short_name)
-        copy_lemmas(tokenizer_dir, "dev.gold", lemma_dir, "dev.gold", short_name)
-        copy_lemmas(tokenizer_dir, "dev.gold", lemma_dir, "dev.in", short_name)
-        copy_lemmas(tokenizer_dir, "test.gold", lemma_dir, "test.gold", short_name)
-        copy_lemmas(tokenizer_dir, "test.gold", lemma_dir, "test.in", short_name)
+        os.makedirs(dest_dir, exist_ok=True)
+        copy_conllu_file(tokenizer_dir, "train.gold", dest_dir, "train.in", short_name)
+        copy_conllu_file(tokenizer_dir, "dev.gold", dest_dir, "dev.gold", short_name)
+        copy_conllu_file(tokenizer_dir, "dev.gold", dest_dir, "dev.in", short_name)
+        copy_conllu_file(tokenizer_dir, "test.gold", dest_dir, "test.gold", short_name)
+        copy_conllu_file(tokenizer_dir, "test.gold", dest_dir, "test.in", short_name)
 
+def process_treebank(treebank, paths):
+    copy_conllu_treebank(treebank, paths, paths["LEMMA_DATA_DIR"])
 
 def main():
     common.main(process_treebank)
