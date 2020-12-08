93e553b1f81457baa6d33faba34416e4c7f63409
John Bauer <horatio@gmail.com>
2020-12-07 19:44:21 -0800

    Have sudachi and jieba tokenizer integrations respect the no_ssplit flag

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -99,6 +99,24 @@ JA_DOC_GOLD_TOKENS = """
 <Token id=8;words=[<Word id=8;text=。>]>
 """.strip()
 
+JA_DOC_GOLD_NOSSPLIT_TOKENS = """
+<Token id=1;words=[<Word id=1;text=北京>]>
+<Token id=2;words=[<Word id=2;text=は>]>
+<Token id=3;words=[<Word id=3;text=中国>]>
+<Token id=4;words=[<Word id=4;text=の>]>
+<Token id=5;words=[<Word id=5;text=首都>]>
+<Token id=6;words=[<Word id=6;text=です>]>
+<Token id=7;words=[<Word id=7;text=。>]>
+<Token id=8;words=[<Word id=8;text=北京>]>
+<Token id=9;words=[<Word id=9;text=の>]>
+<Token id=10;words=[<Word id=10;text=人口>]>
+<Token id=11;words=[<Word id=11;text=は>]>
+<Token id=12;words=[<Word id=12;text=2152万>]>
+<Token id=13;words=[<Word id=13;text=人>]>
+<Token id=14;words=[<Word id=14;text=です>]>
+<Token id=15;words=[<Word id=15;text=。>]>
+""".strip()
+
 ZH_DOC = "北京是中国的首都。 北京有2100万人口，是一个直辖市。\n"
 ZH_DOC_GOLD_TOKENS = """
 <Token id=1;words=[<Word id=1;text=北京>]>
@@ -118,6 +136,25 @@ ZH_DOC_GOLD_TOKENS = """
 <Token id=8;words=[<Word id=8;text=一个>]>
 <Token id=9;words=[<Word id=9;text=直辖市>]>
 <Token id=10;words=[<Word id=10;text=。>]>
+""".strip()
+
+ZH_DOC_GOLD_NOSSPLIT_TOKENS = """
+<Token id=1;words=[<Word id=1;text=北京>]>
+<Token id=2;words=[<Word id=2;text=是>]>
+<Token id=3;words=[<Word id=3;text=中国>]>
+<Token id=4;words=[<Word id=4;text=的>]>
+<Token id=5;words=[<Word id=5;text=首都>]>
+<Token id=6;words=[<Word id=6;text=。>]>
+<Token id=7;words=[<Word id=7;text=北京>]>
+<Token id=8;words=[<Word id=8;text=有>]>
+<Token id=9;words=[<Word id=9;text=2100>]>
+<Token id=10;words=[<Word id=10;text=万>]>
+<Token id=11;words=[<Word id=11;text=人口>]>
+<Token id=12;words=[<Word id=12;text=，>]>
+<Token id=13;words=[<Word id=13;text=是>]>
+<Token id=14;words=[<Word id=14;text=一个>]>
+<Token id=15;words=[<Word id=15;text=直辖市>]>
+<Token id=16;words=[<Word id=16;text=。>]>
 """.strip()
 
 def test_tokenize():
@@ -167,10 +204,26 @@ def test_sudachipy():
     assert JA_DOC_GOLD_TOKENS == '\n\n'.join([sent.tokens_string() for sent in doc.sentences])
     assert all([doc.text[token._start_char: token._end_char] == token.text for sent in doc.sentences for token in sent.tokens])
 
+def test_sudachipy_no_ssplit():
+    nlp = stanza.Pipeline(lang='ja', dir=TEST_MODELS_DIR, processors={'tokenize': 'sudachipy'}, tokenize_no_ssplit=True, package=None)
+    doc = nlp(JA_DOC)
+
+    assert "SudachiPyTokenizer" == nlp.processors['tokenize']._variant.__class__.__name__
+    assert JA_DOC_GOLD_NOSSPLIT_TOKENS == '\n\n'.join([sent.tokens_string() for sent in doc.sentences])
+    assert all([doc.text[token._start_char: token._end_char] == token.text for sent in doc.sentences for token in sent.tokens])
+
 def test_jieba():
     nlp = stanza.Pipeline(lang='zh', dir=TEST_MODELS_DIR, processors={'tokenize': 'jieba'}, package=None)
     doc = nlp(ZH_DOC)
 
     assert "JiebaTokenizer" == nlp.processors['tokenize']._variant.__class__.__name__
     assert ZH_DOC_GOLD_TOKENS == '\n\n'.join([sent.tokens_string() for sent in doc.sentences])
+    assert all([doc.text[token._start_char: token._end_char] == token.text for sent in doc.sentences for token in sent.tokens])
+
+def test_jieba_no_ssplit():
+    nlp = stanza.Pipeline(lang='zh', dir=TEST_MODELS_DIR, processors={'tokenize': 'jieba'}, tokenize_no_ssplit=True, package=None)
+    doc = nlp(ZH_DOC)
+
+    assert "JiebaTokenizer" == nlp.processors['tokenize']._variant.__class__.__name__
+    assert ZH_DOC_GOLD_NOSSPLIT_TOKENS == '\n\n'.join([sent.tokens_string() for sent in doc.sentences])
     assert all([doc.text[token._start_char: token._end_char] == token.text for sent in doc.sentences for token in sent.tokens])
