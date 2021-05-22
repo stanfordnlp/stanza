6e34ac4826cb1bc78b4176a502fcb07b4ff9b2c4
John Bauer <horatio@gmail.com>
2021-05-21 20:27:24 -0700

    Try to install with the pl scripts

--- setup.py
+++ setup.py
@@ -94,7 +94,10 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
+        "": ["*.pl"],
     },
+
+    include_package_data=True,
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
