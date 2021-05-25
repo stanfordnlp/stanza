9768e8db6c0f36dab58b05350bf3db0f5cf9480a
Vy <vythai@stanford.edu>
2021-05-25 11:01:00 -0700

    emoved the UDBASE

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,7 +5,7 @@
 # Set UDBASE to the location of UD data folder
 # The data should be CoNLL-U format
 # For details, see http://universaldependencies.org/conll18/data.html (CoNLL-18 UD data)
-export UDBASE=/sailhome/vythai/
+# export UDBASE=/path/to/UD
 
 # Set NERBASE to the location of NER data folder
 # The data should be BIO format
