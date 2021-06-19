f035ff9f0d3ce20f8c23dcef8f22257defd7a808
John Bauer <horatio@gmail.com>
2021-06-19 09:50:32 -0700

    Code to build a top-down transition sequence from a tree

---
+++ stanza/models/constituency/transition_sequence.py
@@ -0,0 +1,41 @@
+
+
+from stanza.models.constituency.parse_transitions import *
+from stanza.models.constituency.tree_reader import read_trees
+
+def yield_top_down_sequence(tree):
+    if tree.is_preterminal():
+        yield Shift()
+        return
+
+    if tree.is_leaf():
+        return
+
+    if len(tree.children) == 1:
+        labels = []
+        while not tree.is_preterminal() and len(tree.children) == 1:
+            labels.append(tree.label)
+            tree = tree.children[0]
+        for transition in yield_top_down_sequence(tree):
+            yield transition
+        yield CompoundUnary(labels)
+        return
+
+    yield OpenConstituent(tree.label)
+    for child in tree.children:
+        for transition in yield_top_down_sequence(child):
+            yield transition
+    yield CloseConstituent()
+
+def build_top_down_sequence(tree):
+    return [t for t in yield_top_down_sequence(tree)]
+
+if __name__ == '__main__':
+    text="( (SBARQ (WHNP (WP Who)) (SQ (VP (VBZ sits) (PP (IN in) (NP (DT this) (NN seat))))) (. ?)))"
+    #text = "(WP Who)"
+
+    tree = read_trees(text)[0]
+
+    print(tree)
+    transitions = build_top_down_sequence(tree)
+    print(transitions)
