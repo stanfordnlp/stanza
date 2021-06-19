d3f02b9ba1ef2d5ab3d8093ca02183862f1da6f6
John Bauer <horatio@gmail.com>
2021-06-19 09:50:32 -0700

    Code to build a top-down transition sequence from a tree

---
+++ stanza/tests/test_constituency_transition_sequence.py
@@ -0,0 +1,31 @@
+import pytest
+from stanza.models.constituency import parse_transitions
+from stanza.models.constituency import transition_sequence
+from stanza.models.constituency import tree_reader
+
+from stanza.tests import *
+
+pytestmark = [pytest.mark.pipeline, pytest.mark.travis]
+
+def test_top_down():
+    text="((SBARQ (WHNP (WP Who)) (SQ (VP (VBZ sits) (PP (IN in) (NP (DT this) (NN seat))))) (. ?)))"
+    tree = tree_reader.read_trees(text)[0]
+
+    transitions = transition_sequence.build_top_down_sequence(tree)
+    state = parse_transitions.initial_state_from_gold_tree(tree)
+
+    for t in transitions:
+        assert t.is_legal(state)
+        state = t.apply(state)
+
+    # one item for the final tree
+    # one item for the sentinel at the end
+    assert len(state.constituents) == 2
+    # the transition sequence should put all of the words
+    # from the buffer onto the tree
+    # one spot left for the sentinel value
+    assert len(state.word_queue) == 1
+    assert len(state.transitions) == len(transitions) + 1
+
+    result_tree = state.constituents.value
+    assert result_tree == tree
