5a988de481886f1b02ab27a87aaefa3834b21eae
John Bauer <horatio@gmail.com>
2021-07-27 00:05:46 -0700

    Also make the constituents always of 1D while on the queue

--- stanza/models/constituency/parse_transitions.py
+++ stanza/models/constituency/parse_transitions.py
@@ -96,7 +96,7 @@ class State:
         return "State(\n  buffer:%s\n  transitions:%s\n  constituents:%s)" % (str(self.word_queue), str(self.transitions), str(self.constituents))
 
 def initial_state_from_tagged_words(tagged_word_list, model):
-    return State(sentence_length=len(tagged_word_list), num_opens=0, word_queue=model.initial_word_queue(tagged_word_list), transitions=model.initial_transitions())
+    return State(sentence_length=len(tagged_word_list), num_opens=0, word_queue=model.initial_word_queue(tagged_word_list), transitions=model.initial_transitions(), constituents=model.initial_constituents())
 
 def initial_state_from_words(words, tags, model):
     tagged_word_list = []
