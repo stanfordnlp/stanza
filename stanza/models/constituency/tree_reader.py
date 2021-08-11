68f13066ee24ecbfadc9e93b836c22ce72003d66
John Bauer <horatio@gmail.com>
2021-08-11 15:14:57 -0700

    Replace sly with handwritten tree parser

--- stanza/models/constituency/tree_reader.py
+++ stanza/models/constituency/tree_reader.py
@@ -1,100 +1,88 @@
-from sly import Lexer, Parser
-
+from stanza.models.common import utils
 from stanza.models.constituency.parse_tree import Tree
 
-class TreeLexer(Lexer):
-    tokens = { LPAREN, RPAREN, TOKEN }
+tqdm = utils.get_tqdm()
 
-    # String containing ignored characters (between tokens)
-    ignore = ' \t'
+OPEN_PAREN = "("
+CLOSE_PAREN = ")"
 
-    LPAREN = r'\('
-    RPAREN = r'\)'
+def recursive_open_tree(token_iterator, at_root):
+    # TODO: unwind the recursion
+    text = []
+    children = []
 
-    TOKEN = r'[^()\n ]+'
+    token = next(token_iterator, None)
+    while token != None:
+        if token is OPEN_PAREN:
+            children.append(recursive_open_tree(token_iterator, at_root=False))
+        elif token is CLOSE_PAREN:
+            if len(text) == 0:
+                if at_root:
+                    return Tree(label="ROOT", children=children)
+                raise ValueError("Found a tree with no label on a node")
 
-    # Define a rule so we can track line numbers
-    @_(r'\n+')
-    def ignore_newline(self, t):
-        self.lineno += len(t.value)
+            pieces = " ".join(text).split()
+            if len(pieces) == 1:
+                return Tree(label=pieces[0], children=children)
+            if len(children) > 0:
+                raise ValueError("Found a tree with both text children and bracketed children")
+            label = pieces[0]
+            child_label = " ".join(pieces[1:])
+            return Tree(label=label, children=Tree(label=child_label))
+        else:
+            text.append(token)
+        token = next(token_iterator, None)
 
-class TreeParser(Parser):
-    tokens = TreeLexer.tokens
+def recursive_read_trees(token_iterator):
+    """
+    TODO: some of the error cases we hit can be recovered from
+    also, the recursive call can throw errors and we should be able to report where
+    also, just in general it would be good to unwind the recursion
+    """
+    trees = []
+    token = next(token_iterator, None)
+    while token:
+        if token is OPEN_PAREN:
+            trees.append(recursive_open_tree(token_iterator, at_root=True))
+            token = next(token_iterator, None)
+            continue
 
-    # TODO: hopefully there's some other way to parse multiple trees from one file
-    @_('root treelist')
-    def treelist(self, p):
-      trees = p.treelist
-      trees.append(p.root)
-      return trees
+        if len(trees) > 0:
+            error = "Most recent tree: {}".format(trees[-1])
+        else:
+            error = "Error occurred at start of document"
 
-    @_('root')
-    def treelist(self, p):
-        return [p.root]
+        if token is CLOSE_PAREN:
+            raise ValueError("Tree document had too many close parens!  " + error)
+        else:
+            raise ValueError("Tree document had text between trees!  " + error)
 
-    # the extra layer of productions at the top is so that we can handle trees such as
-    #  ((tree stuff))
-    # by adding a ROOT node at the very top
-    @_('LPAREN factor RPAREN')
-    def root(self, p):
-        return Tree(label="ROOT", children=[p.factor])
-
-    @_('factor')
-    def root(self, p):
-        return p.factor
-
-    @_('LPAREN TOKEN subtrees RPAREN')
-    def factor(self, p):
-        token = p.TOKEN
-        children = p.subtrees
-        return Tree(label=token, children=children)
-
-    @_('LPAREN TOKEN words RPAREN')
-    def factor(self, p):
-        token = p.TOKEN
-        text = p.words
-        # We join the text into one token
-        # This is relevant in the case of VI, for example,
-        # or the FR token with a space in the middle of a number
-        text = " ".join(text)
-        child = Tree(label=text)
-        return Tree(label=token, children=[child])
-
-    @_('LPAREN TOKEN RPAREN')
-    def factor(self, p):
-        return Tree(label=p.TOKEN)
-
-    @_('factor')
-    def subtrees(self, p):
-        return [p.factor]
-
-    @_('subtrees factor')
-    def subtrees(self, p):
-        children = p.subtrees
-        children.append(p.factor)
-        return children
-
-    @_('TOKEN')
-    def words(self, p):
-        return [p.TOKEN]
-
-    @_('words TOKEN')
-    def words(self, p):
-        words = p.append(p.TOKEN)
-        return words
+    return trees
 
 def read_trees(text):
     """
     Reads multiple trees from the text
-
-    TODO: there needs to be some error handling to recover from cases such as too many )
-    or to alert for broken trees and which line they occur on
-    and we should check for the result of the reading
     """
-    lexer = TreeLexer()
-    parser = TreeParser()
-    trees = parser.parse(lexer.tokenize(text))
-    trees.reverse()
+    lines = text.split("\n")
+    pieces = []
+    for line in lines:
+        open_pieces = line.split(OPEN_PAREN)
+        for o_idx, open_piece in enumerate(open_pieces):
+            if open_piece:
+                close_pieces = open_piece.split(CLOSE_PAREN)
+                for c_idx, close_piece in enumerate(close_pieces):
+                    close_piece = close_piece.strip()
+                    if close_piece:
+                        pieces.append(close_piece)
+                    if c_idx != len(close_pieces) - 1:
+                        pieces.append(CLOSE_PAREN)
+            if o_idx != len(open_pieces) - 1:
+                pieces.append(OPEN_PAREN)
+
+    if len(pieces) > 10000:
+        pieces = tqdm(pieces)
+    token_iterator = iter(pieces)
+    trees = recursive_read_trees(token_iterator)
     return trees
 
 def read_tree_file(filename):
