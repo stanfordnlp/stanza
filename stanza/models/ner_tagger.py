46118e61bb90ae28ee605b8394f6a813c969c4a4
John Bauer <horatio@gmail.com>
2021-08-22 00:13:35 -0700

    Only report max dev score if ran dev set at least once

--- stanza/models/ner_tagger.py
+++ stanza/models/ner_tagger.py
@@ -249,8 +249,9 @@ def train(args):
 
     logger.info("Training ended with {} steps.".format(global_step))
 
-    best_f, best_eval = max(dev_score_history)*100, np.argmax(dev_score_history)+1
-    logger.info("Best dev F1 = {:.2f}, at iteration = {}".format(best_f, best_eval * args['eval_interval']))
+    if len(dev_score_history) > 0:
+        best_f, best_eval = max(dev_score_history)*100, np.argmax(dev_score_history)+1
+        logger.info("Best dev F1 = {:.2f}, at iteration = {}".format(best_f, best_eval * args['eval_interval']))
 
 def evaluate(args):
     # file paths
