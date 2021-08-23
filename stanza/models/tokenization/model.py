f7b8cdccafecd30f6f5969829efcb091aaad5af3
Gordon <gordonchi11@gmail.com>
2021-08-24 00:31:59 +0800

    Update model.py

--- stanza/models/tokenization/model.py
+++ stanza/models/tokenization/model.py
@@ -11,7 +11,7 @@ class Tokenizer(nn.Module):
 
         self.embeddings = nn.Embedding(nchars, emb_dim, padding_idx=0)
 
-        self.rnn = nn.LSTM(emb_dim + feat_dim, hidden_dim, num_layers=self.args['rnn_layers'], bidirectional=True, batch_first=True, dropout=dropout if self.args['rnn_layers'] > 1 else 0)
+        self.rnn = nn.LSTM(emb_dim, hidden_dim, num_layers=self.args['rnn_layers'], bidirectional=True, batch_first=True, dropout=dropout if self.args['rnn_layers'] > 1 else 0)
 
         if self.args['conv_res'] is not None:
             self.conv_res = nn.ModuleList()
@@ -23,7 +23,7 @@ class Tokenizer(nn.Module):
 
             if self.args.get('hier_conv_res', False):
                 self.conv_res2 = nn.Conv1d(hidden_dim * 2 * len(self.conv_sizes), hidden_dim * 2, 1)
-        self.tok_clf = nn.Linear(hidden_dim * 2, 1)
+
         self.sent_clf = nn.Linear(hidden_dim * 2, 1)
         if self.args['use_mwt']:
             self.mwt_clf = nn.Linear(hidden_dim * 2, 1)
@@ -44,10 +44,6 @@ class Tokenizer(nn.Module):
     def forward(self, x, feats):
         emb = self.embeddings(x)
         emb = self.dropout(emb)
-        feats = self.dropout_feat(feats)
-
-
-        emb = torch.cat([emb, feats], 2)
 
         inp, _ = self.rnn(emb)
 
@@ -67,26 +63,22 @@ class Tokenizer(nn.Module):
 
         inp = self.dropout(inp)
 
-        tok0 = self.tok_clf(inp)
         sent0 = self.sent_clf(inp)
         if self.args['use_mwt']:
             mwt0 = self.mwt_clf(inp)
 
         if self.args['hierarchical']:
             if self.args['hier_invtemp'] > 0:
-                inp2, _ = self.rnn2(inp * (1 - self.toknoise(torch.sigmoid(-tok0 * self.args['hier_invtemp']))))
+                inp2, _ = self.rnn2(inp * (1 - self.toknoise(torch.sigmoid(-sent0 * self.args['hier_invtemp']))))
             else:
                 inp2, _ = self.rnn2(inp)
 
             inp2 = self.dropout(inp2)
 
-            tok0 = tok0 + self.tok_clf2(inp2)
             sent0 = sent0 + self.sent_clf2(inp2)
             if self.args['use_mwt']:
                 mwt0 = mwt0 + self.mwt_clf2(inp2)
 
-        nontok = F.logsigmoid(-tok0)
-        tok = F.logsigmoid(tok0)
         nonsent = F.logsigmoid(-sent0)
         sent = F.logsigmoid(sent0)
         if self.args['use_mwt']:
@@ -96,6 +88,15 @@ class Tokenizer(nn.Module):
         if self.args['use_mwt']:
             pred = torch.cat([nontok, tok+nonsent+nonmwt, tok+sent+nonmwt, tok+nonsent+mwt, tok+sent+mwt], 2)
         else:
-            pred = torch.cat([nontok, tok+nonsent, tok+sent], 2)
+            pred = torch.cat([nonsent, sent], 2)
+
 
         return pred
+
+
+
+
+
+
+
+
