599148b7775b41661cea7ef36c2b517a40063624
John Bauer <horatio@gmail.com>
2021-09-22 07:20:27 -0700

    Make three different versions of SentenceBoundary as an option

--- stanza/models/constituency_parser.py
+++ stanza/models/constituency_parser.py
@@ -75,6 +75,7 @@ import torch
 from stanza import Pipeline
 from stanza.models.common import utils
 from stanza.models.constituency import trainer
+from stanza.models.constituency.lstm_model import SentenceBoundary
 from stanza.models.constituency.parse_transitions import TransitionScheme
 
 logger = logging.getLogger('stanza')
@@ -230,8 +231,8 @@ def parse_args(args=None):
     parser.add_argument('--num_lstm_layers', default=2, type=int, help='How many layers to use in the LSTMs')
     parser.add_argument('--num_output_layers', default=3, type=int, help='How many layers to use at the prediction level')
 
-    parser.add_argument('--sentence_boundary_vectors', action='store_true', help='Train vectors for the boundaries of a sentence')
-    parser.add_argument('--no_sentence_boundary_vectors', action='store_false', dest='sentence_boundary_vectors', help="Don't train vectors for the boundaries of a sentence")
+    parser.add_argument('--sentence_boundary_vectors', default=SentenceBoundary.NONE, type=lambda x: SentenceBoundary[x.upper()],
+                        help='Vectors to learn at the start & end of sentences.  {}'.format(", ".join(x.name for x in SentenceBoundary)))
 
     parser.add_argument('--finetune', action='store_true', help='Load existing model during `train` mode from `load_name` path')
     parser.add_argument('--maybe_finetune', action='store_true', help='Load existing model during `train` mode from `load_name` path if it exists.  Useful for running in situations where a job is frequently being preempted')
