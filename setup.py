c3cd0a2bbc7bc20a4b4003b1c96377b9f3b10505
John Bauer <horatio@gmail.com>
2021-09-30 09:14:21 -0700

    Add missing pytest to the test conda

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'test': ['coverage', 'pytest'],
     },
 
     # If there are data files included in your packages that need to be
