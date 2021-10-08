f97758a571558172fe0231391f4c7ab120627478
John Bauer <horatio@gmail.com>
2021-10-08 16:53:33 -0700

    Fix a specific test on Windows (AFTER corenlp is downloaded and the setup script is run)

--- stanza/tests/__init__.py
+++ stanza/tests/__init__.py
@@ -103,6 +103,8 @@ def safe_rm(path_to_rm):
 
 def compare_ignoring_whitespace(predicted, expected):
     predicted = re.sub('[ \t]+', ' ', predicted.strip())
+    predicted = re.sub('\r\n', '\n', predicted)
     expected = re.sub('[ \t]+', ' ', expected.strip())
+    expected = re.sub('\r\n', '\n', expected)
     assert predicted == expected
 
