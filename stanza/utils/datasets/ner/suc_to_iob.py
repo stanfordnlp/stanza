05f0b6010df34e9ca27d39d55261eb1fcc66036a
John Bauer <horatio@gmail.com>
2021-10-21 10:13:10 -0700

    Add an output file option to the conversion script Add a wrapper for the suc_to_iob script to prepare_ner_dataset.py Add an option to suc_to_iob to use <name> instead of <ne>.  Use those tags when building a model

--- stanza/utils/datasets/ner/suc_to_iob.py
+++ stanza/utils/datasets/ner/suc_to_iob.py
@@ -1,3 +1,27 @@
+"""
+Conversion tool to transform SUC3's xml format to IOB
+
+Copyright 2017, Emil Stenstrom
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from bz2 import BZ2File
 from xml.etree.ElementTree import iterparse
 from enum import Enum, auto
@@ -8,7 +32,7 @@ class ParseDetail(Enum):
     NAMED = auto()
     NAMED_DETAILED = auto()
 
-def parse(fp, parse_detail=ParseDetail.NAMED, skiptypes=[]):
+def parse(fp, parse_detail=ParseDetail.NAMED, skiptypes=[], ner_tag="ne"):
     root = None
     ne_type = "O"
     ne_prefix = ""
@@ -19,7 +43,7 @@ def parse(fp, parse_detail=ParseDetail.NAMED, skiptypes=[]):
         if event == "end" and elem.tag == "sentence":
             yield "\n"
 
-        if event == "start" and elem.tag == "ne" and elem.attrib["type"] not in skiptypes:
+        if event == "start" and elem.tag == ner_tag and elem.attrib["type"] not in skiptypes:
             if parse_detail == ParseDetail.UNNAMED:
                 ne_type = "LABEL"
             elif parse_detail == ParseDetail.NAMED:
@@ -29,7 +53,7 @@ def parse(fp, parse_detail=ParseDetail.NAMED, skiptypes=[]):
 
             ne_prefix = "B-"
 
-        if event == "end" and elem.tag == "ne":
+        if event == "end" and elem.tag == ner_tag:
             ne_type = "O"
             ne_prefix = ""
 
@@ -41,7 +65,7 @@ def parse(fp, parse_detail=ParseDetail.NAMED, skiptypes=[]):
 
         root.clear()
 
-def main():
+def main(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "infile",
@@ -49,6 +73,16 @@ def main():
             Input for that contains the full SUC 3.0 XML.
             Can be the bz2-zipped version or the xml version.
         """
+    )
+    parser.add_argument(
+        "outfile",
+        help="Output file for IOB format."
+    )
+    parser.add_argument(
+        "--ner_tag",
+        help="XML tag to extract the NERs from.",
+        choices=["ne", "name"],
+        default="ne"
     )
     parser.add_argument(
         "--detail",
@@ -62,7 +96,9 @@ def main():
         nargs="+",
         default=[]
     )
-    args = parser.parse_args()
+    args = parser.parse_args(args=args)
+    if args.ner_tag == 'name' and args.detail == 'NAMED_DETAILED':
+        raise ValueError("<name> tags do not have subtypes in this dataset")
 
     MAGIC_BZ2_FILE_START = b"\x42\x5a\x68"
     fp = open(args.infile, "rb")
@@ -73,8 +109,9 @@ def main():
     else:
         fp = open(args.infile, "rb")
 
-    for line in parse(fp, parse_detail=ParseDetail[args.detail], skiptypes=args.skiptypes):
-        print(line, end="")
+    with open(args.outfile, "w") as fout:
+        for line in parse(fp, parse_detail=ParseDetail[args.detail], skiptypes=args.skiptypes, ner_tag=args.ner_tag):
+            fout.write(line)
 
     fp.close()
 
