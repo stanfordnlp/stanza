6e054823d1fa6252c0613e61fca2e48e1e4fba44
John Bauer <horatio@gmail.com>
2021-10-27 17:09:12 -0700

    Attempt to train an UNK tag vector

--- stanza/models/constituency_parser.py
+++ stanza/models/constituency_parser.py
@@ -216,6 +216,7 @@ def parse_args(args=None):
 
     parser.add_argument('--rare_word_unknown_frequency', default=0.02, type=float, help='How often to replace a rare word with UNK when training')
     parser.add_argument('--rare_word_threshold', default=0.02, type=float, help='How many words to consider as rare words as a fraction of the dataset')
+    parser.add_argument('--tag_unknown_frequency', default=0.001, type=float, help='How often to replace a tag with UNK when training')
 
     parser.add_argument('--num_lstm_layers', default=2, type=int, help='How many layers to use in the LSTMs')
     parser.add_argument('--num_output_layers', default=3, type=int, help='How many layers to use at the prediction level')
