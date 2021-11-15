52878ce1411777efb9b82ed883c076dc0869e76f
John Bauer <horatio@gmail.com>
2021-11-15 11:13:05 -0800

    Allow for the possibility of turning off pattn
    Assume 0 attention if not in the args at all - backwards compatible with old models
    
    Add a test which checks the model building when attn is off

--- stanza/tests/constituency/test_lstm_model.py
+++ stanza/tests/constituency/test_lstm_model.py
@@ -196,3 +196,13 @@ def test_forward_labeled_transitions(pt):
     model = build_model(pt, '--no_labeled_transition_embedding')
     run_forward_checks(model)
     
+def test_forward_partitioned_attention(pt):
+    """
+    Test with & without partitioned attention layers
+    """
+    model = build_model(pt, '--pattn_num_heads', '8', '--pattn_num_layers', '8')
+    run_forward_checks(model)
+
+    model = build_model(pt, '--pattn_num_heads', '0', '--pattn_num_layers', '0')
+    run_forward_checks(model)
+    
