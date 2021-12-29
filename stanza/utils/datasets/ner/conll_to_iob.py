9a04382385734e163d206d079cd7ec8b77bdfd89
John Bauer <horatio@gmail.com>
2021-12-29 01:17:03 -0800

    Add a tool for converting da_ddt to stanza's ner input format

---
+++ stanza/utils/datasets/ner/conll_to_iob.py
@@ -0,0 +1,42 @@
+"""
+Process a conll file into BIO
+
+Includes the ability to process a file from a text file
+or a text file within a zip
+
+Main program extracts a piece of the zip file from the Danish DDT dataset
+"""
+
+import io
+import zipfile
+from zipfile import ZipFile
+from stanza.utils.conll import CoNLL
+
+def process_conll(input_file, output_file, zip_file=None):
+    """
+    Process a single file from DDT
+
+    zip_filename: path to ddt.zip
+    in_filename: which piece to read
+    out_filename: where to write the result
+    """
+    doc = CoNLL.conll2doc(input_file=input_file, zip_file=zip_file)
+
+    with open(output_file, "w", encoding="utf-8") as fout:
+        for sentence_idx, sentence in enumerate(doc.sentences):
+            for token_idx, token in enumerate(sentence.tokens):
+                misc = token.misc.split("|")
+                for attr in misc:
+                    if attr.startswith("name="):
+                        ner = attr.split("=", 1)[1]
+                        break
+                else:
+                    raise ValueError("Could not find ner tag in document {}, sentence {}, token {}".format(input_file, sentence_idx, token_idx))
+                fout.write("%s\t%s\n" % (token.text, ner))
+            fout.write("\n")
+
+def main():
+    process_zip("extern_data/ner/da_ddt/ddt.zip", "ddt.train.conllu", "data/ner/da_ddt.train.bio")
+
+if __name__ == '__main__':
+    main()
