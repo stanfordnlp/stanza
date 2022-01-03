4d7be0ba553003f62aaf3d2b61a9402e64ee647f
John Bauer <horatio@gmail.com>
2022-01-03 14:10:21 -0800

    Update to latest version of suc3_to_iob
    Adapt the outfile changes to the updated suc_to_iob script
    
    Latest script merges ne & name tags and remaps a few labels

--- stanza/utils/datasets/ner/suc_to_iob.py
+++ stanza/utils/datasets/ner/suc_to_iob.py
@@ -1,7 +1,7 @@
 """
 Conversion tool to transform SUC3's xml format to IOB
 
-Copyright 2017, Emil Stenstrom
+Copyright 2017-2022, Emil Stenström
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -11,7 +11,8 @@ distribute, sublicense, and/or sell copies of the Software, and to
 permit persons to whom the Software is furnished to do so, subject to
 the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
@@ -24,46 +25,92 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from bz2 import BZ2File
 from xml.etree.ElementTree import iterparse
-from enum import Enum, auto
 import argparse
+from collections import Counter
+import sys
 
-class ParseDetail(Enum):
-    UNNAMED = auto()
-    NAMED = auto()
-    NAMED_DETAILED = auto()
-
-def parse(fp, parse_detail=ParseDetail.NAMED, skiptypes=[], ner_tag="ne"):
+def parse(fp, skiptypes=[]):
     root = None
-    ne_type = "O"
     ne_prefix = ""
+    ne_type = "O"
+    name_prefix = ""
+    name_type = "O"
+
     for event, elem in iterparse(fp, events=("start", "end")):
         if root is None:
             root = elem
 
-        if event == "end" and elem.tag == "sentence":
-            yield "\n"
+        if event == "start":
+            if elem.tag == "name":
+                _type = name_type_to_label(elem.attrib["type"])
+                if (
+                    _type not in skiptypes and
+                    not (_type == "ORG" and ne_type == "LOC")
+                ):
+                    name_type = _type
+                    name_prefix = "B-"
 
-        if event == "start" and elem.tag == ner_tag and elem.attrib["type"] not in skiptypes:
-            if parse_detail == ParseDetail.UNNAMED:
-                ne_type = "LABEL"
-            elif parse_detail == ParseDetail.NAMED:
-                ne_type = elem.attrib["type"]
-            elif parse_detail == ParseDetail.NAMED_DETAILED:
-                ne_type = elem.attrib["type"] + "-" + elem.attrib["subtype"]
+            elif elem.tag == "ne":
+                _type = ne_type_to_label(elem.attrib["type"])
+                if "/" in _type:
+                    _type = ne_type_to_label(_type[_type.index("/") + 1:])
 
-            ne_prefix = "B-"
+                if _type not in skiptypes:
+                    ne_type = _type
+                    ne_prefix = "B-"
 
-        if event == "end" and elem.tag == ner_tag:
-            ne_type = "O"
-            ne_prefix = ""
+            elif elem.tag == "w":
+                if name_type == "PER" and elem.attrib["pos"] == "NN":
+                    name_type = "O"
+                    name_prefix = ""
 
-        if event == "end" and elem.tag == "w":
-            yield elem.text + "\t" + ne_prefix + ne_type + "\n"
+        elif event == "end":
+            if elem.tag == "sentence":
+                yield
 
-            if ne_type != "O":
-                ne_prefix = "I-"
+            elif elem.tag == "name":
+                name_type = "O"
+                name_prefix = ""
+
+            elif elem.tag == "ne":
+                ne_type = "O"
+                ne_prefix = ""
+
+            elif elem.tag == "w":
+                if name_type != "O" and name_type != "OTH":
+                    yield elem.text, name_prefix, name_type
+                elif ne_type != "O":
+                    yield elem.text, ne_prefix, ne_type
+                else:
+                    yield elem.text, "", "O"
+
+                if ne_type != "O":
+                    ne_prefix = "I-"
+
+                if name_type != "O":
+                    name_prefix = "I-"
 
         root.clear()
+
+def ne_type_to_label(ne_type):
+    mapping = {
+        "PRS": "PER",
+    }
+    return mapping.get(ne_type, ne_type)
+
+def name_type_to_label(name_type):
+    mapping = {
+        "inst": "ORG",
+        "product": "OBJ",
+        "other": "OTH",
+        "place": "LOC",
+        "myth": "PER",
+        "person": "PER",
+        "event": "EVN",
+        "work": "WRK",
+        "animal": "PER",
+    }
+    return mapping.get(name_type)
 
 def main(args=None):
     parser = argparse.ArgumentParser()
@@ -76,19 +123,11 @@ def main(args=None):
     )
     parser.add_argument(
         "outfile",
-        help="Output file for IOB format."
-    )
-    parser.add_argument(
-        "--ner_tag",
-        help="XML tag to extract the NERs from.",
-        choices=["ne", "name"],
-        default="ne"
-    )
-    parser.add_argument(
-        "--detail",
-        help="Detail level that the file should be output in.",
-        choices=[e.name for e in ParseDetail],
-        default="NAMED"
+        nargs="?",
+        help="""
+             Output file for IOB format.
+             Optional - will print to stdout otherwise
+        """
     )
     parser.add_argument(
         "--skiptypes",
@@ -96,9 +135,13 @@ def main(args=None):
         nargs="+",
         default=[]
     )
-    args = parser.parse_args(args=args)
-    if args.ner_tag == 'name' and args.detail == 'NAMED_DETAILED':
-        raise ValueError("<name> tags do not have subtypes in this dataset")
+    parser.add_argument(
+        "--stats_only",
+        help="Show statistics of found labels at the end of output.",
+        action='store_true',
+        default=False
+    )
+    args = parser.parse_args(args)
 
     MAGIC_BZ2_FILE_START = b"\x42\x5a\x68"
     fp = open(args.infile, "rb")
@@ -109,11 +152,29 @@ def main(args=None):
     else:
         fp = open(args.infile, "rb")
 
-    with open(args.outfile, "w") as fout:
-        for line in parse(fp, parse_detail=ParseDetail[args.detail], skiptypes=args.skiptypes, ner_tag=args.ner_tag):
-            fout.write(line)
+    if args.outfile is not None:
+        fout = open(args.outfile, "w", encoding="utf-8")
+    else:
+        fout = sys.stdout
+
+    type_stats = Counter()
+    for token in parse(fp, skiptypes=args.skiptypes):
+        if not token:
+            if not args.stats_only:
+                fout.write("\n")
+        else:
+            word, prefix, label = token
+            if args.stats_only:
+                type_stats[label] += 1
+            else:
+                fout.write("%s\t%s%s\n" % (word, prefix, label))
+
+    if args.stats_only:
+        fout.write(str(type_stats) + "\n")
 
     fp.close()
+    if args.outfile is not None:
+        fout.close()
 
 
 if __name__ == '__main__':
