af27aff8ed878fe9a272e9db16c58b5aa8c0393f
J38 <jebolton@stanford.edu>
2022-01-12 02:42:35 -0800

    refactor

--- stanza/tests/server/test_server_pretokenized.py
+++ stanza/tests/server/test_server_pretokenized.py
@@ -9,11 +9,14 @@ from stanza.server import CoreNLPClient
 
 pytestmark = pytest.mark.client
 
-# test Italian pretokenized
-italian_tokens = [
+tokens = {}
+tags = {}
+
+# Italian examples
+tokens["italian"] = [
     "È vero , tutti possiamo essere sostituiti .\n Alcune chiamate partirono da il Quirinale ."
 ]
-italian_tags = [
+tags["italian"] = [
     [
         ["AUX", "ADJ", "PUNCT", "PRON", "AUX", "AUX", "VERB", "PUNCT"],
         ["DET", "NOUN", "VERB", "ADP", "DET", "PROPN", "PUNCT"],
@@ -21,57 +24,53 @@ italian_tags = [
 ]
 
 
-def test_italian_pretokenized():
-    """Test submitting pretokenized Italian text."""
-    with CoreNLPClient(
-        properties="italian",
-        annotators="pos",
-        pretokenized=True,
-        be_quiet=True,
-    ) as client:
-        for input_text, gold_tags in zip(italian_tokens, italian_tags):
-            ann = client.annotate(input_text)
-            for sentence_tags, sentence in zip(gold_tags, ann.sentence):
-                result_tags = [tok.pos for tok in sentence.token]
-                assert sentence_tags == result_tags
-
-
-# test French pretokenized
-french_tokens = [
-    "Les études durent six ans mais leur contenu diffère donc selon les Facultés ."
+# French examples
+tokens["french"] = [
+    (
+     "Les études durent six ans mais leur contenu diffère donc selon les Facultés .\n"
+     "Il est fêté le 22 mai ."
+    )
 ]
-french_tags = [
+tags["french"] = [
     [
-        [
-            "DET",
-            "NOUN",
-            "VERB",
-            "NUM",
-            "NOUN",
-            "CCONJ",
-            "DET",
-            "NOUN",
-            "VERB",
-            "ADV",
-            "ADP",
-            "DET",
-            "PROPN",
-            "PUNCT",
-        ],
+        ["DET", "NOUN", "VERB", "NUM", "NOUN", "CCONJ", "DET", "NOUN", "VERB", "ADV", "ADP", "DET", "PROPN", "PUNCT"],
+        ["PRON", "AUX", "VERB", "DET", "NUM", "NOUN", "PUNCT"]
     ],
 ]
 
 
-def test_french_pretokenized():
+# English examples
+tokens["english"] = ["This shouldn't be split .\n I hope it's not ."]
+tags["english"] = [
+    [
+        ["DT", "NN", "VB", "VBN", "."],
+        ["PRP", "VBP", "PRP$", "RB", "."],
+    ],
+]
+
+
+def pretokenized_test(lang):
     """Test submitting pretokenized French text."""
     with CoreNLPClient(
-        properties="french",
+        properties=lang,
         annotators="pos",
         pretokenized=True,
         be_quiet=True,
     ) as client:
-        for input_text, gold_tags in zip(french_tokens, french_tags):
+        for input_text, gold_tags in zip(tokens[lang], tags[lang]):
             ann = client.annotate(input_text)
             for sentence_tags, sentence in zip(gold_tags, ann.sentence):
                 result_tags = [tok.pos for tok in sentence.token]
                 assert sentence_tags == result_tags
+
+
+def test_english_pretokenized():
+    pretokenized_test("english")
+
+
+def test_italian_pretokenized():
+    pretokenized_test("italian")
+
+
+def test_french_pretokenized():
+    pretokenized_test("french")
