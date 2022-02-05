16d52928730cfcad6151c8dc15a63ce4c4295348
John Bauer <horatio@gmail.com>
2022-02-05 00:33:54 -0800

    Download missing models when creating a pipeline
    Less noisy logging when creating a pipeline from already downloaded items
    Filters variants before trying to download them
    Doesn't try to download models for annotators which don't exist
    Attempt to download a missing resources.json
    Downloads missing resources if REUSE_RESOURCES is set
    
    Adds tests of the reuse_resources and download_resources options
    including checks using mod times to verify that downloads occurred,
    and checks that various download methods will overwrite an incorrect model

--- stanza/tests/pipeline/test_core.py
+++ stanza/tests/pipeline/test_core.py
@@ -1,4 +1,6 @@
 import pytest
+import tempfile
+
 import stanza
 
 from stanza.tests import *
@@ -18,3 +20,111 @@ def test_pretagged():
     nlp = stanza.Pipeline(lang='en', dir=TEST_MODELS_DIR, processors="tokenize,lemma,depparse", pretagged=True)
     # test that the module specific flag overrides the general flag
     nlp = stanza.Pipeline(lang='en', dir=TEST_MODELS_DIR, processors="tokenize,lemma,depparse", depparse_pretagged=True, pretagged=False)
+
+def test_download_missing_ner_model():
+    """
+    Test that the pipeline will automatically download missing models
+    """
+    with tempfile.TemporaryDirectory(dir=".") as test_dir:
+        stanza.download("en", model_dir=test_dir, processors="tokenize", package="combined", verbose=False)
+        pipe = stanza.Pipeline("en", model_dir=test_dir, processors="tokenize,ner", package={"ner": ("ontonotes")})
+
+        assert sorted(os.listdir(test_dir)) == ['en', 'resources.json']
+        en_dir = os.path.join(test_dir, 'en')
+        en_dir_listing = sorted(os.listdir(en_dir))
+        assert en_dir_listing == ['backward_charlm', 'forward_charlm', 'ner', 'tokenize']
+        assert os.listdir(os.path.join(en_dir, 'ner')) == ['ontonotes.pt']
+
+
+def test_download_missing_resources():
+    """
+    Test that the pipeline will automatically download missing models
+    """
+    with tempfile.TemporaryDirectory(dir=".") as test_dir:
+        pipe = stanza.Pipeline("en", model_dir=test_dir, processors="tokenize,ner", package={"tokenize": "combined", "ner": "ontonotes"})
+
+        assert sorted(os.listdir(test_dir)) == ['en', 'resources.json']
+        en_dir = os.path.join(test_dir, 'en')
+        en_dir_listing = sorted(os.listdir(en_dir))
+        assert en_dir_listing == ['backward_charlm', 'forward_charlm', 'ner', 'tokenize']
+        assert os.listdir(os.path.join(en_dir, 'ner')) == ['ontonotes.pt']
+
+
+def test_download_resources_overwrites():
+    """
+    Test that the DOWNLOAD_RESOURCES method overwrites an existing resources.json
+    """
+    with tempfile.TemporaryDirectory(dir=".") as test_dir:
+        pipe = stanza.Pipeline("en", model_dir=test_dir, processors="tokenize", package={"tokenize": "combined"})
+
+        assert sorted(os.listdir(test_dir)) == ['en', 'resources.json']
+        resources_path = os.path.join(test_dir, 'resources.json')
+        mod_time = os.path.getmtime(resources_path)
+
+        pipe = stanza.Pipeline("en", model_dir=test_dir, processors="tokenize", package={"tokenize": "combined"})
+        new_mod_time = os.path.getmtime(resources_path)
+        assert mod_time != new_mod_time
+
+def test_reuse_resources_overwrites():
+    """
+    Test that the REUSE_RESOURCES method does *not* overwrite an existing resources.json
+    """
+    with tempfile.TemporaryDirectory(dir=".") as test_dir:
+        pipe = stanza.Pipeline("en",
+                               download_method=core.DownloadMethod.REUSE_RESOURCES,
+                               model_dir=test_dir,
+                               processors="tokenize",
+                               package={"tokenize": "combined"})
+
+        assert sorted(os.listdir(test_dir)) == ['en', 'resources.json']
+        resources_path = os.path.join(test_dir, 'resources.json')
+        mod_time = os.path.getmtime(resources_path)
+
+        pipe = stanza.Pipeline("en",
+                               download_method=core.DownloadMethod.REUSE_RESOURCES,
+                               model_dir=test_dir,
+                               processors="tokenize",
+                               package={"tokenize": "combined"})
+        new_mod_time = os.path.getmtime(resources_path)
+        assert mod_time == new_mod_time
+
+
+def test_download_not_repeated():
+    """
+    Test that a model is only downloaded once if it already matches the expected model from the resources file
+    """
+    with tempfile.TemporaryDirectory(dir=".") as test_dir:
+        stanza.download("en", model_dir=test_dir, processors="tokenize", package="combined")
+
+        assert sorted(os.listdir(test_dir)) == ['en', 'resources.json']
+        en_dir = os.path.join(test_dir, 'en')
+        en_dir_listing = sorted(os.listdir(en_dir))
+        assert en_dir_listing == ['tokenize']
+        tokenize_path = os.path.join(en_dir, "tokenize", "combined.pt")
+        mod_time = os.path.getmtime(tokenize_path)
+
+        pipe = stanza.Pipeline("en", model_dir=test_dir, processors="tokenize", package={"tokenize": "combined"})
+        assert os.path.getmtime(tokenize_path) == mod_time
+
+
+
+def test_download_fixed():
+    """
+    Test that a model is fixed if the existing model doesn't match the md5sum
+    """
+    for download_method in (core.DownloadMethod.REUSE_RESOURCES, core.DownloadMethod.DOWNLOAD_RESOURCES):
+        with tempfile.TemporaryDirectory(dir=".") as test_dir:
+            stanza.download("en", model_dir=test_dir, processors="tokenize", package="combined")
+
+            assert sorted(os.listdir(test_dir)) == ['en', 'resources.json']
+            en_dir = os.path.join(test_dir, 'en')
+            en_dir_listing = sorted(os.listdir(en_dir))
+            assert en_dir_listing == ['tokenize']
+            tokenize_path = os.path.join(en_dir, "tokenize", "combined.pt")
+
+            with open(tokenize_path, "w") as fout:
+                fout.write("Unban mox opal!")
+            mod_time = os.path.getmtime(tokenize_path)
+
+            pipe = stanza.Pipeline("en", model_dir=test_dir, processors="tokenize", package={"tokenize": "combined"}, download_method=download_method)
+            assert os.path.getmtime(tokenize_path) != mod_time
