2370204e63c74f00664c3ebaf6d9ab45794d0667
John Bauer <horatio@gmail.com>
2022-02-16 19:03:42 -0800

    Dropping a reference will hopefully save some GPU memory

--- stanza/models/constituency/positional_encoding.py
+++ stanza/models/constituency/positional_encoding.py
@@ -26,7 +26,12 @@ class SinusoidalEncoding(nn.Module):
 
     def forward(self, x):
         if max(x) >= self.pe.shape[0]:
-            self.register_buffer('pe', self.build_position(self.pe.shape[1], max(x)+1, device=self.pe.device))
+            # try to drop the reference first before creating a new encoding
+            # the goal being to save memory if we are close to the memory limit
+            device = self.pe.device
+            shape = self.pe.shape[1]
+            self.register_buffer('pe', None)
+            self.register_buffer('pe', self.build_position(shape, max(x)+1, device=device))
         return self.pe[x]
 
     def max_len(self):
