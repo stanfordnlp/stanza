825f636a42b74a18a78a0f78b3b3968042288c4f
John Bauer <horatio@gmail.com>
2022-03-11 00:38:14 -0800

    Add a utility method for constructing a Document from pretokenized sentences and an original text
    Consistently throws ValueError for problems with the pretokenization
    Includes a test of a couple basic examples
    
    Addresses #967

--- stanza/tests/tokenization/test_tokenize_utils.py
+++ stanza/tests/tokenization/test_tokenize_utils.py
@@ -30,3 +30,36 @@ def test_find_spans():
 
     raw = ['<PAD>', 'u', 'n', 'b', 'a', 'n', '<PAD>', 'm', 'o', 'x', ' ', 'o', 'p', 'a', 'l']
     assert utils.find_spans(raw) == [(1, 6), (7, 15)]
+
+def check_offsets(doc, expected_offsets):
+    """
+    Compare the start_char and end_char of the tokens in the doc with the given list of list of offsets
+    """
+    assert len(doc.sentences) == len(expected_offsets)
+    for sentence, offsets in zip(doc.sentences, expected_offsets):
+        assert len(sentence.tokens) == len(offsets)
+        for token, offset in zip(sentence.tokens, offsets):
+            assert token.start_char == offset[0]
+            assert token.end_char == offset[1]
+
+def test_match_tokens_with_text():
+    """
+    Test the conversion of pretokenized text to Document
+    """
+    doc = utils.match_tokens_with_text([["This", "is", "a", "test"]], "Thisisatest")
+    expected_offsets = [[(0, 4), (4, 6), (6, 7), (7, 11)]]
+    check_offsets(doc, expected_offsets)
+
+    doc = utils.match_tokens_with_text([["This", "is", "a", "test"], ["unban", "mox", "opal", "!"]], "Thisisatest  unban mox  opal!")
+    expected_offsets = [[(0, 4), (4, 6), (6, 7), (7, 11)],
+                        [(13, 18), (19, 22), (24, 28), (28, 29)]]
+    check_offsets(doc, expected_offsets)
+
+    with pytest.raises(ValueError):
+        doc = utils.match_tokens_with_text([["This", "is", "a", "test"]], "Thisisatestttt")
+
+    with pytest.raises(ValueError):
+        doc = utils.match_tokens_with_text([["This", "is", "a", "test"]], "Thisisates")
+
+    with pytest.raises(ValueError):
+        doc = utils.match_tokens_with_text([["This", "iz", "a", "test"]], "Thisisatest")
