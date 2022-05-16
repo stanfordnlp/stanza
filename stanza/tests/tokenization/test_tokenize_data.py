db6810f1bcc9b601df192ae1eb830f27153884bc
John Bauer <horatio@gmail.com>
2022-05-15 21:07:02 -0700

    Run some basic tests on the dictionary in the ZH tokenizer

--- stanza/tests/tokenization/test_tokenize_data.py
+++ stanza/tests/tokenization/test_tokenize_data.py
@@ -44,6 +44,12 @@ def tokenizer():
     tokenizer = pipeline.processors['tokenize']
     return tokenizer
 
+@pytest.fixture(scope="module")
+def zhtok():
+    pipeline = Pipeline("zh-hans", dir=TEST_MODELS_DIR, download_method=None, processors="tokenize")
+    tokenizer = pipeline.processors['tokenize']
+    return tokenizer
+
 EXPECTED_TWO_NL_RAW = [[('T', 0), ('h', 0), ('i', 0), ('s', 0), (' ', 0), ('i', 0), ('s', 0), (' ', 0), ('a', 0), (' ', 0), ('t', 0), ('e', 0), ('s', 0), ('t', 0)], [('f', 0), ('o', 0), ('o', 0)]]
 # in this test, the newline after test becomes a space labeled 0
 EXPECTED_ONE_NL_RAW = [[('T', 0), ('h', 0), ('i', 0), ('s', 0), (' ', 0), ('i', 0), ('s', 0), (' ', 0), ('a', 0), (' ', 0), ('t', 0), ('e', 0), ('s', 0), ('t', 0), (' ', 0), ('f', 0), ('o', 0), ('o', 0)]]
@@ -104,3 +110,46 @@ def test_convert_units_file(tokenizer):
         batches = DataLoader(tokenizer.config, input_files={'txt': txt_file, 'label': label_file}, vocab=tokenizer.vocab, evaluation=True, dictionary=tokenizer.trainer.dictionary)
         assert batches.data == EXPECTED_ONE_NL_FILE
 
+def test_dictionary(zhtok):
+    """
+    Tests some features of the zh tokenizer dictionary
+    """
+    assert zhtok.trainer.lexicon is not None
+    assert zhtok.trainer.dictionary is not None
+
+    assert "老师" in zhtok.trainer.lexicon
+    # egg-white-stuff, eg protein
+    assert "蛋白质" in zhtok.trainer.lexicon
+    # egg-white
+    assert "蛋白" in zhtok.trainer.dictionary['prefixes']
+    # egg
+    assert "蛋" in zhtok.trainer.dictionary['prefixes']
+    # white-stuff
+    assert "白质" in zhtok.trainer.dictionary['suffixes']
+    # stuff
+    assert "质" in zhtok.trainer.dictionary['suffixes']
+
+def test_dictionary_feats(zhtok):
+    """
+    Test the results of running a sentence into the dictionary featurizer
+    """
+    raw_text = "我想吃蛋白质"
+    batches = DataLoader(zhtok.config, input_text=raw_text, vocab=zhtok.vocab, evaluation=True, dictionary=zhtok.trainer.dictionary)
+    data = batches.data
+    assert len(data) == 1
+    assert len(data[0]) == 6
+
+    expected_features = [
+        # in our example, the 2-grams made by the one character words at the start
+        # don't form any prefixes or suffixes
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+    ]
+
+    for i, expected in enumerate(expected_features):
+        dict_features = batches.extract_dict_feat(data[0], i)
+        assert dict_features == expected
