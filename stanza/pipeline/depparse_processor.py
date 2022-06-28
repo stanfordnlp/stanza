6bb730a07e25aa6f3868001d2a1cd743a460c16b
John Bauer <horatio@gmail.com>
2022-06-28 09:59:08 -0700

    Add a method to extract the known relations from a dependency parser in a pipeline.  This was already possible using the vocab, but this is significantly easier for a user to find.  #1066

--- stanza/pipeline/depparse_processor.py
+++ stanza/pipeline/depparse_processor.py
@@ -4,6 +4,7 @@ Processor for performing dependency parsing
 
 from stanza.models.common import doc
 from stanza.models.common.utils import unsort
+from stanza.models.common.vocab import VOCAB_PREFIX
 from stanza.models.depparse.data import DataLoader
 from stanza.models.depparse.trainer import Trainer
 from stanza.pipeline._constants import *
@@ -33,6 +34,13 @@ class DepparseProcessor(UDProcessor):
     def _set_up_model(self, config, pipeline, use_gpu):
         self._pretrain = pipeline.foundation_cache.load_pretrain(config['pretrain_path']) if 'pretrain_path' in config else None
         self._trainer = Trainer(pretrain=self.pretrain, model_file=config['model_path'], use_cuda=use_gpu)
+
+    def get_known_relations(self):
+        """
+        Return a list of relations which this processor can produce
+        """
+        keys = [k for k in self.vocab['deprel']._unit2id.keys() if k not in VOCAB_PREFIX]
+        return keys
 
     def process(self, document):
         if any(word.upos is None and word.xpos is None for sentence in document.sentences for word in sentence.words):
---
+++ stanza/tests/pipeline/test_pipeline_depparse_processor.py
@@ -0,0 +1,32 @@
+"""
+Basic testing of part of speech tagging
+"""
+
+import pytest
+import stanza
+from stanza.models.common.vocab import VOCAB_PREFIX
+
+from stanza.tests import TEST_MODELS_DIR
+
+pytestmark = [pytest.mark.pipeline, pytest.mark.travis]
+
+@pytest.fixture(scope="module")
+def english_depparse():
+    """
+    Get a depparse_processor for English
+    """
+    nlp = stanza.Pipeline(**{'processors': 'tokenize,pos,lemma,depparse', 'dir': TEST_MODELS_DIR, 'lang': 'en'})
+    assert 'depparse' in nlp.processors
+    return nlp.processors['depparse']
+
+def test_get_known_relations(english_depparse):
+    """
+    Test getting the known relations from a processor.
+
+    Doesn't test that all the relations exist, since who knows what will change in the future
+    """
+    relations = english_depparse.get_known_relations()
+    assert len(relations) > 5
+    assert 'case' in relations
+    for i in VOCAB_PREFIX:
+        assert i not in relations
