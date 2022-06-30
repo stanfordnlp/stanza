e36f78222b2c91c0c705ebc70f5fe6813932c587
unknown <azshan@stanford.edu>
2022-06-30 15:31:30 -0700

    Added more documentation for usage, including necessary spaCy installations

--- stanza/utils/visualization/document_to_viz.py
+++ stanza/utils/visualization/document_to_viz.py
@@ -13,7 +13,7 @@ def visualize_doc(doc, pipeline):
     """
     visualization_options = {"compact": True, "bg": "#09a3d5", "color": "white", "distance": 80,
                              "font": "Source Sans Pro"}
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")   # Must install the latest version of spaCy's en_core_web_sm before running
     sentences_to_visualize = []
     for sentence in doc.sentences:
         words, lemmas, heads, deps, tags = [], [], [], [], []
@@ -28,7 +28,7 @@ def visualize_doc(doc, pipeline):
                     heads.append(sent_len - word.id)
                 else:
                     heads.append(sent_len - word.head)
-        else:
+        else:   # standard left to right rendering
             for word in sentence.words:
                 words.append(word.text)
                 lemmas.append(word.lemma)
@@ -41,7 +41,7 @@ def visualize_doc(doc, pipeline):
         document_result = Doc(nlp.vocab, words=words, lemmas=lemmas, heads=heads, deps=deps, pos=tags)
         sentences_to_visualize.append(document_result)
 
-        for line in sentences_to_visualize:  # render using displaCy
+        for line in sentences_to_visualize:  # render all sentences through displaCy
             displacy.render(line, style="dep", options=visualization_options)
 
 
@@ -58,11 +58,14 @@ def visualize_str(text, pipeline):
 
 def main():
     print("PRINTING ARABIC DOCUMENTS")
-    # example sentences
+    # example sentences in right to left language
     visualize_str('برلين ترفض حصول شركة اميركية على رخصة تصنيع دبابة "ليوبارد" الالمانية', "ar")
     visualize_str("هل بإمكاني مساعدتك؟", "ar")
     visualize_str("أراك في مابعد", "ar")
     visualize_str("لحظة من فضلك", "ar")
+    # example sentences in left to right language
+    visualize_str("This is a sentence.", "en")
+    visualize_str("中国是一个很有意思的国家。", "zh")
 
 
 main()
