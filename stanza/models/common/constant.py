229e409ac2fea1f79d6c7ca72767b119dd21f065
John Bauer <horatio@gmail.com>
2022-07-15 15:05:29 -0700

    Oops, should just return the short_name when this happens

--- stanza/models/common/constant.py
+++ stanza/models/common/constant.py
@@ -385,7 +385,7 @@ def treebank_to_short_name(treebank):
     if treebank in treebank_special_cases:
         return treebank_special_cases.get(treebank)
     if SHORTNAME_RE.match(treebank):
-        short_name = treebank
+        return treebank
 
     if treebank.startswith('UD_'):
         treebank = treebank[3:]
