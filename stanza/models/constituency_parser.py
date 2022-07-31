06cb4cb48acedc7c312f9c3c60980c1d727d41e9
John Bauer <horatio@gmail.com>
2022-07-31 15:27:29 -0700

    Making combining the input the default for the lattn layer

--- stanza/models/constituency_parser.py
+++ stanza/models/constituency_parser.py
@@ -400,7 +400,8 @@ def parse_args(args=None):
     parser.add_argument('--lattn_d_ff', default=2048, type=int, help='Dimension of the Feed-forward layer')
     parser.add_argument('--lattn_relu_dropout', default=0.2, type=float, help='Relu dropout for the label attention')
     parser.add_argument('--lattn_residual_dropout', default=0.2, type=float, help='Residual dropout for the label attention')
-    parser.add_argument('--lattn_combined_input', action='store_true', help='Combine all inputs for the lattn, not just the pattn')
+    parser.add_argument('--lattn_combined_input', default=True, action='store_true', help='Combine all inputs for the lattn, not just the pattn')
+    parser.add_argument('--no_lattn_combined_input', dest='lattn_combined_input', action='store_false', help="Don't combine all inputs for the lattn, not just the pattn")
 
     parser.add_argument('--log_norms', default=False, action='store_true', help='Log the parameters norms while training.  A very noisy option')
     parser.add_argument('--watch_regex', default=None, help='regex to describe which weights and biases to output, if any')
