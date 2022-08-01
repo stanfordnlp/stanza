34ac70e66f8e36a017489aa955127cbee2ed7a2e
John Bauer <horatio@gmail.com>
2022-08-01 00:45:23 -0700

    Add an option for an alternate output directory if needed

--- stanza/utils/charlm/oscar_to_text.py
+++ stanza/utils/charlm/oscar_to_text.py
@@ -10,6 +10,7 @@ python3 -m stanza.utils.charlm.oscar_to_text <path> ...
 each path can be a file or a directory with multiple .jsonl files in it
 """
 
+import argparse
 import glob
 import json
 import lzma
@@ -17,13 +18,19 @@ import os
 import sys
 from stanza.models.common.utils import open_read_text
 
-def extract_file(input_filename):
+def extract_file(output_directory, input_filename):
     print("Extracting %s" % input_filename)
-    json_idx = input_filename.rfind(".jsonl")
-    if json_idx < 0:
-        output_filename = input_filename + ".txt.xz"
+    if output_directory is None:
+        output_directory, output_filename = os.path.split(input_filename)
     else:
-        output_filename = input_filename[:json_idx] + ".txt.xz"
+        _, output_filename = os.path.split(input_filename)
+
+    json_idx = output_filename.rfind(".jsonl")
+    if json_idx < 0:
+        output_filename = output_filename + ".txt.xz"
+    else:
+        output_filename = output_filename[:json_idx] + ".txt.xz"
+    output_filename = os.path.join(output_directory, output_filename)
     print("Writing content to %s" % output_filename)
     with open_read_text(input_filename) as fin:
         with lzma.open(output_filename, "wt") as fout:
@@ -34,13 +41,22 @@ def extract_file(input_filename):
                 fout.write(content)
                 fout.write("\n\n")
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", default=None, help="Output directory for saving files.  If None, will write to the original directory")
+    parser.add_argument("filenames", nargs="+", help="Filenames or directories to process")
+    args = parser.parse_args()
+    return args
+
 def main():
     """
     Go through each of the given filenames or directories, convert json to .txt.xz
     """
-    for filename in sys.argv[1:]:
+    args = parse_args()
+    for filename in args.filenames:
+        os.makedirs(args.output, exist_ok=True)
         if os.path.isfile(filename):
-            extract_file(filename)
+            extract_file(args.output, filename)
         elif os.path.isdir(filename):
             files = glob.glob(os.path.join(filename, "*jsonl*"))
             files = sorted([x for x in files if os.path.isfile(x)])
@@ -48,7 +64,7 @@ def main():
             if len(files) > 0:
                 print("  %s" % "\n  ".join(files))
             for json_filename in files:
-                extract_file(json_filename)
+                extract_file(args.output, json_filename)
 
 if __name__ == "__main__":
     main()
