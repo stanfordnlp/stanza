d2ba168a939666dbe7975d2661c9fa551f5c09ea
John Bauer <horatio@gmail.com>
2022-08-01 23:29:49 -0700

    Automatically download POS (with charlm & wordvec) when redoing depparse

--- stanza/utils/datasets/prepare_depparse_treebank.py
+++ stanza/utils/datasets/prepare_depparse_treebank.py
@@ -11,11 +11,15 @@ Example:
 
 from enum import Enum
 import logging
+import os
 
+from stanza.models import tagger
+from stanza.resources.common import download, DEFAULT_MODEL_DIR
+from stanza.resources.prepare_resources import default_charlms, pos_charlms
 import stanza.utils.datasets.common as common
 import stanza.utils.datasets.prepare_tokenizer_treebank as prepare_tokenizer_treebank
-from stanza.models import tagger
 from stanza.utils.training.run_pos import pos_batch_size, wordvec_args
+from stanza.utils.training.common import add_charlm_args, build_charlm_args, choose_charlm
 
 logger = logging.getLogger('stanza')
 
@@ -36,7 +40,26 @@ def add_specific_args(parser) -> None:
                         help='Use predicted tags for building the depparse data')
     parser.add_argument('--wordvec_pretrain_file', type=str, default=None,
                         help='Exact name of the pretrain file to read')
+    parser.add_argument('--tagger_model', type=str, default=None,
+                        help='Tagger save file to use.  If not specified, order searched will be saved/models, then $STANZA_RESOURCES_DIR')
+    add_charlm_args(parser)
 # fmt: on
+
+def choose_tagger_model(short_language, dataset, tagger_model):
+    """
+    Preferentially chooses a retrained tagger model, but tries to download one if that doesn't exist
+    """
+    if tagger_model:
+        return tagger_model
+
+    save_path = os.path.join("saved_models", "pos", "%s_%s_tagger.pt" % (short_language, dataset))
+    if os.path.exists(save_path):
+        return save_path
+
+    # TODO: just create a Pipeline for the retagging instead?
+    pos_path = os.path.join(DEFAULT_MODEL_DIR, short_language, "pos", dataset + ".pt")
+    download(lang=short_language, package=None, processors={"pos": dataset})
+    return pos_path
 
 
 def process_treebank(treebank, paths, args) -> None:
@@ -45,7 +68,7 @@ def process_treebank(treebank, paths, args) -> None:
         prepare_tokenizer_treebank.copy_conllu_treebank(treebank, paths, paths["DEPPARSE_DATA_DIR"])
     elif args.tag_method is Tags.PREDICTED:
         short_name = common.project_to_short_name(treebank)
-        short_language = short_name.split("_")[0]
+        short_language, dataset = short_name.split("_")
 
         # fmt: off
         base_args = ["--wordvec_dir", paths["WORDVEC_DIR"],
@@ -54,7 +77,18 @@ def process_treebank(treebank, paths, args) -> None:
                      "--batch_size", pos_batch_size(short_name),
                      "--mode", "predict"]
         # fmt: on
-        base_args = base_args + wordvec_args(short_language)
+
+        # perhaps download a tagger if one doesn't already exist
+        tagger_model = choose_tagger_model(short_language, dataset, args.tagger_model)
+        base_args = base_args + ['--save_name', tagger_model]
+
+        # word vector file for POS
+        base_args = base_args + wordvec_args(short_language, dataset, args)
+
+        # charlm for POS
+        charlm = choose_charlm(short_language, dataset, args.charlm, default_charlms, pos_charlms)
+        charlm_args = build_charlm_args(short_language, charlm)
+        base_args = base_args + charlm_args
 
         def retag_dataset(tokenizer_dir, tokenizer_file, dest_dir, dest_file, short_name):
             original = f"{tokenizer_dir}/{short_name}.{tokenizer_file}.conllu"
@@ -64,8 +98,6 @@ def process_treebank(treebank, paths, args) -> None:
                            "--gold_file", original,
                            "--output_file", retagged]
             # fmt: on
-            if args.wordvec_pretrain_file:
-                tagger_args.extend(["--wordvec_pretrain_file", args.wordvec_pretrain_file])
             tagger_args = base_args + tagger_args
             logger.info("Running tagger to retag {} to {}\n  Args: {}".format(original, retagged, tagger_args))
             tagger.main(tagger_args)
