788b2a9cf3d32b79149480073c446a00c729b5be
John Bauer <horatio@gmail.com>
2022-09-14 12:00:33 -0700

    3.9 is a supported version of python now

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
