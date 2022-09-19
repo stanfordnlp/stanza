4ee7f6c305f173245de7345bcb1b4904618e368d
Alex Shan <alexshanvi@gmail.com>
2022-09-19 10:17:00 -0700

    Added sub mapping to task list in case no worker map

--- stanza/utils/payingAnnotators.py
+++ stanza/utils/payingAnnotators.py
@@ -1,24 +1,6 @@
 import json
 import os
 
-# Sample of JSON object returned from AWS:
-# sample_json = {"answers": [{"acceptanceTime": "2022-08-05T06:44:45.745Z", "answerContent": {
-#     "crowd-entity-annotation": {"entities": [{"endOffset": 80, "label": "Miscellaneous", "startOffset": 73}]}},
-#                             "submissionTime": "2022-08-05T06:44:56.111Z", "timeSpentInSeconds": 10.366,
-#                             "workerId": "private.us-east-1.47a0115a1f1786e9",
-#                             "workerMetadata": {"identityData": {"identityProviderType": "Cognito",
-#                                                                 "issuer": "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_5BJRp9lN7",
-#                                                                 "sub": "7efc17ac-3397-4472-afe5-89184ad145d0"}}}]}
-
-# Ties workers to AWS Worker IDs, sample below
-workers = {
-        "7efc17ac-3397-4472-afe5-89184ad145d0": "Worker1",
-        "afce8c28-969c-4e73-a20f-622ef122f585": "Worker2",
-        "91f6236e-63c6-4a84-8fd6-1efbab6dedab": "Worker3",
-        "6f202e93-e6b6-4e1d-8f07-0484b9a9093a": "Worker4",
-        "2b674d33-f656-44b0-8f90-d70a1ab71ec2": "Worker5"
-        }
-
 
 def get_worker_subs(json_string):
     """
@@ -34,11 +16,14 @@ def get_worker_subs(json_string):
     return subs
 
 
-def track_tasks(input_path, worker_map):
+def track_tasks(input_path, worker_map=None):
     """
     Takes a path to a folder containing the worker annotation metadata from AWS Sagemaker labeling job and a
     dictionary mapping AWS worker subs to their names or identification tags and returns a dictionary mapping
     the names/identification tags to the number of labeling tasks completed.
+
+    If no worker map is provided, this function returns a dictionary mapping the worker "sub" fields to
+    the number of tasks they completed.
 
     :param input_path: string of the path to the directory containing the worker annotation sub-directories
     :param worker_map: dictionary mapping AWS worker subs to the worker identifications
@@ -47,19 +32,21 @@ def track_tasks(input_path, worker_map):
     tracker = {}
     res = {}
     for direc in os.listdir(input_path):
-        subdir = os.listdir(input_path + "\\" + direc)
-        json_file_path = input_path + "\\" + direc + "\\" + subdir[0]
-        json_file = open(json_file_path)
-        json_string = json_file.read()
+        subdir_path = os.path.join(input_path, direc)
+        subdir = os.listdir(subdir_path)
+        json_file_path = os.path.join(subdir_path, subdir[0])
+        with open(json_file_path) as json_file:
+            json_string = json_file.read()
         subs = get_worker_subs(json_string)
         for sub in subs:
-            if sub not in tracker:
-                tracker[sub] = 0
-            tracker[sub] += 1
-    for sub in tracker:
-        worker = worker_map[sub]
-        res[worker] = tracker[sub]
-    return res
+            tracker[sub] = tracker.get(sub, 0) + 1
+
+    if worker_map:
+        for sub in tracker:
+            worker = worker_map[sub]
+            res[worker] = tracker[sub]
+        return res
+    return tracker
 
 
 def main():
@@ -72,6 +59,8 @@ def main():
         "2b674d33-f656-44b0-8f90-d70a1ab71ec2": "Worker5"
         }
     ))
+    # sample from completed labeling job -- no worker map provided
+    print(track_tasks('C:\\Users\\Alex\\Desktop\\awscli-labeling'))
     return
 
 
