b1786950a9f8f717e390cfd23fcdd1c947aad795
Keith Hughitt <keith.hughitt@gmail.com>
2022-10-05 20:24:47 -0700

    Added simple test for tokenizer maxlen check

---
+++ stanza/tests/tokenization/test_replace_long_tokens.py
@@ -0,0 +1,16 @@
+"""
+Check to make sure long tokens are replaced with "UNK" by the tokenization processor
+"""
+import pytest
+import stanza
+
+pytestmark = [pytest.mark.pipeline, pytest.mark.travis]
+
+def test_replace_long_tokens():
+    nlp = stanza.Pipeline(lang="en", processors="tokenize")
+
+    test_str = "foo " + "x" * 100000 + " bar"
+
+    res = nlp(test_str)
+
+    assert res.sentences[0].words[1].text == "UNK"
