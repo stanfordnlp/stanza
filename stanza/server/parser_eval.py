19438648d688596c13307d97513fdf17a2bb6a63
John Bauer <horatio@gmail.com>
2022-10-07 23:27:10 -0700

    Add an option to make the parser evaluation a lot quieter

--- stanza/server/parser_eval.py
+++ stanza/server/parser_eval.py
@@ -44,11 +44,14 @@ class EvaluateParser(JavaProtobufContext):
     This is a context window which keeps a process open.  Should allow
     for multiple requests without launching new java processes each time.
     """
-    def __init__(self, classpath=None, kbest=None):
+    def __init__(self, classpath=None, kbest=None, silent=False):
         if kbest is not None:
             extra_args = ["-evalPCFGkBest", "{}".format(kbest), "-evals", "pcfgTopK"]
         else:
             extra_args = []
+
+        if silent:
+            extra_args.extend(["-evals", "summary=False"])
 
         super(EvaluateParser, self).__init__(classpath, EvaluateParserResponse, EVALUATE_JAVA, extra_args=extra_args)
 
