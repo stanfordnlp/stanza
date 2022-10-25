c4c790552d6881af5e837aed269d7ef551897a13
John Bauer <horatio@gmail.com>
2022-10-24 18:31:20 -0700

    Code changes to make the demo work - can refactor things to make loading and saving easier

--- stanza/models/constituency/trans_lm.py
+++ stanza/models/constituency/trans_lm.py
@@ -20,6 +20,7 @@ class TransformerModel(nn.Module):
                  nlayers: int, dropout: float = 0.5):
         super().__init__()
         self.model_type = 'Transformer'
+        self.ntokens = ntoken
         self.pos_encoder = PositionalEncoding(d_model, dropout)
         encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
@@ -78,12 +79,12 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+def data_process(vocab, tokenizer, raw_text_iter: dataset.IterableDataset) -> Tensor:
     """Converts raw text into a flat Tensor."""
     data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
     return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
 
-def batchify(data: Tensor, bsz: int) -> Tensor:
+def batchify(device, data: Tensor, bsz: int) -> Tensor:
     """Divides the data into bsz separate sequences, removing extra elements
     that wouldn't cleanly fit.
 
@@ -115,21 +116,21 @@ def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
     target = source[i+1:i+1+seq_len].reshape(-1)
     return data, target
 
-def train(model: nn.Module) -> None:
+def train(criterion, optimizer, scheduler, epoch, device, train_data, model: nn.Module) -> None:
     model.train()  # turn on train mode
     total_loss = 0.
     log_interval = 200
     start_time = time.time()
-    src_mask = generate_square_subsequent_mask(bptt).to(device)
+    src_mask = generate_square_subsequent_mask(BPTT).to(device)
 
-    num_batches = len(train_data) // bptt
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+    num_batches = len(train_data) // BPTT
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, BPTT)):
         data, targets = get_batch(train_data, i)
         seq_len = data.size(0)
-        if seq_len != bptt:  # only on last batch
+        if seq_len != BPTT:  # only on last batch
             src_mask = src_mask[:seq_len, :seq_len]
         output = model(data, src_mask)
-        loss = criterion(output.view(-1, ntokens), targets)
+        loss = criterion(output.view(-1, model.ntokens), targets)
 
         optimizer.zero_grad()
         loss.backward()
@@ -148,18 +149,18 @@ def train(model: nn.Module) -> None:
             total_loss = 0
             start_time = time.time()
 
-def evaluate(model: nn.Module, eval_data: Tensor) -> float:
+def evaluate(criterion, device, model: nn.Module, eval_data: Tensor) -> float:
     model.eval()  # turn on evaluation mode
     total_loss = 0.
-    src_mask = generate_square_subsequent_mask(bptt).to(device)
+    src_mask = generate_square_subsequent_mask(BPTT).to(device)
     with torch.no_grad():
-        for i in range(0, eval_data.size(0) - 1, bptt):
+        for i in range(0, eval_data.size(0) - 1, BPTT):
             data, targets = get_batch(eval_data, i)
             seq_len = data.size(0)
-            if seq_len != bptt:
+            if seq_len != BPTT:
                 src_mask = src_mask[:seq_len, :seq_len]
             output = model(data, src_mask)
-            output_flat = output.view(-1, ntokens)
+            output_flat = output.view(-1, model.ntokens)
             total_loss += seq_len * criterion(output_flat, targets).item()
     return total_loss / (len(eval_data) - 1)
 
@@ -174,17 +175,17 @@ def main():
     # train_iter was "consumed" by the process of building the vocab,
     # so we have to create it again
     train_iter, val_iter, test_iter = WikiText2()
-    train_data = data_process(train_iter)
-    val_data = data_process(val_iter)
-    test_data = data_process(test_iter)
+    train_data = data_process(vocab, tokenizer, train_iter)
+    val_data = data_process(vocab, tokenizer, val_iter)
+    test_data = data_process(vocab, tokenizer, test_iter)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     batch_size = 20
     eval_batch_size = 10
-    train_data = batchify(train_data, batch_size)  # shape [seq_len, batch_size]
-    val_data = batchify(val_data, eval_batch_size)
-    test_data = batchify(test_data, eval_batch_size)
+    train_data = batchify(device, train_data, batch_size)  # shape [seq_len, batch_size]
+    val_data = batchify(device, val_data, eval_batch_size)
+    test_data = batchify(device, test_data, eval_batch_size)
 
     ntokens = len(vocab)  # size of vocabulary
     emsize = 200  # embedding dimension
@@ -205,8 +206,8 @@ def main():
 
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
-        train(model)
-        val_loss = evaluate(model, val_data)
+        train(criterion, optimizer, scheduler, epoch, device, train_data, model)
+        val_loss = evaluate(criterion, device, model, val_data)
         val_ppl = math.exp(val_loss)
         elapsed = time.time() - epoch_start_time
         print('-' * 89)
@@ -220,7 +221,7 @@ def main():
 
         scheduler.step()
 
-    test_loss = evaluate(best_model, test_data)
+    test_loss = evaluate(criterion, device, model, test_data)
     test_ppl = math.exp(test_loss)
     print('=' * 89)
     print(f'| End of training | test loss {test_loss:5.2f} | '
