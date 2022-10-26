afb1efc4d662d6d227c9987631b676ac537ca9b2
SecroLoL <alexshanvi@gmail.com>
2022-10-25 22:44:53 -0700

    Dumping out useless print statements and import statements

--- stanza/tests/ner/test_pay_amt_annotators.py
+++ stanza/tests/ner/test_pay_amt_annotators.py
@@ -1,10 +1,10 @@
 from stanza.utils import payingAnnotators
-import os
+
 """
 Simple test for tracking AMT annotator work
 """
 
-print(os.getcwd())
+
 def test_amt_annotator_track():
     completed_amt_job_metadata = '.\\awscli-labeling'
     workers = {
@@ -20,7 +20,7 @@ def test_amt_annotator_track():
 
 
 def test_amt_annotator_track_no_map():
-    completed_amt_job_metadata = '.\\awscli-labeling'  
+    completed_amt_job_metadata = '.\\awscli-labeling'
     sub_to_count = payingAnnotators.track_tasks(completed_amt_job_metadata)
     assert sub_to_count == {'6f202e93-e6b6-4e1d-8f07-0484b9a9093a': 948, '2b674d33-f656-44b0-8f90-d70a1ab71ec2': 865,
             'afce8c28-969c-4e73-a20f-622ef122f585': 134, '91f6236e-63c6-4a84-8fd6-1efbab6dedab': 735,
