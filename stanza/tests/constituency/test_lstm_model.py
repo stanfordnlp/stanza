5fd6ff3667e5dc931436f6c8dca1c2d722a82bf3
John Bauer <horatio@gmail.com>
2022-11-01 13:28:50 -0700

    lattn_partitioned == False should affect the input proj dimension as well

--- stanza/tests/constituency/test_lstm_model.py
+++ stanza/tests/constituency/test_lstm_model.py
@@ -271,6 +271,9 @@ def test_lattn_projection(pretrain_file):
         model = build_model(pretrain_file, '--pattn_d_model', '1024', '--lattn_d_proj', '64', '--lattn_d_l', '16', '--lattn_d_input_proj', '256')
         run_forward_checks(model)
 
+    model = build_model(pretrain_file, '--pattn_d_model', '1024', '--lattn_d_proj', '64', '--lattn_d_l', '16', '--no_lattn_partitioned', '--lattn_d_input_proj', '256')
+    run_forward_checks(model)
+
     model = build_model(pretrain_file, '--lattn_d_proj', '64', '--lattn_d_l', '16', '--lattn_d_input_proj', '768')
     run_forward_checks(model)
 
