e65bdef79e12693eadaa61c612e1c99431ad0765
John Bauer <horatio@gmail.com>
2023-01-10 00:29:29 -0800

    Add a more useful error when the pipe (process pipe, not NLP pipeline) is not initialized

--- stanza/server/java_protobuf_requests.py
+++ stanza/server/java_protobuf_requests.py
@@ -167,6 +167,7 @@ class JavaProtobufContext(object):
         if extra_args is None:
             extra_args = []
         self.extra_args = extra_args
+        self.pipe = None
 
     def open_pipe(self):
         self.pipe = subprocess.Popen(["java", "-cp", self.classpath, self.java_main, "-multiple"] + self.extra_args,
@@ -177,6 +178,7 @@ class JavaProtobufContext(object):
         if self.pipe.poll() is None:
             self.pipe.stdin.write((0).to_bytes(4, 'big'))
             self.pipe.stdin.flush()
+            self.pipe = None
 
     def __enter__(self):
         self.open_pipe()
@@ -186,6 +188,9 @@ class JavaProtobufContext(object):
         self.close_pipe()
 
     def process_request(self, request):
+        if self.pipe is None:
+            raise RuntimeError("Pipe to java process is not open or was closed")
+
         text = request.SerializeToString()
         self.pipe.stdin.write(len(text).to_bytes(4, 'big'))
         self.pipe.stdin.write(text)
