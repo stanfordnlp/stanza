f1caf90b1cea23dcf1c8f53d1e24c601fdd2273f
Alex Shan <alexshanvi@gmail.com>
2023-01-25 22:17:14 -0800

    Fixing dir paths to make tests run, import errors fixed

--- stanza/tests/ner/test_pay_amt_annotators.py
+++ stanza/tests/ner/test_pay_amt_annotators.py
@@ -2,11 +2,13 @@ from stanza.utils import payingAnnotators
 
 """
 Simple test for tracking AMT annotator work
+
+TODO: Update paths for general use not just my desktop
 """
 
 
 def test_amt_annotator_track():
-    completed_amt_job_metadata = '.\\aws_labeling_copy'
+    completed_amt_job_metadata = 'C:\\Users\\SystemAdmin\\PycharmProjects\\stanza\\stanza\\tests\\ner\\aws_labeling_copy'
     workers = {
         "7efc17ac-3397-4472-afe5-89184ad145d0": "Worker1",
         "afce8c28-969c-4e73-a20f-622ef122f585": "Worker2",
@@ -20,7 +22,7 @@ def test_amt_annotator_track():
 
 
 def test_amt_annotator_track_no_map():
-    completed_amt_job_metadata = '.\\awscli-labeling'
+    completed_amt_job_metadata = 'C:\\Users\\SystemAdmin\\PycharmProjects\\stanza\\stanza\\tests\\ner\\aws_labeling_copy'
     sub_to_count = payingAnnotators.track_tasks(completed_amt_job_metadata)
     assert sub_to_count == {'6f202e93-e6b6-4e1d-8f07-0484b9a9093a': 20, '2b674d33-f656-44b0-8f90-d70a1ab71ec2': 20,
                             'afce8c28-969c-4e73-a20f-622ef122f585': 3, '91f6236e-63c6-4a84-8fd6-1efbab6dedab': 16}
@@ -33,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print("TESTS COMPLETED!")
