d9d937eb5072eae5e0ffd0856d8c6b013e084084
John Bauer <horatio@gmail.com>
2023-02-01 00:13:04 -0800

    Move payingAnnotators to an NER utils directory (and make the name python-case)

--- stanza/tests/ner/test_pay_amt_annotators.py
+++ stanza/tests/ner/test_pay_amt_annotators.py
@@ -7,8 +7,8 @@ import zipfile
 
 import pytest
 
-from stanza.utils import payingAnnotators
 from stanza.tests import TEST_WORKING_DIR
+from stanza.utils.ner import paying_annotators
 
 DATA_SOURCE = os.path.join(TEST_WORKING_DIR, "in", "aws_annotations.zip")
 
@@ -30,12 +30,12 @@ def test_amt_annotator_track(completed_amt_job_metadata):
         "2b674d33-f656-44b0-8f90-d70a1ab71ec2": "Worker5"
     }  # map AMT annotator subs to relevant identifier
 
-    tracked_work = payingAnnotators.track_tasks(completed_amt_job_metadata, workers)
+    tracked_work = paying_annotators.track_tasks(completed_amt_job_metadata, workers)
     assert tracked_work == {'Worker4': 20, 'Worker5': 20, 'Worker2': 3, 'Worker3': 16}
 
 
 def test_amt_annotator_track_no_map(completed_amt_job_metadata):
-    sub_to_count = payingAnnotators.track_tasks(completed_amt_job_metadata)
+    sub_to_count = paying_annotators.track_tasks(completed_amt_job_metadata)
     assert sub_to_count == {'6f202e93-e6b6-4e1d-8f07-0484b9a9093a': 20, '2b674d33-f656-44b0-8f90-d70a1ab71ec2': 20,
                             'afce8c28-969c-4e73-a20f-622ef122f585': 3, '91f6236e-63c6-4a84-8fd6-1efbab6dedab': 16}
 
