c5ac47864f367d07cead24313510676ac05defd4
John Bauer <horatio@gmail.com>
2023-02-01 00:07:09 -0800

    Zip up all of the AMT sample data, then expand it at the start of test_pay_amt_annotators.py

--- stanza/tests/ner/test_pay_amt_annotators.py
+++ stanza/tests/ner/test_pay_amt_annotators.py
@@ -1,14 +1,27 @@
-from stanza.utils import payingAnnotators
-
 """
 Simple test for tracking AMT annotator work
-
-TODO: Update paths for general use not just my desktop
 """
 
+import os
+import zipfile
 
-def test_amt_annotator_track():
-    completed_amt_job_metadata = 'C:\\Users\\SystemAdmin\\PycharmProjects\\stanza\\stanza\\tests\\ner\\aws_labeling_copy'
+import pytest
+
+from stanza.utils import payingAnnotators
+from stanza.tests import TEST_WORKING_DIR
+
+DATA_SOURCE = os.path.join(TEST_WORKING_DIR, "in", "aws_annotations.zip")
+
+@pytest.fixture(scope="module")
+def completed_amt_job_metadata(tmp_path_factory):
+    assert os.path.exists(DATA_SOURCE)
+    unzip_path = tmp_path_factory.mktemp("amt_test")
+    input_path = unzip_path / "ner" / "aws_labeling_copy"
+    with zipfile.ZipFile(DATA_SOURCE, 'r') as zin:
+        zin.extractall(unzip_path)
+    return input_path
+
+def test_amt_annotator_track(completed_amt_job_metadata):
     workers = {
         "7efc17ac-3397-4472-afe5-89184ad145d0": "Worker1",
         "afce8c28-969c-4e73-a20f-622ef122f585": "Worker2",
@@ -21,8 +34,7 @@ def test_amt_annotator_track():
     assert tracked_work == {'Worker4': 20, 'Worker5': 20, 'Worker2': 3, 'Worker3': 16}
 
 
-def test_amt_annotator_track_no_map():
-    completed_amt_job_metadata = 'C:\\Users\\SystemAdmin\\PycharmProjects\\stanza\\stanza\\tests\\ner\\aws_labeling_copy'
+def test_amt_annotator_track_no_map(completed_amt_job_metadata):
     sub_to_count = payingAnnotators.track_tasks(completed_amt_job_metadata)
     assert sub_to_count == {'6f202e93-e6b6-4e1d-8f07-0484b9a9093a': 20, '2b674d33-f656-44b0-8f90-d70a1ab71ec2': 20,
                             'afce8c28-969c-4e73-a20f-622ef122f585': 3, '91f6236e-63c6-4a84-8fd6-1efbab6dedab': 16}
