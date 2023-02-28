133cd5ee7becdd1f949909ce314fc6231f7b65f1
John Bauer <horatio@gmail.com>
2023-02-28 08:50:31 -0800

    Test that passing in a lang_config with processors specifically for EN turns off unwanted processors for that language

--- stanza/tests/langid/test_multilingual.py
+++ stanza/tests/langid/test_multilingual.py
@@ -10,7 +10,7 @@ from stanza.tests import TEST_MODELS_DIR
 
 pytestmark = [pytest.mark.pipeline, pytest.mark.travis]
 
-def run_multilingual_pipeline(**kwargs):
+def run_multilingual_pipeline(en_has_dependencies=True, fr_has_dependencies=True, **kwargs):
     english_text = "This is an English sentence."
     english_words = ["This", "is", "an", "English", "sentence", "."]
     english_deps_gold = "\n".join((
@@ -33,20 +33,26 @@ def run_multilingual_pipeline(**kwargs):
         "('.', 4, 'punct')"
     ))
 
-    lang_configs = {"en": {"processors": "tokenize,pos,lemma,depparse"},
-                    "fr": {"processors": "tokenize,pos,lemma,depparse"}}
-    nlp = MultilingualPipeline(model_dir=TEST_MODELS_DIR, lang_configs=lang_configs, **kwargs)
+    if 'lang_configs' in kwargs:
+        nlp = MultilingualPipeline(model_dir=TEST_MODELS_DIR, **kwargs)
+    else:
+        lang_configs = {"en": {"processors": "tokenize,pos,lemma,depparse"},
+                        "fr": {"processors": "tokenize,pos,lemma,depparse"}}
+        nlp = MultilingualPipeline(model_dir=TEST_MODELS_DIR, lang_configs=lang_configs, **kwargs)
     docs = [english_text, french_text]
     docs = nlp(docs)
 
     assert docs[0].lang == "en"
     assert len(docs[0].sentences) == 1
     assert [x.text for x in docs[0].sentences[0].words] == english_words
-    assert docs[0].sentences[0].dependencies_string() == english_deps_gold
+    if en_has_dependencies:
+        assert docs[0].sentences[0].dependencies_string() == english_deps_gold
+
     assert len(docs[1].sentences) == 1
     assert docs[1].lang == "fr"
     assert [x.text for x in docs[1].sentences[0].words] == french_words
-    assert docs[1].sentences[0].dependencies_string() == french_deps_gold
+    if fr_has_dependencies:
+        assert docs[1].sentences[0].dependencies_string() == french_deps_gold
 
 
 def test_multilingual_pipeline():
@@ -60,3 +66,14 @@ def test_multilingual_pipeline_small_cache():
     Test with the cache size 1
     """
     run_multilingual_pipeline(max_cache_size=1)
+
+
+def test_multilingual_config():
+    """
+    Test with only tokenize for the EN pipeline
+    """
+    lang_configs = {
+        "en": {"processors": "tokenize"}
+    }
+
+    run_multilingual_pipeline(en_has_dependencies=False, lang_configs=lang_configs)
