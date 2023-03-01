a86018024aa2e2351c8851453799f5a788877901
Alex Shan <alexshanvi@gmail.com>
2023-02-28 17:08:37 -0800

    Moved files into stanza/utils/visualization/utils.py, remove from here for consistency

--- stanza/utils/find_nth.py
+++
@@ -1,9 +0,0 @@
-def find_nth(haystack, needle, n):
-    """
-    Returns the starting index of the nth occurrence of the substring 'needle' in the string 'haystack'.
-    """
-    start = haystack.find(needle)
-    while start >= 0 and n > 1:
-        start = haystack.find(needle, start + len(needle))
-        n -= 1
-    return start
--- stanza/utils/round_base.py
+++
@@ -1,5 +0,0 @@
-def round_base(num, base=10):
-    """
-    Rounding a number to its nearest multiple of the base. round_base(49.2, base=50) = 50.
-    """
-    return base * round(num / base)
