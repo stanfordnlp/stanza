349b56c7d46b7de28cd62be59f6131cc1e17d487
John Bauer <horatio@gmail.com>
2023-04-17 23:41:24 -0700

    Add minimum requirement for protobuf... versions lower than this don't import correctly

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     install_requires=[
         'emoji', 
         'numpy', 
-        'protobuf', 
+        'protobuf>=3.15.0',
         'requests', 
         'six', 
         'torch>=1.3.0',
