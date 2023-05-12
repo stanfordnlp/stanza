032d2addb4894a4bc349af8b20e09c84b505f9e1
John Bauer <horatio@gmail.com>
2023-05-12 11:59:27 -0700

    Whitespace

--- stanza/models/ner_tagger.py
+++ stanza/models/ner_tagger.py
@@ -231,8 +231,8 @@ def train(args):
     if args['lr_decay'] > 0:
         # learning rate changes on plateau -- no improvement on model for patience number of epochs
         # change is made as a factor of the learning rate decay
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(trainer.optimizer, mode='max', factor=args['lr_decay'], \
-            patience=args['patience'], verbose=True, min_lr=args['min_lr'])
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(trainer.optimizer, mode='max', factor=args['lr_decay'],
+                                                               patience=args['patience'], verbose=True, min_lr=args['min_lr'])
     else:
         scheduler = None
 
