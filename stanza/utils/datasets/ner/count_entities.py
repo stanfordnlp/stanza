48149f80fa8c8ae27433e401176570de5bf84104
John Bauer <horatio@gmail.com>
2023-08-29 23:03:57 -0700

    Output the numbers of tokens in a doc when using count_entities on an NER dataset

--- stanza/utils/datasets/ner/count_entities.py
+++ stanza/utils/datasets/ner/count_entities.py
@@ -1,8 +1,10 @@
 
 import argparse
 from collections import defaultdict
+import json
 
-from stanza.utils.datasets.ner.utils import read_json_entities
+from stanza.models.common.doc import Document
+from stanza.utils.datasets.ner.utils import list_doc_entities
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Report the coverage of one NER file on another.")
@@ -15,7 +17,11 @@ def count_entities(*filenames):
     entity_collection = defaultdict(list)
 
     for filename in filenames:
-        entities = read_json_entities(filename)
+        with open(filename) as fin:
+            doc = Document(json.load(fin))
+            num_tokens = sum(1 for sentence in doc.sentences for token in sentence.tokens)
+            print("Number of tokens in %s: %d" % (filename, num_tokens))
+            entities = list_doc_entities(doc)
 
         for ent in entities:
             entity_collection[ent[1]].append(ent[0])
