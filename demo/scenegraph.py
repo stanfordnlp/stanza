b9b2b4e81e475ecfe22ef7b3794925ad34bfcce3
John Bauer <horatio@gmail.com>
2023-09-13 18:25:56 -0700

    Pretty print the results and avoid wastefully loading the annotators

--- demo/scenegraph.py
+++ demo/scenegraph.py
@@ -4,12 +4,17 @@ Very short demo for the SceneGraph interface in the CoreNLP server
 Requires CoreNLP >= 4.5.5, Stanza >= 1.5.1
 """
 
+import json
+
 from stanza.server import CoreNLPClient
 
-# start_server=None is if you have the server running in another process on the same host
+# start_server=None if you have the server running in another process on the same host
 # you can start it with whatever normal options CoreNLPClient has
-with CoreNLPClient(start_server=None) as client:
+#
+# preload=False avoids having the server unnecessarily load annotators
+# if you don't plan on using them
+with CoreNLPClient(preload=False) as client:
     result = client.scenegraph("Jennifer's antennae are on her head.")
-    print(result)
+    print(json.dumps(result, indent=2))
 
 
