14d618c1656bae25426986feeb1225bb5f132986
John Bauer <horatio@gmail.com>
2023-09-18 17:36:39 -0700

    Rather than looking for default packages in the default_processors dict, look for packages of any package name in the packages dict in the resources file.  This will make it possible to create packages of all different names, not just default

--- stanza/resources/common.py
+++ stanza/resources/common.py
@@ -17,9 +17,9 @@ import zipfile
 from tqdm.auto import tqdm
 
 from stanza.utils.helper_func import make_table
-from stanza.pipeline._constants import TOKENIZE, MWT, POS, LEMMA, DEPPARSE, \
-    NER, SENTIMENT
+from stanza.pipeline._constants import TOKENIZE, MWT, POS, LEMMA, DEPPARSE, NER, SENTIMENT
 from stanza.pipeline.registry import PIPELINE_NAMES, PROCESSOR_VARIANTS
+from stanza.resources.default_packages import PACKAGES
 from stanza._version import __resources_version__
 
 logger = logging.getLogger('stanza')
@@ -181,12 +181,10 @@ def add_mwt(processors, resources, lang):
     TODO: how does this handle EWT in English?
     """
     value = processors[TOKENIZE]
-    if value == "default" and MWT in resources[lang]['default_processors']:
-        logger.warning("Language %s package default expects mwt, which has been added", lang)
-        processors[MWT] = 'default'
-    elif (value in resources[lang][TOKENIZE]
-          and MWT in resources[lang]
-          and value in resources[lang][MWT]):
+    if value in resources[lang][PACKAGES] and MWT in resources[lang][PACKAGES][value]:
+        logger.warning("Language %s package %s expects mwt, which has been added", lang, value)
+        processors[MWT] = value
+    elif (value in resources[lang][TOKENIZE] and MWT in resources[lang] and value in resources[lang][MWT]):
         logger.warning("Language %s package %s expects mwt, which has been added", lang, value)
         processors[MWT] = value
 
@@ -224,11 +222,11 @@ def maintain_processor_list(resources, lang, package, processors, allow_pretrain
                     logger.debug(f'Found {key}: {value}.')
                     processor_list[key].append(value)
                 # allow values to be default in some cases
-                elif key in resources[lang].get('default_processors', {}) and value == 'default':
+                elif value in resources[lang][PACKAGES] and key in resources[lang][PACKAGES][value]:
                     logger.debug(
-                        f'Found {key}: {resources[lang]["default_processors"][key]}.'
+                        f'Found {key}: {resources[lang][PACKAGES][value][key]}.'
                     )
-                    processor_list[key].append(resources[lang]['default_processors'][key])
+                    processor_list[key].append(resources[lang][PACKAGES][value][key])
                 # allow processors to be set to variants that we didn't implement
                 elif value in PROCESSOR_VARIANTS[key]:
                     logger.debug(
@@ -258,8 +256,8 @@ def maintain_processor_list(resources, lang, package, processors, allow_pretrain
     # resolve package
     if package:
         logger.debug(f'Processing parameter "package"...')
-        if package == 'default':
-            for key, value in resources[lang]['default_processors'].items():
+        if package in resources[lang][PACKAGES]:
+            for key, value in resources[lang][PACKAGES][package].items():
                 if key not in processor_list:
                     logger.debug(f'Found {key}: {value}.')
                     processor_list[key].append(value)
