864dfba1628fffcaba90af64ec841741e60d3b52
John Bauer <horatio@gmail.com>
2023-10-02 22:21:02 -0700

    Add an argument to prepare_resources.py to prepare only one languages at a time

--- stanza/resources/prepare_resources.py
+++ stanza/resources/prepare_resources.py
@@ -31,6 +31,7 @@ def parse_args():
     parser.add_argument('--input_dir', type=str, default="/u/nlp/software/stanza/models/current-models-%s" % __resources_version__, help='Input dir for various models.  Defaults to the recommended home on the nlp cluster')
     parser.add_argument('--output_dir', type=str, default="/u/nlp/software/stanza/models/%s" % __resources_version__, help='Output dir for various models.')
     parser.add_argument('--packages_only', action='store_true', default=False, help='Only build the package maps instead of rebuilding everything')
+    parser.add_argument('--lang', type=str, default=None, help='Only process this language.  If left blank, will prepare all languages.  To use this argument, a previous prepared resources with all of the languages is necessary.')
     args = parser.parse_args()
     args.input_dir = os.path.abspath(args.input_dir)
     args.output_dir = os.path.abspath(args.output_dir)
@@ -331,6 +332,12 @@ def get_dependencies(processor, lang, package):
 def process_dirs(args):
     dirs = sorted(os.listdir(args.input_dir))
     resources = {}
+    if args.lang:
+        resources = json.load(open(os.path.join(args.output_dir, 'resources.json')))
+        # this one language gets overridden
+        # if this is not done, and we reuse the old resources,
+        # any models which were deleted will still be in the resources
+        resources[args.lang] = {}
 
     for model_dir in dirs:
         print(f"Processing models in {model_dir}")
@@ -339,6 +346,9 @@ def process_dirs(args):
             if not model.endswith('.pt'): continue
             # get processor
             lang, package, processor = split_model_name(model)
+            if args.lang and lang != args.lang:
+                continue
+
             # copy file
             input_path = os.path.join(args.input_dir, model_dir, model)
             output_path = os.path.join(args.output_dir, lang, "models", processor, package + '.pt')
@@ -385,6 +395,9 @@ def process_default_zips(args):
             continue
         if lang not in default_treebanks:
             raise AssertionError(f'{lang} not in default treebanks!!!')
+
+        if args.lang and lang != args.lang:
+            continue
 
         print(f'Preparing default models for language {lang}')
 
@@ -547,6 +560,9 @@ def process_packages(args):
             continue
         if lang not in default_treebanks:
             raise AssertionError(f'{lang} not in default treebanks!!!')
+
+        if args.lang and lang != args.lang:
+            continue
 
         default_processors = get_default_processors(resources, lang)
 
