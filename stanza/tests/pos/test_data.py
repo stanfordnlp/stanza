414ec45acc56ddacb62f0b6cf573938cdcb177b2
John Bauer <horatio@gmail.com>
2023-10-07 13:39:38 -0700

    Small test of reading a basic data file for the POS tagger

--- stanza/tests/pos/test_data.py
+++ stanza/tests/pos/test_data.py
@@ -10,7 +10,22 @@ from stanza.models import tagger
 from stanza.models.pos.data import DataLoader
 from stanza.utils.conll import CoNLL
 
-from stanza.tests.pos.test_tagger import TRAIN_DATA_NO_XPOS, TRAIN_DATA_NO_UPOS, TRAIN_DATA_NO_FEATS
+from stanza.tests.pos.test_tagger import TRAIN_DATA, TRAIN_DATA_NO_XPOS, TRAIN_DATA_NO_UPOS, TRAIN_DATA_NO_FEATS
+
+def test_basic_reading():
+    """
+    Test that a dataset with no xpos is detected by the DataLoader
+    """
+    # empty args for building the data object
+    args = tagger.parse_args(args=[])
+
+    train_data, _ = CoNLL.conll2dict(input_str=TRAIN_DATA)
+    train_doc = Document(train_data)
+
+    data = DataLoader(train_doc, args['batch_size'], args, None)
+    assert data.has_upos
+    assert data.has_xpos
+    assert data.has_feats
 
 def test_no_xpos():
     """
