c7b77c89ef75ae00a60a454bcab4467efff3a66c
Alex Shan <alexshanvi@gmail.com>
2023-11-08 17:10:41 -0800

    Overhaul model evaluation to decompose functions. Score by sequences. Update f1 scoring to become multi-class

--- stanza/models/lemma_classifier/evaluate_models.py
+++ stanza/models/lemma_classifier/evaluate_models.py
@@ -9,79 +9,59 @@ sys.path.append(parentdir)
 
 import stanza
 from typing import Any, List, Tuple
-from models.lemma_classifier.baseline_model import BaselineModel
 import utils
-
-# TODO: make this code more modular. No magic constants without a constants file.
-
-class Evaluator():
-
-    def __init__(self, target_word, target_upos, lemma_choices) -> None:
-        self.target_word = target_word
-        self.target_upos = target_upos
-        self.lemma_choices = lemma_choices
-        if len(lemma_choices) != 2: # TODO: Update this to handle arbitrarily many
-            raise ValueError(f"Lemma choices are limited to exactly two. You gave {len(lemma_choices)}: {lemma_choices}")
+from collections import defaultdict
 
 
-    def update_counts(self, gold_tag: str, pred_tag: str, true_pos: int, false_pos: int, false_neg: int) -> Tuple[int, int, int]:
-        """"
-        Takes in a prediction along with the counts for true positive, false positive and false negative and updates the counts
-        of the measurements according to the prediction.
+def evaluate_sequences(gold_tag_sequences, pred_tag_sequences, verbose=True):
+    """
+    Evaluates a model's predicted tags against a set of gold tags. Computes precision, recall, and f1 for all classes.
 
-        TODO: generalize the way positive and negative lemmas are treated. currently we limit two 2 lemmas but this could be different
-        """
-        lemma_1, lemma_2 = self.lemma_choices[0], self.lemma_choices[1]
-        if gold_tag == lemma_1 and pred_tag == lemma_1:
-            true_pos += 1
-        elif gold_tag == lemma_1 and pred_tag == lemma_2: 
-            false_neg += 1
-        elif gold_tag == lemma_2 and pred_tag == lemma_1:
-            false_pos += 1
-        return true_pos, false_pos, false_neg
+    Precision = true positives / true positives + false positives
+    Recall = true positives / true positives + false negatives
+    F1 = 2 * (Precision * Recall) / (Precision + Recall)
+    """
+    assert len(gold_tag_sequences) == len(pred_tag_sequences), \
+    f"Length of gold tag sequences is {len(gold_tag_sequences)}, while length of predicted tag sequence is {len(pred_tag_sequences)}"        
+    
+    confusion = defaultdict(lambda: defaultdict(int))
+    
+    for gold_tags, pred_tags in zip(gold_tag_sequences, pred_tag_sequences):
 
+        assert len(gold_tags) == len(pred_tags), f"Number of gold tags doesn't match number of predicted tags ({len(gold_tags)}, {len(pred_tags)})"
+        for gold, pred in zip(gold_tags, pred_tags):
+            confusion[gold][pred] += 1
 
-    def evaluate_models(self, eval_path: str, binary_classifier: Any, baseline_classifier: BaselineModel) -> Tuple[dict, dict]:
-        """
-        Evaluates both the binary classifier and baseline classifier on a test file,
-        checking the predicted lemmas for each "'s" token against the gold lemma.
+    multi_class_result = defaultdict(lambda: defaultdict(float))
+    # compute precision, recall and f1 for each class and store inside of `multi_class_result`
+    for gold_tag in confusion.keys():
 
-        Precision = true positives / true positives + false positives
-        Recall = true positives / true positives + false negatives
-        F1 = 2 * (Precision * Recall) / (Precision + Recall)
-        """
-        gold_doc = utils.load_doc_from_conll_file(eval_path)
-
-        bin_tp, bin_fp, bin_fn = 0, 0, 0
-        bl_tp, bl_fp, bl_fn = 0, 0, 0  # baseline counts
-
-        for sentence in gold_doc.sentences:
-            for word in sentence.words:
-                if word.text == self.target_word and word.upos in self.target_upos:   # only evaluate when the UPOS tag is AUX
-                    gold_tag = word.lemma
-                    # predict binary classifier
-                    bin_predict = None  # TODO
-                    # predict baseline classifier
-                    baseline_predict = baseline_classifier.predict(word.text)  # TODO
-                    
-                    # score binary classifier
-                    bin_tp, bin_fp, bin_fn = self.update_counts(gold_tag, bin_predict, bin_tp, bin_fp, bin_fn)
-                    bl_tp, bl_fp, bl_fn = self.update_counts(gold_tag, baseline_predict, bl_tp, bl_fp, bl_fn)
-
-        # compute precision, recall, f1
-        bin_precision, bin_recall =  bin_tp / (bin_tp + bin_fp), bin_tp / (bin_tp + bin_fn)
-        bin_results = {"precision":  bin_precision,
-                        "recall": bin_recall,
-                        "f1": 2 * (bin_precision * bin_recall) / (bin_precision + bin_recall)
-                   }
-
-        bl_precision, bl_recall =  bl_tp / (bl_tp + bl_fp), bl_tp / (bl_tp + bl_fn)
-        bl_results = {"precision":  bl_precision,
-                        "recall": bl_recall,
-                        "f1": 2 * (bl_precision * bl_recall) / (bl_precision + bl_recall)
-                    }
+        try:
+            prec = confusion.get(gold_tag, {}).get(gold_tag, 0) / sum([confusion.get(k, {}).get(gold_tag, 0) for k in confusion.keys()])
+        except ZeroDivisionError:
+            prec = 0.0 
         
-        return bin_results, bl_results
+        try:
+            recall = confusion.get(gold_tag, {}).get(gold_tag, 0) / sum(confusion.get(gold_tag, {}).values())
+        except ZeroDivisionError:
+            recall = 0.0
+
+        try:
+            f1 = 2 * (prec * recall) / (prec + recall)
+        except ZeroDivisionError:
+            f1 = 0.0 
+
+        multi_class_result[gold_tag] = {
+            "precision": prec,
+            "recall": recall,
+            "f1": f1
+        }
+    
+    if verbose:
+        for lemma in multi_class_result:
+            print(f"Lemma '{lemma}' had precision {100 * multi_class_result[lemma]['precision']}, recall {100 * multi_class_result[lemma]['recall']} and F1 score of {100 * multi_class_result[lemma]['f1']}")
+
+    return multi_class_result, confusion   
 
 
 def main():
@@ -108,11 +88,6 @@ def main():
     print(f"The number of 's found was {count}.")
     print(f"There were {have_count} occurrences of the lemma being 'have'.")
     print(f"There were {be_count} occurrences of the lemma being 'be'.")
-
-    evaluator = Evaluator(target_word="'s", target_upos="AUX", lemma_choices=["be", "have"])
-    bl_model = BaselineModel("'s", "be")
-    bin_results, bl_results = evaluator.evaluate_models(coNLL_path, None, bl_model)
-    print(bin_results, bl_results)
 
 
 if __name__ == "__main__":
