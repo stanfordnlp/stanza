366cf65e036ce9b2e5f17ac1077e4016b2459613
Alex Shan <alexshanvi@gmail.com>
2023-11-12 16:16:30 -0800

    Delete old model code and complete with full init and forward methods

--- stanza/models/lemma_classifier/model.py
+++ stanza/models/lemma_classifier/model.py
@@ -1,106 +1,61 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import utils
 from torchtext.vocab import GloVe
 from torchtext.data import get_tokenizer
+from constants import *
+
 
 # Define a custom model for your binary classifier
 class LemmaClassifier(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, embeddings):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, embeddings, padding_idx = 0):
         super(LemmaClassifier, self).__init__()
 
+        self.embedding_dim = embedding_dim
+
         # Embedding layer with GloVe embeddings
-        self.embedding = nn.Embedding.from_pretrained(embeddings, padding_idx=0)
+        self.embedding = nn.Embedding.from_pretrained(embeddings, padding_idx=padding_idx)
         
         # LSTM layer
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, bidirectional=True)
 
         # MLP layers
         self.mlp = nn.Sequential(
-            nn.Linear(hidden_dim + 1, 64),  # Add 1 for positional index
+            nn.Linear(hidden_dim * 2, 64),
             nn.ReLU(),
             nn.Linear(64, output_dim)
         )
 
-    def forward(self, text, pos_index):
-        # Token embeddings
-        embedded = self.embedding(text)
+    def forward(self, text: torch.tensor, pos_index: int):
+        """
+        Computes the forward pass of the neural net
 
-        # LSTM forward pass
+        Args:
+            text (torch.tensor): Tensor of the tokenized indices of the words in the input sentence, with unknown words having their index set to UNKNOWN_TOKEN_IDX
+            pos_index (int): The position index of the target token for lemmatization classification in the sentence.
+
+        Returns:
+            torch.tensor: Output logits of the neural network
+        """
+        # Token embeddings
+        glove = get_glove(self.embedding_dim)
+        # UNKNOWN_TOKEN will be our <UNK> token
+        # UNKNOWN_TOKEN_IDX will be the custom index for the <UNK> token
+        unk_token_indices = utils.extract_unknown_token_indices(text, UNKNOWN_TOKEN_IDX)
+        unknown_mask = (text == UNKNOWN_TOKEN_IDX)
+        masked_indices = text.masked_fill(unknown_mask, 0)  # Replace UNKNOWN_TOKEN_IDX with 0 for embedding lookup
+
+        # replace 0 token vectors with the true unknown 
+        embedded = self.embedding(masked_indices)
+        for unk_token_idx in unk_token_indices:
+            embedded[unk_token_idx] = glove[UNKNOWN_TOKEN]
+
         lstm_out, (hidden, _) = self.lstm(embedded)
 
-        # Extract the last hidden state
-        print(lstm_out)
-        lstm_out = lstm_out[-1]
-
-        # Concatenate the last hidden state with positional index
-        print(f"LSTM OUT: {lstm_out}, shape: {lstm_out.shape}")
-        print(f"POS INDEX: {pos_index}, shape: {pos_index.shape}")
-        # concatenated = torch.cat((lstm_out, pos_index), dim=1)
-        concatenated = torch.cat((lstm_out, pos_index), dim=0)
-        print(concatenated, concatenated.shape)
-
+        # Extract the hidden state at the index of the token
+        lstm_out = lstm_out[pos_index]
 
         # MLP forward pass
-        output = self.mlp(concatenated)
-
-        print(output)
-
+        output = self.mlp(lstm_out)
         return output
-
-# Define the hyperparameters
-vocab_size = 10000  # Adjust based on your dataset
-embedding_dim = 100
-hidden_dim = 256
-output_dim = 2  # Binary classification (be or have)
-
-# Load GloVe embeddings (adjust the path and dimensions)
-glove = GloVe(name='6B', dim=embedding_dim)
-vocab_size = len(glove.itos)  # Size of the GloVe vocabulary
-
-# Initialize the model with GloVe embeddings
-model = LemmaClassifier(vocab_size, embedding_dim, hidden_dim, output_dim, glove.vectors)
-
-# Define the loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Sample input data (make sure to adapt this to your dataset)
-tokenizer = get_tokenizer('basic_english')
-sentence = "The cat's tail is long"
-# tokenized_sentence = tokenizer(sentence)  # Tokenize the input sentence  , maybe use stanza next time
-tokenized_sentence = ['the', 'cat', "'s", 'tail', 'is', 'long']
-
-# Convert the tokenized input to a tensor
-# tokenized_text = [word for word in tokenized_sentence]
-positional_index = tokenized_sentence.index("'s")
-tokenized_text = torch.tensor([glove.stoi[word] for word in tokenized_sentence])   # maybe just word not glove.stoi[word]
-
-print(tokenized_text)
-
-positional_index = torch.tensor([positional_index])  # Convert positional index to tensor
-
-# Forward pass
-output = model(tokenized_text, positional_index)
-print(output)
-# output_probs = torch.softmax(output, dim=0)
-
-# # Choose the class with the highest probability as the prediction
-# predicted_class = torch.argmax(output_probs, dim=0)
-
-# print(predicted_class, predicted_class.shape)
-
-# Compute the loss
-# target = torch.tensor([0], dtype=torch.long)  # 0 for "be" and 1 for "have"
-# Define the target label (0 for "be" and 1 for "have")
-target = torch.tensor([0, 1], dtype=torch.float)  # Use dtype=torch.long for class labels, but torch.float seems to make this work
-print(target)
-loss = criterion(output, target)
-print(loss)
-
-# Backpropagation and optimization
-optimizer.zero_grad()
-loss.backward()
-optimizer.step()
-
-# Training loop and evaluation code can be added here
