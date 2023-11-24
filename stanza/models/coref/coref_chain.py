33f1539c488eabc2734751ff3405ea2f7da9eac8
John Bauer <horatio@gmail.com>
2023-11-23 21:08:21 -0800

    Compose the words & tokens of a mention into its representative text

--- stanza/models/coref/coref_chain.py
+++ stanza/models/coref/coref_chain.py
@@ -4,6 +4,5 @@ Coref chain suitable for attaching to a Document after coref processing
 
 from collections import namedtuple
 
-# TODO: it would probably be nicer to include the text, at least of the representative mention
 CorefMention = namedtuple('CorefMention', ['sentence', 'start_word', 'end_word'])
-CorefChain = namedtuple('CorefChain', ['mentions', 'representative'])
+CorefChain = namedtuple('CorefChain', ['mentions', 'representative', 'representative_text'])
