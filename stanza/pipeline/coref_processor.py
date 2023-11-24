6d4030b0ef10e7c2d7e07b4349d30b2e5bac19c0
John Bauer <horatio@gmail.com>
2023-11-23 21:08:21 -0800

    Compose the words & tokens of a mention into its representative text

--- stanza/pipeline/coref_processor.py
+++ stanza/pipeline/coref_processor.py
@@ -2,10 +2,52 @@
 Processor that attaches coref annotations to a document
 """
 
+from stanza.models.common.utils import misc_to_space_after
 from stanza.models.coref.coref_chain import CorefMention, CorefChain
 
 from stanza.pipeline._constants import *
 from stanza.pipeline.processor import UDProcessor, register_processor
+
+def extract_text(document, sent_id, start_word, end_word):
+    sentence = document.sentences[sent_id]
+    tokens = []
+
+    # the coref model indexes the words from 0,
+    # whereas the ids we are looking at on the tokens start from 1
+    # here we will switch to ID space
+    start_word = start_word + 1
+    end_word = end_word + 1
+
+    # For each position between start and end word:
+    # If a word is part of an MWT, and the entire token
+    # is inside the range, we use that Token's text for that span
+    # This will let us easily handle words which are split into pieces
+    # Otherwise, we only take the text of the word itself
+    next_idx = start_word
+    while next_idx < end_word:
+        word = sentence.words[next_idx-1]
+        parent_token = word.parent
+        if isinstance(parent_token.id, int) or len(parent_token.id) == 1:
+            tokens.append(parent_token)
+            next_idx += 1
+        elif parent_token.id[0] >= start_word and parent_token.id[1] < end_word:
+            tokens.append(parent_token)
+            next_idx = parent_token.id[1] + 1
+        else:
+            tokens.append(word)
+            next_idx += 1
+
+    # We use the SpaceAfter or SpacesAfter attribute on each Word or Token
+    # we chose in the above loop to separate the text pieces
+    text = []
+    for token in tokens:
+        text.append(token.text)
+        text.append(misc_to_space_after(token.misc))
+    # the last token space_after will be discarded
+    # so that we don't have stray WS at the end of the mention text
+    text = text[:-1]
+    return "".join(text)
+
 
 @register_processor(COREF)
 class CorefProcessor(UDProcessor):
@@ -80,8 +122,9 @@ class CorefProcessor(UDProcessor):
                 end_word = word_pos[span[1]]
                 mentions.append(CorefMention(sent_id, start_word, end_word))
             representative = mentions[best_span]
+            representative_text = extract_text(document, representative.sentence, representative.start_word, representative.end_word)
 
-            chain = CorefChain(mentions, representative)
+            chain = CorefChain(mentions, representative, representative_text)
             clusters.append(chain)
 
         document.coref = clusters
