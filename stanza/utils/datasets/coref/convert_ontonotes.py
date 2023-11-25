f8237c975b49c9a116eea00c3081cd4b08b2da88
John Bauer <horatio@gmail.com>
2023-11-24 18:05:21 -0800

    Add Karel's method to reattach heads using the conjunction - here we use CCONJ instead of CC (upos)

--- stanza/utils/datasets/coref/convert_ontonotes.py
+++ stanza/utils/datasets/coref/convert_ontonotes.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import lru_cache
 import json
 import os
 
@@ -9,6 +10,63 @@ from stanza.utils.default_paths import get_default_paths
 from stanza.utils.get_tqdm import get_tqdm
 
 tqdm = get_tqdm()
+
+# TODO: move this to a utility module and try it on other languages
+class DynamicDepth():
+    """
+    Implements a cache + dynamic programming to find the relative depth of every word in a subphrase given the head word for every word.
+    """
+    def get_parse_depths(self, heads, start, end):
+        """Return the relative depth for every word
+
+        Args:
+            heads (list): List where each entry is the index of that entry's head word in the dependency parse
+            start (int): starting index of the heads for the subphrase
+            end (int): ending index of the heads for the subphrase
+
+        Returns:
+            list: Relative depth in the dependency parse for every word
+        """
+        self.heads = heads[start:end]
+        self.relative_heads = [h - start if h else -100 for h in self.heads] # -100 to deal with 'none' headwords
+
+        depths = [self._get_depth_recursive(h) for h in range(len(self.relative_heads))]
+
+        return depths
+
+    @lru_cache(maxsize=None)
+    def _get_depth_recursive(self, index):
+        """Recursively get the depths of every index using a cache and recursion
+
+        Args:
+            index (int): Index of the word for which to calculate the relative depth
+
+        Returns:
+            int: Relative depth of the word at the index
+        """
+        # if the head for the current index is outside the scope, this index is a relative root
+        if self.relative_heads[index] >= len(self.relative_heads) or self.relative_heads[index] < 0:
+            return 0
+        return self._get_depth_recursive(self.relative_heads[index]) + 1
+
+def find_cconj_head(heads, upos, start, end):
+    """
+    Finds how far each word is from the head of a span, then uses the closest CCONJ to the head as the new head
+
+    If no CCONJ is present, returns None
+    """
+    # use head information to extract parse depth
+    dynamicDepth = DynamicDepth()
+    depth = dynamicDepth.get_parse_depths(heads, start, end)
+    depth_limit = 2
+
+    # return first 'CCONJ' token above depth limit, if exists
+    # unlike the original paper, we expect the parses to use UPOS, hence CCONJ instead of CC
+    cc_indexes = [i for i in range(end - start) if upos[i+start] == 'CCONJ' and depth[i] < depth_limit]
+    if cc_indexes:
+        return cc_indexes[0] + start
+    return None
+
 
 def read_paragraphs(section):
     for doc in section:
@@ -28,6 +86,7 @@ def read_paragraphs(section):
 def convert_dataset_section(pipe, section):
     processed_section = []
     section = list(x for x in read_paragraphs(section))
+
     for idx, (doc_id, part_id, paragraph) in enumerate(tqdm(section)):
         sentences = [x['words'] for x in paragraph]
         sentence_lens = [len(x) for x in sentences]
@@ -63,23 +122,28 @@ def convert_dataset_section(pipe, section):
         word_total = 0
         for parsed_sentence, ontonotes_sentence in zip(doc.sentences, paragraph):
             coref_spans = ontonotes_sentence['coref_spans']
+            sentence_upos = [x.upos for x in parsed_sentence.words]
+            sentence_heads = [x.head - 1 if x.head > 0 else None for x in parsed_sentence.words]
             for span in coref_spans:
                 # input is expected to be start word, end word + 1
                 # counting from 0
+                # whereas the OntoNotes coref_span is [start_word, end_word] inclusive
                 span_start = span[1] + word_total
                 span_end = span[2] + word_total + 1
-                for candidate_head in range(span[1], span[2] + 1):
-                    # stanza uses 0 to mark the head, whereas OntoNotes is counting
-                    # words from 0, so we have to subtract 1 from the stanza heads
-                    #print(span, candidate_head, parsed_sentence.words[candidate_head].head - 1)
-                    # treat the head of the phrase as the first word that has a head outside the phrase
-                    if (parsed_sentence.words[candidate_head].head - 1 < span[1] or
-                        parsed_sentence.words[candidate_head].head - 1 > span[2]):
-                        break
-                else:
-                    # if none have a head outside the phrase (circular??)
-                    # then just take the first word
-                    candidate_head = span[1]
+                candidate_head = find_cconj_head(sentence_heads, sentence_upos, span[1], span[2]+1)
+                if candidate_head is None:
+                    for candidate_head in range(span[1], span[2] + 1):
+                        # stanza uses 0 to mark the head, whereas OntoNotes is counting
+                        # words from 0, so we have to subtract 1 from the stanza heads
+                        #print(span, candidate_head, parsed_sentence.words[candidate_head].head - 1)
+                        # treat the head of the phrase as the first word that has a head outside the phrase
+                        if (parsed_sentence.words[candidate_head].head - 1 < span[1] or
+                            parsed_sentence.words[candidate_head].head - 1 > span[2]):
+                            break
+                    else:
+                        # if none have a head outside the phrase (circular??)
+                        # then just take the first word
+                        candidate_head = span[1]
                 #print("----> %d" % candidate_head)
                 candidate_head += word_total
                 span_clusters[span[0]].append((span_start, span_end))
