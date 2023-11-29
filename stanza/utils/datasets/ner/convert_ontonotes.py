d96dfda27f677535c8b5201140051dd905e420c0
John Bauer <horatio@gmail.com>
2023-11-29 09:42:10 -0800

    Ignore nt_ files for English OntoNotes NER, since those files are missing the name annotations in the original LDC release but have blank annotations instead in the HF version

--- stanza/utils/datasets/ner/convert_ontonotes.py
+++ stanza/utils/datasets/ner/convert_ontonotes.py
@@ -12,9 +12,14 @@ from stanza.utils.datasets.ner.utils import write_dataset
 
 ID_TO_TAG = ["O", "B-PERSON", "I-PERSON", "B-NORP", "I-NORP", "B-FAC", "I-FAC", "B-ORG", "I-ORG", "B-GPE", "I-GPE", "B-LOC", "I-LOC", "B-PRODUCT", "I-PRODUCT", "B-DATE", "I-DATE", "B-TIME", "I-TIME", "B-PERCENT", "I-PERCENT", "B-MONEY", "I-MONEY", "B-QUANTITY", "I-QUANTITY", "B-ORDINAL", "I-ORDINAL", "B-CARDINAL", "I-CARDINAL", "B-EVENT", "I-EVENT", "B-WORK_OF_ART", "I-WORK_OF_ART", "B-LAW", "I-LAW", "B-LANGUAGE", "I-LANGUAGE",]
 
-def convert_dataset_section(section):
+def convert_dataset_section(config_name, section):
     sentences = []
     for doc in section:
+        # the nt_ sentences (New Testament) in the HF version of OntoNotes
+        # have blank named_entities, even though there was no original .name file
+        # that corresponded with these annotations
+        if config_name.startswith("english") and doc['document_id'].startswith("pt/nt"):
+            continue
         for sentence in doc['sentences']:
             words = sentence['words']
             tags = [ID_TO_TAG[x] for x in sentence['named_entities']]
@@ -39,7 +44,7 @@ def process_dataset(short_name, conll_path, ner_output_path):
     else:
         raise ValueError("Unknown short name for downloading ontonotes: %s" % short_name)
     dataset = load_dataset("conll2012_ontonotesv5", config_name, cache_dir=conll_path)
-    datasets = [convert_dataset_section(x) for x in [dataset['train'], dataset['validation'], dataset['test']]]
+    datasets = [convert_dataset_section(config_name, x) for x in [dataset['train'], dataset['validation'], dataset['test']]]
     write_dataset(datasets, ner_output_path, short_name)
 
 def main():
