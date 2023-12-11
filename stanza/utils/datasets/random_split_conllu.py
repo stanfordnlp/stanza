358747c0128086dacdeb65a67ffb8a996b4e851e
John Bauer <horatio@gmail.com>
2023-12-11 15:36:47 -0800

    Keep the comments with the sentences when splitting a conllu document

--- stanza/utils/datasets/random_split_conllu.py
+++ stanza/utils/datasets/random_split_conllu.py
@@ -34,10 +34,10 @@ def main():
     doc = CoNLL.conll2doc(args.filename)
     random.seed(args.seed)
 
-    train_sentences = []
-    dev_sentences = []
-    test_sentences = []
-    splits = [train_sentences, dev_sentences, test_sentences]
+    train_doc = ([], [])
+    dev_doc = ([], [])
+    test_doc = ([], [])
+    splits = [train_doc, dev_doc, test_doc]
     for sentence in doc.sentences:
         sentence_dict = sentence.to_dict()
         if args.remove_xpos:
@@ -47,9 +47,10 @@ def main():
             for x in sentence_dict:
                 x.pop('feats', None)
         split = random.choices(splits, weights)[0]
-        split.append(sentence_dict)
+        split[0].append(sentence_dict)
+        split[1].append(sentence.comments)
 
-    splits = [Document(split) for split in splits]
+    splits = [Document(split[0], comments=split[1]) for split in splits]
     for split_doc, split_name in zip(splits, ("train", "dev", "test")):
         filename = os.path.join(output_directory, "%s.%s.in.conllu" % (args.short_name, split_name))
         print("Outputting %d sentences to %s" % (len(split_doc.sentences), filename))
