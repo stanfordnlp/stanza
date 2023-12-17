bdca7d046be1255094801a2622bc4bb0fb07f8d8
John Bauer <horatio@gmail.com>
2023-12-17 01:09:05 -0800

    Oops

--- stanza/models/lemma_classifier/train_model.py
+++ stanza/models/lemma_classifier/train_model.py
@@ -120,7 +120,7 @@ class LemmaClassifierTrainer():
 
         for epoch in range(num_epochs):
             # go over entire dataset with each epoch
-            for texts, position, label in tqdm(zip(texts_batch, positions_batch, labels_batch)):
+            for texts, position, label in tqdm(zip(texts_batch, positions_batch, labels_batch), total=len(texts_batch)):
                 if position < 0 or position > len(texts) - 1:  # validate position index
                     raise ValueError(f"Found position {position} in text: {texts}, which is not possible.")
                 
