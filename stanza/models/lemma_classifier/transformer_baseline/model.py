f6df544cb15bff5583d27f4702e0a5ee3098807b
SecroLoL <azshan@stanford.edu>
2023-12-20 15:38:56 -0800

    edit

--- stanza/models/lemma_classifier/transformer_baseline/model.py
+++ stanza/models/lemma_classifier/transformer_baseline/model.py
@@ -59,7 +59,7 @@ class LemmaClassifierWithTransformer(nn.Module):
         input_ids_tensor = torch.tensor([input_ids])
         input_ids_tensor.to(self.device)  # Move data to device
 
-        logging.info(f"TRANSFORMER is on {next(self.transformer.parameters()).device()}")
+        logging.info(f"TRANSFORMER is on {next(self.transformer.parameters()).device}")
         logging.info(f"INPUT ID TENSORS is on {input_ids_tensor.device()}")
 
         # Forward pass through Transformer
