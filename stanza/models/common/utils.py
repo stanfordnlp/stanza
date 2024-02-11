1f3a85f7d27342e99e792b867263667293e62118
John Bauer <horatio@gmail.com>
2024-02-11 08:34:05 -0800

    Oops, fix usage of weight_decay in the common function to build the bert optimizer

--- stanza/models/common/utils.py
+++ stanza/models/common/utils.py
@@ -251,7 +251,7 @@ def get_split_optimizer(name, model, lr, betas=(0.9, 0.999), eps=1e-8, momentum=
     }
     if bert_parameters is not None and bert_learning_rate > 0.0:
         if bert_weight_decay is not None:
-            bert_parameters['weight_decay'] = bert_weight_decay
+            extra_args['weight_decay'] = bert_weight_decay
         optimizers["bert_optimizer"] = dispatch_optimizer(name, bert_parameters, lr=lr, betas=betas, eps=eps, momentum=momentum, **extra_args)
     return optimizers
 
