3e450d41f67f1a90b47377344a5d349c306e591d
BLKSerene <blkserene@gmail.com>
2024-04-24 13:55:35 +0800

    Drop dependency on tomli and prefer tomllib in the standard library for Python >= 3.11

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         'protobuf>=3.15.0',
         'requests', 
         'networkx',
-        'tomli',
+        'tomli;python_version<"3.11"',
         'torch>=1.3.0',
         'tqdm',
     ],
--- stanza/models/coref/model.py
+++ stanza/models/coref/model.py
@@ -9,7 +9,10 @@ import re
 from typing import Any, Dict, List, Optional, Set, Tuple
 
 import numpy as np      # type: ignore
-import tomli
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
 import torch
 import transformers     # type: ignore
 
@@ -893,7 +896,7 @@ class CorefModel:  # pylint: disable=too-many-instance-attributes
     def _load_config(config_path: str,
                      section: str) -> Config:
         with open(config_path, "rb") as fin:
-            config = tomli.load(fin)
+            config = tomllib.load(fin)
         default_section = config["DEFAULT"]
         current_section = config[section]
         unknown_keys = (set(current_section.keys())
