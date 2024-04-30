e8a040737241375e31f8dd3003608de5e9ecf911
SecroLoL <azshan@stanford.edu>
2024-04-30 11:07:23 -0700

    Add constants for model building and args loading

---
+++ stanza/models/summarization/constants.py
@@ -0,0 +1,10 @@
+"""
+Constant values for model building and execution
+"""
+
+# for model.py
+DEFAULT_ENCODER_HIDDEN_DIM = 128
+DEFAULT_ENCODER_NUM_LAYERS = 1
+UNK_ID = 1
+
+DEFAULT_BATCH_SIZE = 16
