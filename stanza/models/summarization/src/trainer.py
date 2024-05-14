55a9a4eb8ccb7deaf8a5d58d0f7d466cb973ba0e
SecroLoL <azshan@stanford.edu>
2024-05-13 17:06:16 -0700

    Add trainer init, method to build the model after parsing args, and begin method for training model

--- stanza/models/summarization/src/trainer.py
+++ stanza/models/summarization/src/trainer.py
@@ -33,6 +33,81 @@ if not logger.handlers:
 tqdm = get_tqdm()
 
 
+class SummarizationTrainer():
+
+    def __init__(self, model_args: dict, embedding_file: str, lr: float):
+        """
+        TODO: finish this documentation
+
+        Model arguments:
+        {
+        batch size, 
+        encoder hidden dim,
+        encoder num layers,
+        decoder hidden dim,
+        decoder num layers,
+        pgen,
+        coverage,
+        }
+
+        embedding_file:
+
+        lr:
+        """
+        self.model_args = model_args
+
+        pt = load_pretrain(embedding_file)
+        self.pt_embedding = pt
+        self.lr = lr 
+
+    def build_model(self) -> BaselineSeq2Seq:
+        """
+        Build the model for training using the model args
+
+        Raises any errors depending on model argument errors
+        """
+
+        # parse input for valid args
+        batch_size = self.model_args.get("batch_size", DEFAULT_BATCH_SIZE)
+        encoder_hidden_dim = self.model_args.get("encoder_hidden_dim", DEFAULT_ENCODER_HIDDEN_DIM)
+        encoder_num_layers = self.model_args.get("encoder_num_layers", DEFAULT_ENCODER_NUM_LAYERS)
+        decoder_hidden_dim = self.model_args.get("decoder_hidden_dim", DEFAULT_DECODER_HIDDEN_DIM)
+        decoder_num_layers = self.model_args.get("decoder_num_layers", DEFAULT_DECODER_NUM_LAYERS)
+        pgen = self.model_args.get("pgen", False)
+        coverage = self.model_args.get("coverage", False)
+
+        parsed_model_args = {
+            "batch_size": batch_size,
+            "encoder_hidden_dim": encoder_hidden_dim,
+            "encoder_num_layers": encoder_num_layers,
+            "decoder_hidden_dim": decoder_hidden_dim,
+            "decoder_num_layers": decoder_num_layers,
+            "pgen": pgen,
+            "coverage": coverage
+        }
+
+        # log the model args
+        model_args_message = f"""Using the following arguments for model initialization:\n\n Batch size: {batch_size}\nEncoder hidden dim {encoder_hidden_dim}\nEncoder num layers {encoder_num_layers}\nDecoder hidden dim {decoder_hidden_dim}\nDecoder num layers {decoder_num_layers}\nusing pgen: {pgen}\nusing coverage: {coverage}"""
+        logger.info(model_args_message)
+
+        # return the model obj
+        return BaselineSeq2Seq(parsed_model_args, self.pt_embedding)
+
+    def train(self, num_epochs: int, save_name: str, train_file: str, eval_file: str) -> None:
+        """
+        Trains a model on batches of texts
+
+        Args:
+            num_epochs (int): Number of training epochs 
+            save_name (str): Path to store trained model
+            eval_file (str): Path to the validation set file for evaluating model checkpoints
+            train_file (str): Path to training data file containing tokenized text for each article
+
+        Returns:
+            None (model with best validation set performance will be saved to the save file)
+        """
+        pass 
+
 def main():
     logger.info("This is a test")
 
