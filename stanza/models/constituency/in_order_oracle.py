1c3afbd618c50ba8346bbbe7b9aedb700465ed99
John Bauer <horatio@gmail.com>
2024-05-15 21:42:03 -0700

    Add notes on ZH ambiguous experiments

--- stanza/models/constituency/in_order_oracle.py
+++ stanza/models/constituency/in_order_oracle.py
@@ -614,6 +614,20 @@ class RepairType(Enum):
           unambiguous c-s/shift              0.8376   0.8392
           ambiguous c-s/shift closed early   0.8363   0.8359
           ambiguous c-s/shift closed late    0.8365   0.8383
+
+    --------------------------------------------------------
+
+    Running ZH experiments to verify some of the above findings
+    bert + peft, only 200 epochs
+
+    Comparing wrong_open fixes
+          w/o ambiguous open                 0.9145   0.9144
+          w/ ambiguous open_three_subtrees   0.9146   0.9142
+
+    Testing three possible repairs for close-shift/shift
+          unambiguous c-s/shift              0.9155   0.9146
+          ambiguous c-s/shift closed early   0.9145   0.9153
+          ambiguous c-s/shift closed late    0.9138   0.9140
     """
     def __new__(cls, fn, correct=False, debug=False):
         """
