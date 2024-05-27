0b63ec1fedee88eee2f3b6180d550dbc2f8ca5f8
SecroLoL <azshan@stanford.edu>
2024-05-26 17:18:26 -0700

    Update default train and eval path roots

--- stanza/models/summarization/constants.py
+++ stanza/models/summarization/constants.py
@@ -10,10 +10,11 @@ DEFAULT_ENCODER_NUM_LAYERS = 1
 DEFAULT_DECODER_HIDDEN_DIM = 128
 DEFAULT_DECODER_NUM_LAYERS = 1
 UNK_ID = 1
+UNK = "<UNK>"
 
 DEFAULT_BATCH_SIZE = 16
 
-DEFAULT_EVAL_FILE_PATH = os.path.join(os.path.dirname(__file__), "data", "eval.txt")
-DEFAULT_TRAIN_FILE_PATH = os.path.join(os.path.dirname(__file__), "data", "train.txt")
+DEFAULT_EVAL_ROOT = os.path.join(os.path.dirname(__file__), "data", "validation")
+DEFAULT_TRAIN_ROOT = os.path.join(os.path.dirname(__file__), "data", "train")
 DEFAULT_SAVE_NAME = os.path.join(os.path.dirname(__file__), "saved_models", "summarization_model.pt")
 DEFAULT_WORDVEC_PRETRAIN_FILE = os.path.join(os.path.dirname(__file__), "pretrain", "en", "glove.pt")
