3dfabb1318f7ed12c939d7ccbfed366ed50f7521
SecroLoL <azshan@stanford.edu>
2024-06-01 14:58:33 -0700

    Add util function to generate checkpoint for model paths

--- stanza/models/summarization/src/utils.py
+++ stanza/models/summarization/src/utils.py
@@ -39,10 +39,36 @@ def convert_text_to_token_ids(vocab_map: Mapping[str, int], text_batch: List[Lis
 
     if max_steps is not None:  # Truncate
          text_batch = [article[: max_steps] for article in text_batch]
-         
+
     for article in text_batch:
         article_token_ids = torch.tensor([vocab_map.get(word.lower(), UNK_ID) for word in article])
         token_ids.append(article_token_ids)
 
     padded_inputs = pad_sequence(token_ids, batch_first=True)
     return padded_inputs
+
+def generate_checkpoint_path(save_path: str):
+
+    """
+    Given a model path, create a checkpoint path by appending '_ckpt' to the filename.
+
+    Args:
+    model_path (str): The original path to the model file.
+
+    Returns:
+    str: The new path with '_ckpt' appended to the filename.
+    """
+    # Extract the directory and filename
+    dir_name = os.path.dirname(save_path)
+    base_name = os.path.basename(save_path)
+    
+    # Split the filename into name and extension
+    name, ext = os.path.splitext(base_name)
+    
+    # Create the new filename with '_ckpt' appended
+    new_base_name = f"{name}_ckpt{ext}"
+    
+    # Construct the new path
+    new_path = os.path.join(dir_name, new_base_name)
+    
+    return new_path
