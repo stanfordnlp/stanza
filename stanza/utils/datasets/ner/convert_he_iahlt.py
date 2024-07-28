68c613bc2921fc8b7ddd3a38122929577b6770d7
John Bauer <horatio@gmail.com>
2024-07-27 23:18:46 -0700

    Grab sentences from both IAHLT wiki and knesset for NER, although dropping any sentence with nested NER

--- stanza/utils/datasets/ner/convert_he_iahlt.py
+++ stanza/utils/datasets/ner/convert_he_iahlt.py
@@ -5,57 +5,65 @@ from stanza.utils.conll import CoNLL
 import stanza.utils.default_paths as default_paths
 from stanza.utils.datasets.ner.utils import write_dataset
 
+def extract_single_sentence(sentence):
+    current_entity = "O"
+    words = []
+    for word in sentence.words:
+        text = word.text
+        misc = word.misc
+        if misc is None:
+            if current_entity == 'O':
+                entity = 'O'
+            else:
+                entity = "I-" + current_entity
+            words.append((text, entity))
+            continue
+        pieces = misc.split("|")
+        for piece in pieces:
+            if piece.startswith("Entity="):
+                entity = piece.split("=", maxsplit=1)[1]
+                assert '(' not in entity[1:] and ')' not in entity[:-1]
+                if entity.startswith("(") and entity.endswith(")"):
+                    assert current_entity == 'O'
+                    entity = "B-" + entity[1:-1]
+                elif entity.startswith("("):
+                    assert current_entity == 'O'
+                    entity = entity[1:]
+                    current_entity = entity
+                    entity = "B-" + entity
+                elif entity.endswith(")"):
+                    entity = entity[:-1]
+                    assert current_entity == entity
+                    entity = "I-" + entity
+                    current_entity = "O"
+                else:
+                    assert current_entity == entity
+                    entity = "I-" + entity
+                words.append((text, entity))
+                break
+        else: # closes for loop
+            if current_entity == 'O':
+                entity = 'O'
+            else:
+                entity = "I-" + current_entity
+            words.append((text, entity))
+    return words
+
 def extract_sentences(doc):
     sentences = []
     for sentence in doc.sentences:
-        current_entity = "O"
-        words = []
-        for word in sentence.words:
-            text = word.text
-            misc = word.misc
-            if misc is None:
-                if current_entity == 'O':
-                    entity = 'O'
-                else:
-                    entity = "I-" + current_entity
-                words.append((text, entity))
-                continue
-            pieces = misc.split("|")
-            for piece in pieces:
-                if piece.startswith("Entity="):
-                    entity = piece.split("=", maxsplit=1)[1]
-                    if entity.startswith("(") and entity.endswith(")"):
-                        assert current_entity == 'O'
-                        entity = "B-" + entity[1:-1]
-                    elif entity.startswith("("):
-                        assert current_entity == 'O'
-                        entity = entity[1:]
-                        current_entity = entity
-                        entity = "B-" + entity
-                    elif entity.endswith(")"):
-                        entity = entity[:-1]
-                        assert current_entity == entity
-                        entity = "I-" + entity
-                        current_entity = "O"
-                    else:
-                        assert current_entity == entity
-                        entity = "I-" + entity
-                    words.append((text, entity))
-                    break
-            else: # closes for loop
-                if current_entity == 'O':
-                    entity = 'O'
-                else:
-                    entity = "I-" + current_entity
-                words.append((text, entity))
-        sentences.append(words)
+        try:
+            words = extract_single_sentence(sentence)
+            sentences.append(words)
+        except AssertionError:
+            print("Skipping sentence %s" % sentence.sent_id)
 
     return sentences
 
 def convert_iahlt(udbase, output_dir, short_name):
     shards = ("train", "dev", "test")
-    ud_datasets = ["UD_Hebrew-IAHLTknesset"]
-    base_filenames = ["he_iahltknesset-ud-%s.conllu"]
+    ud_datasets = ["UD_Hebrew-IAHLTwiki", "UD_Hebrew-IAHLTknesset"]
+    base_filenames = ["he_iahltwiki-ud-%s.conllu", "he_iahltknesset-ud-%s.conllu"]
     datasets = defaultdict(list)
 
     for ud_dataset, base_filename in zip(ud_datasets, base_filenames):
