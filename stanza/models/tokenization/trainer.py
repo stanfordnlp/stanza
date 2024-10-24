1714e2361e82a889f2efdc33bba270bb0f06291d
John Bauer <horatio@gmail.com>
2024-10-24 14:42:37 -0700

    Also oops

--- stanza/models/tokenization/trainer.py
+++ stanza/models/tokenization/trainer.py
@@ -79,7 +79,8 @@ class Trainer(BaseTrainer):
 
     def load(self, filename):
         try:
-            checkpoint = torch.load(filename, lambda storage, loc: storage, weights_only=True)
+            # the tokenizers with dictionaries won't properly load weights_only=True because they have a set
+            checkpoint = torch.load(filename, lambda storage, loc: storage)
         except BaseException:
             logger.error("Cannot load model from {}".format(filename))
             raise
