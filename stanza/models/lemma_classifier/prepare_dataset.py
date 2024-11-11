4766f7e47638c770772fda581975c41eeae36544
John Bauer <horatio@gmail.com>
2024-11-10 23:44:31 -0800

    Write data in json instead of one line per datum.  Will make it easier to store other information with the data, such as the tag being processed

--- stanza/models/lemma_classifier/prepare_dataset.py
+++ stanza/models/lemma_classifier/prepare_dataset.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import re
 
@@ -43,6 +44,22 @@ class DataProcessor():
                 occurrences.append(idx)
         return occurrences
 
+    @staticmethod
+    def write_output_file(save_name, target_upos, sentences):
+        with open(save_name, "w+", encoding="utf-8") as output_f:
+            output_f.write("{\n")
+            output_f.write('  "upos": %s,\n' % json.dumps(target_upos))
+            output_f.write('  "sentences": [')
+            wrote_sentence = False
+            for sentence in sentences:
+                if not wrote_sentence:
+                    output_f.write("\n    ")
+                    wrote_sentence = True
+                else:
+                    output_f.write(",\n    ")
+                output_f.write(json.dumps(sentence))
+            output_f.write("\n  ]\n}\n")
+
     def process_document(self, doc, save_name: str) -> None:
         """
         Takes any sentence from `doc` that meets the condition of `keep_sentence` and writes its tokens, index of target word, and lemma to `save_name`
@@ -53,57 +70,28 @@ class DataProcessor():
             doc (Stanza.doc): Document object that represents the file to be analyzed
             save_name (str): Path to the file for storing output
         """
-        with open(save_name, "w+", encoding="utf-8") as output_f:
-            for sentence in doc.sentences:
-                # for each sentence, we need to determine if it should be added to the output file.
-                # if the sentence fulfills keep_sentence, then we will save it along with the target word's index and its corresponding lemma
-                if self.keep_sentence(sentence):
-                    tokens = [token.text for token in sentence.words]
-                    indexes = self.find_all_occurrences(sentence)
-                    for idx in indexes:
-                        if self.allowed_lemmas.fullmatch(sentence.words[idx].lemma):
-                            # for each example found, we write the tokens, their respective upos tags, the target token index, lemma, and the number of tokens in the sentence
-                            upos_tags = [sentence.words[i].upos for i in range(len(sentence.words))]
-                            num_tokens = len(upos_tags)
-                            # TODO maybe this should just be done in JSON to avoid lengthy and non-extendable data processing.
-                            output_f.write(f'{" ".join(tokens)} {" ".join(upos_tags)} {idx} {sentence.words[idx].lemma} {num_tokens}\n')
+        sentences = []
+        for sentence in doc.sentences:
+            # for each sentence, we need to determine if it should be added to the output file.
+            # if the sentence fulfills keep_sentence, then we will save it along with the target word's index and its corresponding lemma
+            if self.keep_sentence(sentence):
+                tokens = [token.text for token in sentence.words]
+                indexes = self.find_all_occurrences(sentence)
+                for idx in indexes:
+                    if self.allowed_lemmas.fullmatch(sentence.words[idx].lemma):
+                        # for each example found, we write the tokens,
+                        # their respective upos tags, the target token index,
+                        # and the target lemma
+                        upos_tags = [sentence.words[i].upos for i in range(len(sentence.words))]
+                        num_tokens = len(upos_tags)
+                        sentences.append({
+                            "words": tokens,
+                            "upos_tags": upos_tags,
+                            "index": idx,
+                            "lemma": sentence.words[idx].lemma
+                        })
 
-    def read_processed_data(self, file_name: str) -> List[dict]:
-        """
-        Reads the output file from `process_document()` and outputs a list that contains the sentences of interest. Each object within the list
-        contains a map with three (key, val) pairs:
-
-        "words" is a list that contains the tokens of the sentence
-        "index" is an integer representing which token in "words" the lemma annotation corresponds to
-        "upos" is a string that corresponds to the target token's UPOS tag
-        "lemma" is a string that is the lemma of the target word in the sentence.
-
-        """
-        output = []
-        with open(file_name, "r", encoding="utf-8") as f:
-            for line in f.readlines():
-                if not line:
-                    continue
-
-                obj = {}
-                split = line.split()
-                num_tokens = int(split[-1])
-
-                # Extract data fields
-                words = split[: num_tokens]
-                upos_tags = split[num_tokens: 2 * num_tokens]
-                index = int(split[-3])
-                lemma = split[-2]
-
-                obj["words"] = words
-                obj["index"] = index
-                obj["upos_tags"] = upos_tags
-                obj["lemma"] = lemma
-
-                output.append(obj)
-
-        return output
-
+        self.write_output_file(save_name, self.target_upos, sentences)
 
 def main(args=None):
     parser = argparse.ArgumentParser()
