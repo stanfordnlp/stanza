65b18c539a722ff790ad11025e7ef3648dee77f0
John Bauer <horatio@gmail.com>
2025-03-17 16:50:38 -0700

    If any coref tokens are completely vaporized by the tokenizer, instead replace them with '_' and try again

--- stanza/models/coref/dataset.py
+++ stanza/models/coref/dataset.py
@@ -39,7 +39,13 @@ class CorefDataset(Dataset):
             subwords = []
             word_id = []
             for i, word in enumerate(doc["cased_words"]):
-                tokenized_word = self.__token_map.get(word, self.tokenizer.tokenize(word))
+                tokenized = self.tokenizer.tokenize(word)
+                if len(tokenized) == 0:
+                    word = "_"
+                    doc["cased_words"][i] = word
+                    tokenized = self.tokenizer.tokenize(word)
+                    assert len(tokenized) > 0
+                tokenized_word = self.__token_map.get(word, tokenized)
                 tokenized_word = list(filter(self.__filter_func, tokenized_word))
                 word2subword.append((len(subwords), len(subwords) + len(tokenized_word)))
                 subwords.extend(tokenized_word)
