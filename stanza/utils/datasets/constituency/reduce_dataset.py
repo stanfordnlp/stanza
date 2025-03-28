1482fd81cf4bc89e7c58b533b04de0c63e6e834f
John Bauer <horatio@gmail.com>
2025-03-27 23:51:02 -0700

    Add a script to randomly select N (typically 5000) of the training sentences from a dataset

---
+++ stanza/utils/datasets/constituency/reduce_dataset.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import random
+
+from stanza.models.constituency import tree_reader
+import stanza.utils.default_paths as default_paths
+from stanza.utils.datasets.constituency.utils import SHARDS, write_dataset
+
+def main():
+    parser = argparse.ArgumentParser(description="Script that cuts a treebank down to size")
+    parser.add_argument('--input', type=str, default=None, help='Input treebank')
+    parser.add_argument('--output', type=str, default=None, help='Output treebank')
+    parser.add_argument('--size', type=int, default=5000, help='How many trees')
+    args = parser.parse_args()
+
+    random.seed(1234)
+
+    paths = default_paths.get_default_paths()
+    output_directory = paths["CONSTITUENCY_DATA_DIR"]
+
+    # data/constituency/en_ptb3_train.mrg
+    input_filenames = [os.path.join(output_directory, "%s_%s.mrg" % (args.input, shard)) for shard in SHARDS]
+    output_filenames = ["%s_%s.mrg" % (args.output, shard) for shard in SHARDS]
+    shrink_datasets = [True, False, False]
+
+    datasets = []
+    for input_filename, shrink in zip(input_filenames, shrink_datasets):
+        treebank = tree_reader.read_treebank(input_filename)
+        if shrink:
+            random.shuffle(treebank)
+            treebank = treebank[:args.size]
+        datasets.append(treebank)
+    write_dataset(datasets, output_directory, args.output)
+
+if __name__ == '__main__':
+    main()
