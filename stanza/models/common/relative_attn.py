5cb52f32344765a41859da3ca3398272473108aa
John Bauer <horatio@gmail.com>
2025-04-07 02:43:20 -0700

    The sinks were being masked away for tokens after 1.  This should fix that error

--- stanza/models/common/relative_attn.py
+++ stanza/models/common/relative_attn.py
@@ -38,7 +38,7 @@ class RelativeAttention(nn.Module):
 
         self.register_buffer(
             "mask", 
-            torch.tril(torch.ones(window + num_sinks, window + num_sinks), diagonal=-1).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+            torch.tril(torch.ones(window, window), diagonal=-1).unsqueeze(0).unsqueeze(0).unsqueeze(0)
         )
         self.register_buffer(
             "flipped_mask",
@@ -63,44 +63,43 @@ class RelativeAttention(nn.Module):
             seq_len = self.window
 
         if self.num_sinks > 0:
-            orig_seq_len += self.num_sinks
-            seq_len += self.num_sinks
             # could keep a parameter to train sinks, but as it turns out,
             # the position vectors just overlap that parameter space anyway
             # generally the model trains the sinks to zero if we do that
             sink = torch.zeros((batch_size, self.num_sinks, d_model), dtype=x.dtype, device=x.device)
             x = torch.cat((sink, x), axis=1)
 
-        # k.shape = (batch_size, num_heads, d_head, seq_len)
-        k = self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
+        # k.shape = (batch_size, num_heads, d_head, seq_len + num_sinks)
+        k = self.key(x).reshape(batch_size, seq_len + self.num_sinks, self.num_heads, -1).permute(0, 2, 3, 1)[:, :, :, self.num_sinks:]
 
-        # v.shape = (batch_size, num_heads, d_head, seq_len)
-        v = self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
+        # v.shape = (batch_size, num_heads, d_head, seq_len + num_sinks)
+        v = self.value(x).reshape(batch_size, seq_len + self.num_sinks, self.num_heads, -1).permute(0, 2, 3, 1)
 
-        # q.shape = (batch_size, num_heads, d_head, seq_len)
-        q = self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
-        # q.shape = (batch_size, num_heads, d_head, window, seq_len)
+        # q.shape = (batch_size, num_heads, d_head, seq_len + num_sinks)
+        q = self.query(x).reshape(batch_size, seq_len + self.num_sinks, self.num_heads, -1).permute(0, 2, 3, 1)
+        # q.shape = (batch_size, num_heads, d_head, window + num_sinks, seq_len)
         q = self.skew_repeat(q)
         q = q + self.position
 
-        # qk.shape = (batch_size, num_heads, d_head, window, seq_len)
+        # qk.shape = (batch_size, num_heads, d_head, window + num_sinks, seq_len)
         qk = torch.einsum('bndws,bnds->bndws', q, k)
 
+        # TODO: fix mask
         # mask out the padding spaces at the end
         # can only attend to spots that aren't padded
         if orig_seq_len < seq_len:
             # mask out the part of the sentence which is empty
-            shorter_mask = self.flipped_mask[:, :, :, :, -orig_seq_len:]
-            qk[:, :, :, :, :orig_seq_len] = qk[:, :, :, :, :orig_seq_len].masked_fill(shorter_mask == 1, float("-inf"))
-            qk = qk[:, :, :, :orig_seq_len, :orig_seq_len]
+            shorter_mask = self.flipped_mask[:, :, :, :orig_seq_len, -orig_seq_len:]
+            qk = qk[:, :, :, :(orig_seq_len + self.num_sinks), :orig_seq_len]
+            qk[:, :, :, -orig_seq_len:, :] = qk[:, :, :, -orig_seq_len:, :].masked_fill(shorter_mask == 1, float("-inf"))
         else:
-            qk[:, :, :, :, -(self.window + self.num_sinks):] = qk[:, :, :, :, -(self.window + self.num_sinks):].masked_fill(self.flipped_mask == 1, float("-inf"))
+            qk[:, :, :, -self.window:, -self.window:] = qk[:, :, :, -self.window:, -self.window:].masked_fill(self.flipped_mask == 1, float("-inf"))
         qk = F.softmax(qk, dim=3)
 
         # v.shape = (batch_size, num_heads, d_head, window, seq_len)
         v = self.skew_repeat(v)
         if orig_seq_len < seq_len:
-            v = v[:, :, :, :orig_seq_len, :orig_seq_len]
+            v = v[:, :, :, :(orig_seq_len + self.num_sinks), :orig_seq_len]
         # result.shape = (batch_size, num_heads, d_head, orig_seq_len)
         result = torch.einsum('bndws,bndws->bnds', qk, v)
         # batch_size, orig_seq_len, d_output
@@ -109,32 +108,43 @@ class RelativeAttention(nn.Module):
         if self.reverse:
             result = torch.flip(result, (1,))
 
-        return self.dropout(result[:, self.num_sinks:, :])
+        return self.dropout(result)
 
     def skew_repeat(self, q):
-        total_window = self.window + self.num_sinks
+        """
+        q (currently, at least) is num_sinks + seq_len long
+        and the num_sinks are there to be chopped off the front
+        then the seq_len remainder is skewed
+        """
+        if self.num_sinks > 0:
+            q_sink = q[:, :, :, :self.num_sinks]
+            q_sink = q_sink.unsqueeze(4)
+            q_sink = q_sink.repeat(1, 1, 1, 1, q.shape[-1] - self.num_sinks)
+            q = q[:, :, :, self.num_sinks:]
         # make stripes that look like this
         # (seq_len 5, window 3)
         #   1 2 3 4 5
         #   1 2 3 4 5
         #   1 2 3 4 5
-        q = q.unsqueeze(4).repeat(1, 1, 1, 1, total_window).transpose(3, 4)
+        q = q.unsqueeze(4).repeat(1, 1, 1, 1, self.window).transpose(3, 4)
         # now the stripes look like
         #   1 2 3 4 5
         #   0 2 3 4 5
         #   0 0 3 4 5
-        q[:, :, :, :, :total_window] = q[:, :, :, :, :total_window].masked_fill(self.mask == 1, 0)
+        q[:, :, :, :, :self.window] = q[:, :, :, :, :self.window].masked_fill(self.mask == 1, 0)
         q_shape = list(q.shape)
         q_new_shape = list(q.shape)[:-2] + [-1]
         q = q.reshape(q_new_shape)
         zeros = torch.zeros_like(q[:, :, :, :1])
-        zeros = zeros.repeat(1, 1, 1, total_window)
+        zeros = zeros.repeat(1, 1, 1, self.window)
         q = torch.cat((q, zeros), axis=-1)
-        q_new_shape = q_new_shape[:-1] + [total_window, -1]
+        q_new_shape = q_new_shape[:-1] + [self.window, -1]
         # now the stripes look like
         #   1 2 3 4 5
         #   2 3 4 5 0
         #   3 4 5 0 0
         # q.shape = (batch_size, num_heads, d_head, window, seq_len)
         q = q.reshape(q_new_shape)[:, :, :, :, :-1]
+        if self.num_sinks > 0:
+            q = torch.cat([q_sink, q], dim=3)
         return q
