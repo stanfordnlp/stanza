ff9c9143087cf0bc4c681a92cad5de8ff58ec7d4
John Bauer <horatio@gmail.com>
2025-09-22 10:06:42 -0700

    Make it so run_pos.py functions on Windows as well.  https://github.com/stanfordnlp/stanza-train/issues/20
    Don't need to create a tempfile for POS

--- stanza/utils/training/run_pos.py
+++ stanza/utils/training/run_pos.py
@@ -1,5 +1,5 @@
 
-
+import io
 import logging
 import os
 
@@ -46,9 +46,9 @@ def run_treebank(mode, paths, treebank, short_name,
     if short_name == 'vi_vlsp22':
         train_file += f";{pos_dir}/vi_vtb.train.in.conllu"
     dev_in_file    = f"{pos_dir}/{short_name}.dev.in.conllu"
-    dev_pred_file  = temp_output_file if temp_output_file else f"{pos_dir}/{short_name}.dev.pred.conllu"
+    dev_pred_file  = f"{pos_dir}/{short_name}.dev.pred.conllu"
     test_in_file   = f"{pos_dir}/{short_name}.test.in.conllu"
-    test_pred_file = temp_output_file if temp_output_file else f"{pos_dir}/{short_name}.test.pred.conllu"
+    test_pred_file = f"{pos_dir}/{short_name}.test.pred.conllu"
 
     charlm_args = build_pos_charlm_args(short_language, dataset, command_args.charlm)
     bert_args = common.choose_transformer(short_language, command_args, extra_args)
@@ -76,7 +76,6 @@ def run_treebank(mode, paths, treebank, short_name,
 
         train_args = ["--wordvec_dir", paths["WORDVEC_DIR"],
                       "--train_file", train_file,
-                      "--output_file", dev_pred_file,
                       "--lang", short_language,
                       "--shorthand", short_name,
                       "--mode", "train"]
@@ -89,38 +88,46 @@ def run_treebank(mode, paths, treebank, short_name,
 
     if mode == Mode.SCORE_DEV or mode == Mode.TRAIN:
         dev_args = ["--wordvec_dir", paths["WORDVEC_DIR"],
-                    "--output_file", dev_pred_file,
                     "--lang", short_language,
                     "--shorthand", short_name,
                     "--mode", "predict"]
         if eval_file is None:
             dev_args += ['--eval_file', dev_in_file]
+        if command_args.save_output:
+            dev_args.extend(["--output_file", dev_pred_file])
         dev_args = dev_args + build_pos_wordvec_args(short_language, dataset, extra_args) + charlm_args + bert_args
         dev_args = dev_args + extra_args
         logger.info("Running dev POS for {} with args {}".format(treebank, dev_args))
-        tagger.main(dev_args)
+        _, dev_doc = tagger.main(dev_args)
+        if not command_args.save_output:
+            dev_pred_file = "{:C}\n\n".format(dev_doc)
+            dev_pred_file = io.StringIO(dev_pred_file)
 
         results = common.run_eval_script_pos(eval_file if eval_file else dev_in_file, dev_pred_file)
         logger.info("Finished running dev set on\n{}\n{}".format(treebank, results))
-        if not temp_output_file:
+        if command_args.save_output:
             logger.info("Output saved to %s", dev_pred_file)
 
     if mode == Mode.SCORE_TEST or mode == Mode.TRAIN:
         test_args = ["--wordvec_dir", paths["WORDVEC_DIR"],
-                     "--output_file", test_pred_file,
                      "--lang", short_language,
                      "--shorthand", short_name,
                      "--mode", "predict"]
         if eval_file is None:
             test_args += ['--eval_file', test_in_file]
+        if command_args.save_output:
+            dev_args.extend(["--output_file", test_pred_file])
         test_args = test_args + build_pos_wordvec_args(short_language, dataset, extra_args) + charlm_args + bert_args
         test_args = test_args + extra_args
         logger.info("Running test POS for {} with args {}".format(treebank, test_args))
-        tagger.main(test_args)
+        _, test_doc = tagger.main(test_args)
+        if not command_args.save_output:
+            test_pred_file = "{:C}\n\n".format(test_doc)
+            test_pred_file = io.StringIO(test_pred_file)
 
         results = common.run_eval_script_pos(eval_file if eval_file else test_in_file, test_pred_file)
         logger.info("Finished running test set on\n{}\n{}".format(treebank, results))
-        if not temp_output_file:
+        if command_args.save_output:
             logger.info("Output saved to %s", test_pred_file)
 
 
