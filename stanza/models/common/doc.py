8090e8db8276cbb671895d9160c263cf4881c0d7
John Bauer <horatio@gmail.com>
2025-10-15 01:03:39 -0700

    Add a method for getting enhanced_dependencies from a Sentence.  Creates an empty one if the graph is currently None, so don't use it unless you know you want such a thing

--- stanza/models/common/doc.py
+++ stanza/models/common/doc.py
@@ -619,6 +619,19 @@ class Sentence(StanzaObject):
         return self._enhanced_dependencies is not None and len(self._enhanced_dependencies) > 0
 
     @property
+    def enhanced_dependencies(self):
+        """
+        Returns the enhanced_dependencies graph.
+
+        Creates an empty one if one currently does not exist.
+        """
+        graph = self._enhanced_dependencies
+        if graph is None:
+            graph = nx.MultiDiGraph()
+            self._enhanced_dependencies = graph
+        return graph
+
+    @property
     def index(self):
         """
         Access the index of this sentence within the doc.
