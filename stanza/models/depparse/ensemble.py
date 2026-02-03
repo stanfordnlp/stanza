a17c50660189c126f870cd7ce9f6008feb8808d7
John Bauer <horatio@gmail.com>
2026-02-03 00:23:24 -0800

    When building an ensemble, need to track the reversed parameter, or the ensemble won't work properly when reversed

--- stanza/models/depparse/ensemble.py
+++ stanza/models/depparse/ensemble.py
@@ -3,6 +3,7 @@ Build an ensemble of GraphParsers
 """
 
 import argparse
+import copy
 from stanza.models.common import pretrain
 from stanza.models.common.foundation_cache import FoundationCache
 from stanza.models.depparse.model import GraphParser, EnsembleGraphParser
@@ -10,18 +11,28 @@ from stanza.models.depparse.transition.model import TransitionParser, EnsembleTr
 from stanza.models.depparse.trainer import Trainer
 
 def build_ensemble(args, pt, model_names, foundation_cache, device=None):
+    args = copy.deepcopy(args)
     models = []
     trainers = []
     for load_name in model_names:
         tr = Trainer.load(load_name, pt, args=args, device=device, foundation_cache=foundation_cache)
         models.append(tr.model)
         trainers.append(tr)
+
     if all(isinstance(x, GraphParser) for x in models):
         ensemble = EnsembleGraphParser(args, models[0].vocab, models)
     elif all(isinstance(x, TransitionParser) for x in models):
         ensemble = EnsembleTransitionParser(args, models[0].vocab, models)
     else:
         raise ValueError("Not all models are an ensemble type!  %s" % ([type(x) for x in models]))
+
+    if all(x.args.get('reversed', False) for x in models):
+        args['reversed'] = True
+    elif all(not x.args.get('reversed', False) for x in models):
+        args['reversed'] = False
+    else:
+        raise ValueError("Models do not agree on data diredction!  %s" % ([x.args.get('reversed', False) for x in models]))
+
     tr = Trainer(args=args, vocab=ensemble.vocab, model=ensemble, build_optimizer=False)
     tr.model_name = ",".join(model_names)
     return tr, trainers
