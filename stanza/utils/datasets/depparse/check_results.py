fb774592bff83da9dde9203cb5c22579754d64d0
John Bauer <horatio@gmail.com>
2026-02-15 23:28:07 -0800

    Add a flag to check_results which tells it to stop when it reaches a certain step.  Also, report dev scores for the last step found, if the run is unfinished

--- stanza/utils/datasets/depparse/check_results.py
+++ stanza/utils/datasets/depparse/check_results.py
@@ -4,31 +4,49 @@ A small script to report the dev/test scores from a depparse run, along with ave
 Uses the expected log format from the depparse.  Will not work otherwise.
 """
 
+import argparse
+import re
 import sys
 
+dev_re = re.compile(".*INFO: step ([0-9]+).*dev_score = ([.0-9]+).*")
+
+
 def main():
-    filenames = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="Grep through a list of files looking for the final results or best results up to a point")
+    parser.add_argument("filenames", nargs="+", help="Files to check")
+    parser.add_argument("--step", default=None, type=int, help="If set, stop checking at this step")
+    args = parser.parse_args()
+
+    filenames = args.filenames
     if len(filenames) == 0:
         return
 
     dev_scores = []
     test_scores = []
 
+    max_step = None
     for filename in filenames:
         with open(filename, encoding="utf-8") as fin:
             lines = fin.readlines()
             dev_score = None
             test_score = None
             for line in lines:
-                if line.find("Parser score") < 0:
-                    continue
-                score = float(line.strip().split()[-1])
-                if "dev" in line:
-                    dev_score = score
-                elif "test" in line:
-                    test_score = score
-                else:
-                    raise AssertionError("Did the parser score layout change?  Got an unexpected score line in %s" % filename)
+                if line.find("Parser score") >= 0:
+                    score = float(line.strip().split()[-1])
+                    if "dev" in line:
+                        dev_score = score
+                    elif "test" in line:
+                        test_score = score
+                    else:
+                        raise AssertionError("Did the parser score layout change?  Got an unexpected score line in %s" % filename)
+                    max_step = None
+                dev_match = dev_re.match(line)
+                if dev_match:
+                    step = int(dev_match.groups()[0])
+                    if args.step is not None and step > args.step:
+                        break
+                    max_step = step
+                    dev_score = float(dev_match.groups()[1]) * 100
             if dev_score is None:
                 dev_score = "N/A"
             else:
@@ -39,7 +57,10 @@ def main():
             else:
                 test_scores.append(test_score)
                 test_score = "%.2f" % test_score
-            print("%s     %s  %s" % (filename, dev_score, test_score))
+            if max_step is not None:
+                print("%s     %s  (%d)" % (filename, dev_score, max_step))
+            else:
+                print("%s     %s  %s" % (filename, dev_score, test_score))
 
     if len(dev_scores) > 0:
         dev_score = sum(dev_scores) / len(dev_scores)
