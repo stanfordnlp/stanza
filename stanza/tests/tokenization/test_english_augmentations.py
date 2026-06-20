1b42d4933ea7efc11c38737d9faf5e3b71558e33
John Bauer <horatio@gmail.com>
2026-06-20 15:06:50 -0700

    Add a brief test that verifies the English tokenizer is correctly processing both 'x, y' and 'x ,y'

---
+++ stanza/tests/tokenization/test_english_augmentations.py
@@ -0,0 +1,30 @@
+import pytest
+
+import stanza
+from stanza.tests import TEST_MODELS_DIR
+
+pytestmark = [pytest.mark.pipeline, pytest.mark.travis]
+
+
+@pytest.fixture(scope="module")
+def en_tokenize_pipeline():
+    """Tokenize-only English pipeline (no pos, depparse, etc.)."""
+    return stanza.Pipeline(
+        "en",
+        model_dir=TEST_MODELS_DIR,
+        download_method=None,
+        processors="tokenize",
+    )
+
+def test_comma_typos(en_tokenize_pipeline):
+    text = "This, tests comma errors"
+    doc = en_tokenize_pipeline(text)
+    assert len(doc.sentences) == 1
+    assert len(doc.sentences[0].words) == 5
+    assert doc.sentences[0].words[1].text == ','
+
+    text = "This ,tests comma errors"
+    doc = en_tokenize_pipeline(text)
+    assert len(doc.sentences) == 1
+    assert len(doc.sentences[0].words) == 5
+    assert doc.sentences[0].words[1].text == ','
