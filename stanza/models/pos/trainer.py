6c7984fb6b9b6ce179301623bc3e3c1d59a1b639
John Bauer <horatio@gmail.com>
2026-09-01 16:54:18 -0700

    Initial version of rewriting the XPOS column to allow multiple columns instead of just one.  Core issue: BHO has very little training data, but there's a 250000 word tagged dataset available in a slightly different tag format, so ideally we would be able to use one to supplement the other.  Also relevant would be including more English datasets or including a separate Sindhi tagset, for example

--- stanza/models/pos/trainer.py
+++ stanza/models/pos/trainer.py
@@ -11,20 +11,26 @@ from stanza.models.common.trainer import Trainer as BaseTrainer
 from stanza.models.common import utils, loss
 from stanza.models.common.foundation_cache import load_bert, load_bert_with_peft, NoTransformerFoundationCache
 from stanza.models.common.peft_config import build_peft_wrapper, load_peft_wrapper
-from stanza.models.pos.model import Tagger
+from stanza.models.pos.model import Tagger, remap_legacy_state_dict
+from stanza.models.pos.tag_columns import tag_columns_from_args, tag_columns_from_config, tag_columns_to_config
 from stanza.models.pos.vocab import MultiVocab
 
 logger = logging.getLogger('stanza')
 
 def unpack_batch(batch, device):
-    """ Unpack a batch from the data loader. """
-    inputs = [b.to(device) if b is not None else None for b in batch[:8]]
-    orig_idx = batch[8]
-    word_orig_idx = batch[9]
-    sentlens = batch[10]
-    wordlens = batch[11]
-    text = batch[12]
-    return inputs, orig_idx, word_orig_idx, sentlens, wordlens, text
+    """ Unpack a batch from the data loader.
+
+    Addressed by name rather than by position, as the number of tag
+    columns varies, and with it the offsets of the fields after them.
+    """
+    def to_device(b):
+        return b.to(device) if b is not None else None
+
+    inputs = [to_device(batch.words), to_device(batch.words_mask),
+              to_device(batch.wordchars), to_device(batch.wordchars_mask),
+              [to_device(tag) for tag in batch.tags],
+              to_device(batch.pretrained)]
+    return inputs, batch.orig_idx, batch.word_orig_idx, batch.lens, batch.word_lens, batch.text
 
 class Trainer(BaseTrainer):
     """ A trainer for training models. """
@@ -63,7 +69,7 @@ class Trainer(BaseTrainer):
     def update(self, batch, eval=False):
         device = next(self.model.parameters()).device
         inputs, orig_idx, word_orig_idx, sentlens, wordlens, text = unpack_batch(batch, device)
-        word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained = inputs
+        word, word_mask, wordchars, wordchars_mask, tags, pretrained = inputs
 
         if eval:
             self.model.eval()
@@ -71,7 +77,7 @@ class Trainer(BaseTrainer):
             self.model.train()
             for optimizer in self.optimizers.values():
                 optimizer.zero_grad()
-        loss, _ = self.model(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, word_orig_idx, sentlens, wordlens, text)
+        loss, _ = self.model(word, word_mask, wordchars, wordchars_mask, tags, pretrained, word_orig_idx, sentlens, wordlens, text)
         if loss == 0.0:
             return loss
 
@@ -91,16 +97,20 @@ class Trainer(BaseTrainer):
     def predict(self, batch, unsort=True):
         device = next(self.model.parameters()).device
         inputs, orig_idx, word_orig_idx, sentlens, wordlens, text = unpack_batch(batch, device)
-        word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained = inputs
+        word, word_mask, wordchars, wordchars_mask, tags, pretrained = inputs
 
         self.model.eval()
         batch_size = word.size(0)
-        _, preds = self.model(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, word_orig_idx, sentlens, wordlens, text)
-        upos_seqs = [self.vocab['upos'].unmap(sent) for sent in preds[0].tolist()]
-        xpos_seqs = [self.vocab['xpos'].unmap(sent) for sent in preds[1].tolist()]
-        feats_seqs = [self.vocab['feats'].unmap(sent) for sent in preds[2].tolist()]
+        _, preds = self.model(word, word_mask, wordchars, wordchars_mask, tags, pretrained, word_orig_idx, sentlens, wordlens, text)
 
-        pred_tokens = [[[upos_seqs[i][j], xpos_seqs[i][j], feats_seqs[i][j]] for j in range(sentlens[i])] for i in range(batch_size)]
+        # one sequence per output column, in column order.  Columns
+        # which aren't written back to the document (an extra tagset
+        # has nowhere in CoNLL-U to go) are predicted but dropped here
+        seqs = [[self.vocab[column.name].unmap(sent) for sent in pred.tolist()]
+                for column, pred in zip(self.model.tag_columns, preds)
+                if column.output]
+
+        pred_tokens = [[[seq[i][j] for seq in seqs] for j in range(sentlens[i])] for i in range(batch_size)]
         if unsort:
             pred_tokens = utils.unsort(pred_tokens, orig_idx)
         return pred_tokens
@@ -112,10 +122,16 @@ class Trainer(BaseTrainer):
             skipped = [k for k in model_state.keys() if k.split('.')[0] in self.model.unsaved_modules]
             for k in skipped:
                 del model_state[k]
+        # the tag columns are namedtuples of enums in the args, but the
+        # model file is read back with weights_only, which will only
+        # unpickle builtins, so they are flattened to plain strings here
+        # and turned back into columns in load()
+        config = dict(self.args)
+        config['tag_columns'] = tag_columns_to_config(tag_columns_from_args(self.args))
         params = {
                 'model': model_state,
                 'vocab': self.vocab.state_dict(),
-                'config': self.args
+                'config': config
                 }
         if self.args.get('use_peft', False):
             # Hide import so that peft dependency is optional
@@ -141,6 +157,7 @@ class Trainer(BaseTrainer):
             logger.error("Cannot load model from {}".format(filename))
             raise
         self.args = checkpoint['config']
+        self.args['tag_columns'] = tag_columns_from_config(self.args.get('tag_columns'))
         if args is not None: self.args.update(args)
 
         # preserve old models which were created before transformers were added
@@ -176,4 +193,18 @@ class Trainer(BaseTrainer):
             logger.debug("Model %s has a finetuned transformer.  Not using transformer cache to make sure the finetuned version of the transformer isn't accidentally used elsewhere", filename)
             foundation_cache = NoTransformerFoundationCache(foundation_cache)
         self.model = Tagger(self.args, self.vocab, emb_matrix=emb_matrix, share_hid=self.args['share_hid'], foundation_cache=foundation_cache, bert_model=bert_model, bert_tokenizer=bert_tokenizer, force_bert_saved=force_bert_saved, peft_name=peft_name)
-        self.model.load_state_dict(checkpoint['model'], strict=False)
+        incompatible = self.model.load_state_dict(remap_legacy_state_dict(checkpoint['model']), strict=False)
+        # strict=False, as save() leaves out the modules in
+        # unsaved_modules - the transformer, the pretrain, the charlms -
+        # which are stored separately.  Those are the only parameters
+        # allowed to be absent, so the same list forgives them here.
+        # Anything else means the model does not match its parameters,
+        # most likely because the tag columns are not the ones it was
+        # trained with, and a head would silently be left at its random
+        # initialization
+        missing = [x for x in incompatible.missing_keys
+                   if x.split('.')[0] not in self.model.unsaved_modules]
+        unexpected = list(incompatible.unexpected_keys)
+        if missing or unexpected:
+            raise ValueError("Cannot load the POS model %s: the saved parameters do not match the model.  Tag columns are %s.  Missing parameters: %s  Unexpected parameters: %s" %
+                             (filename, self.model.tag_names, missing, unexpected))
