302702760242a3b7df90b569b3d33460a41affb3
John Bauer <horatio@gmail.com>
2026-09-02 23:48:06 -0700

    Also exclude words which are entirely spaces... no way that is a valid expansion of an MWT.  Came up when training a QAF MWT

--- stanza/models/mwt/trainer.py
+++ stanza/models/mwt/trainer.py
@@ -122,7 +122,7 @@ class Trainer(BaseTrainer):
             # furthermore, if there are no spaces predicted by the seq2seq,
             # might as well use the original in case the seq2seq went crazy
             # this particular error came up training a Hebrew MWT
-            pred_tokens = [x if x and ' ' in x else y for x, y in zip(pred_tokens, orig_text)]
+            pred_tokens = [x if x.strip() and ' ' in x else y for x, y in zip(pred_tokens, orig_text)]
         if unsort:
             pred_tokens = utils.unsort(pred_tokens, orig_idx)
         return pred_tokens
