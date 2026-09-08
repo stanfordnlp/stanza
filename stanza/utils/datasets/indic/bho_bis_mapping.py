82128cd3d45736a74490f88443f162b4bc88b0ef
John Bauer <horatio@gmail.com>
2026-09-08 08:49:49 -0700

    bho_combined - a combination of the UD BHO dataset and an xpos-only dataset

---
+++ stanza/utils/datasets/indic/bho_bis_mapping.py
@@ -0,0 +1,44 @@
+# UD_Bhojpuri-BHTB XPOS -> BIS
+XPOS_TO_BIS = {
+    "N_NN":    "NN",
+    "N_NNC":   "NN",
+    "N_NNP":   "NNP",
+    "N_NST":   "NST",
+
+    "V_VM":    "VM",
+    "V_VAUX":  "VAUX",
+
+    "PR_PRP":  "PRP",
+    "PR_PRF":  "PRP",
+    "PR_PRI":  "PRP",
+    "PR_PRL":  "PRP",
+    "PR_PRQ":  "WQ",
+
+    "DM_DMQ":  "WQ",
+    "DM_DMD":  "DEM",
+    "DM_DMR":  "DEM",
+    "DM_DMI":  "DEM",
+
+    "CC_CCD":  "CC",
+    "CC_CCS":  "CC",
+
+    "QT_QTC":  "QC",
+    "QT_QTF":  "QF",
+    "QT_QTO":  "QO",
+
+    "RP_NEG":  "NEG",
+    "RP_RPD":  "RDP",
+    "RP_INTF": "INTF",
+    "RP_CL":   "CL",
+    "RP_INJ":  "INJ",
+
+    "RD_ECH":  "ECH",
+    "RD_PUNC": "SYM",
+    "RD_SYM":  "SYM",
+
+    "JJ":      "JJ",
+    "PSP":     "PSP",
+    "RB":      "RB",
+    "RP":      "RP",
+}
+
